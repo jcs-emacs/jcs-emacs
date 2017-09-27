14b114513bb354ff3515254efd7dc596270ff0bf
jenchieh <jcs090218@gmail.com>
2017-09-27 14:26:53 +0800

    Correct the file name by copy and pasting issue.

--- .emacs.d/elisp/jcs-ex/ex-func/jcs-trans-window.el
+++ .emacs.d/elisp/jcs-ex/ex-func/jcs-trans-window.el
@@ -13,12 +13,12 @@
 
 ;; Copyright (C) 2017 Jen-Chieh Shen
 
-;; jcs-function is free software: you can redistribute it and/or modify
+;; jcs-trans-window is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; jcs-function is distributed in the hope that it will be useful,
+;; jcs-trans-window is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
