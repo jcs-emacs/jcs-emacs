14be4cc2676ad37a06219577954f289982103462
jenchieh <jcs090218@gmail.com>
2017-11-21 16:35:26 +0800

    remove no needed empty line.

--- .emacs.d/elisp/jcs-ex/jcs-plugin.el
+++ .emacs.d/elisp/jcs-ex/jcs-plugin.el
@@ -219,23 +219,5 @@
 (require 'helm)
 
 
-;;; Diminish
-;; NOTE(jenchieh): Do not show theses modes in the mode line.
-(require 'diminish)
-(diminish 'abbrev-mode)
-(diminish 'auto-complete-mode)
-(diminish 'company-mode)
-(diminish 'flycheck-mode)
-(diminish 'flymake-mode)
-(diminish 'helm-mode)
-(diminish 'helm-gtags-mode)
-(diminish 'impatient-mode)
-(diminish 'js2-refactor-mode)
-(diminish 'js2r)
-(diminish 'outline-minor-mode)
-(diminish 'skewer-mode)
-(diminish 'yas-minor-mode)
-(diminish 'auto-highlight-symbol-mode)
-
 ;;------------------------------------------------------------------------------------------------------
 ;; This is the end of jcs-plugin.el file
