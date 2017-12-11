50a178528ea6bc6419fbbf8b3e9d8dee7ff8b8b9
jenchieh <jcs090218@gmail.com>
2017-12-11 08:32:18 +0800

    Make all casy's key binding to global key file.

--- .emacs.d/elisp/jcs-ex/jcs-global-key.el
+++ .emacs.d/elisp/jcs-ex/jcs-global-key.el
@@ -31,7 +31,90 @@
 ;; JayCeS Global Key
 ;;================================================
 
-;;; bind the key
+;; Setup my key binding
+(global-set-key (read-kbd-macro "\eb")  'ido-switch-buffer)
+(global-set-key (read-kbd-macro "\eB")  'ido-switch-buffer-other-window)
+
+;; no screwing with my middle mouse button
+(global-unset-key [mouse-2])
+
+(define-key global-map "\et" 'load-todo)
+(define-key global-map "\eT" 'load-log)
+
+(define-key global-map [f8] 'casey-replace-string)
+
+(define-key global-map "\ep" 'quick-calc)
+
+(define-key global-map [C-right] 'forward-word)
+(define-key global-map [C-left] 'backward-word)
+(define-key global-map [C-up] 'previous-blank-line)
+(define-key global-map [C-down] 'next-blank-line)
+(define-key global-map [end] 'end-of-line)
+(define-key global-map [pgup] 'forward-page)
+(define-key global-map [pgdown] 'backward-page)
+(define-key global-map [C-next] 'scroll-other-window)
+(define-key global-map [C-prior] 'scroll-other-window-down)
+
+(define-key global-map "\e " 'set-mark-command)
+(define-key global-map "\eq" 'jcs-other-window-prev)
+(define-key global-map "\ea" 'yank)
+(define-key global-map "\ez" 'kill-region)
+(define-key global-map [M-up] 'previous-blank-line)
+(define-key global-map [M-down] 'next-blank-line)
+(define-key global-map [M-right] 'forward-word)
+(define-key global-map [M-left] 'backward-word)
+
+(define-key global-map "\e:" 'View-back-to-mark)
+(define-key global-map "\e;" 'exchange-point-and-mark)
+
+(define-key global-map [f9] 'first-error)
+(define-key global-map [f10] 'previous-error)
+(define-key global-map [f11] 'next-error)
+
+(define-key global-map "\en" 'next-error)
+(define-key global-map "\eN" 'previous-error)   ;; 'ALT-SHIFT-n' = '\eN'
+
+(define-key global-map "\eg" 'goto-line)
+(define-key global-map "\ej" 'imenu)
+
+;; Editting
+(define-key global-map "" 'copy-region-as-kill)
+(define-key global-map "" 'yank)
+(define-key global-map "" 'nil)
+(define-key global-map "" 'rotate-yank-pointer)
+(define-key global-map "\eu" 'undo)
+(define-key global-map "\e6" 'upcase-word)
+(define-key global-map "\e^" 'captilize-word)
+(define-key global-map "\e." 'fill-paragraph)
+
+
+(define-key global-map "\el" 'casey-replace-in-region)
+
+(define-key global-map "\eo" 'query-replace)
+(define-key global-map "\eO" 'casey-replace-string)
+
+;; \377 is alt-backspace
+(define-key global-map "\377" 'backward-kill-word)
+(define-key global-map [M-delete] 'kill-word)
+
+(define-key global-map "\e[" 'start-kbd-macro)
+(define-key global-map "\e]" 'end-kbd-macro)
+(define-key global-map "\e'" 'call-last-kbd-macro)
+
+;; Buffers
+(define-key global-map "\er" 'revert-buffer)
+(define-key global-map "\ek" 'kill-this-buffer)
+(define-key global-map "\es" 'save-buffer)
+
+(define-key global-map "\t" 'dabbrev-expand)
+(define-key global-map [S-tab] 'indent-for-tab-command)
+(define-key global-map [backtab] 'indent-for-tab-command)
+(define-key global-map "\C-y" 'indent-for-tab-command)
+(define-key global-map [C-tab] 'indent-region)
+;;(define-key global-map "      " 'indent-region)
+
+
+;;; Bind the Key
 ;;(define-key global-map "\ef" 'ido-find-file)
 ;;(define-key global-map "\eF" 'ido-find-file-other-window)
 (define-key global-map "\ef" 'jcs-helm-find-files)
