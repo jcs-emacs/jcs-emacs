5e849cf99b2ffb87ea02798774412aafd9cf91a6
jenchieh <jcs090218@gmail.com>
2017-12-11 08:32:50 +0800

    Remove casey global key to proper file.

--- .emacs.d/elisp/jcs-ex/jcs-env.el
+++ .emacs.d/elisp/jcs-ex/jcs-env.el
@@ -57,8 +57,7 @@
 (when casey-win32
   (setq casey-makescript "build.bat")
   (setq jenchieh-runscript "run.bat")
-  (setq casey-font "outline-Liberation Mono")
-  )
+  (setq casey-font "outline-Liberation Mono"))
 
 (when casey-aquamacs
   (cua-mode 0)
@@ -74,14 +73,12 @@
   (scroll-bar-mode nil)
   (setq mac-pass-command-to-system nil)
   (setq casey-makescript "./build.macosx")
-  (setq jenchieh-runscript "./run.macosx")
-  )
+  (setq jenchieh-runscript "./run.macosx"))
 
 (when casey-linux
   (setq casey-makescript "./build.linux")
   (setq jenchieh-runscript "./run.linux")
-  (display-battery-mode 1)
-  )
+  (display-battery-mode 1))
 
 ;; Turn off the toolbar
 (tool-bar-mode 0)
@@ -92,13 +89,8 @@
 (require 'compile)
 (ido-mode t)
 
-;; Setup my key binding
-(global-set-key (read-kbd-macro "\eb")  'ido-switch-buffer)
-(global-set-key (read-kbd-macro "\eB")  'ido-switch-buffer-other-window)
-
 (defun casey-ediff-setup-windows (buffer-A buffer-B buffer-C control-buffer)
-  (ediff-setup-windows-plain buffer-A buffer-B buffer-C control-buffer)
-  )
+  (ediff-setup-windows-plain buffer-A buffer-B buffer-C control-buffer))
 (setq ediff-window-setup-function 'casey-ediff-setup-windows)
 (setq ediff-split-window-function 'split-window-horizontally)
 
@@ -126,9 +118,7 @@
 
 (defun load-todo ()
   (interactive)
-  (find-file casey-todo-file)
-  )
-(define-key global-map "\et" 'load-todo)
+  (find-file casey-todo-file))
 
 (defun insert-timeofday ()
   (interactive "*")
@@ -148,12 +138,7 @@
   (insert-timeofday)
   (newline-and-indent)
   (newline-and-indent)
-  (end-of-buffer)
-  )
-(define-key global-map "\eT" 'load-log)
-
-;; no screwing with my middle mouse button
-(global-unset-key [mouse-2])
+  (end-of-buffer))
 
 ;; Bright-red TODOs
 (setq fixme-modes '(actionscript-mode
@@ -247,13 +232,12 @@
 (modify-face 'font-lock-option-face "dark green" nil nil t nil t nil nil)
 (modify-face 'font-lock-or-face "green yellow" nil nil t nil t nil nil)
 
+
 (defun casey-replace-string (FromString ToString)
   "Replace a string without moving point."
   (interactive "sReplace: \nsReplace: %s  With: ")
   (save-excursion
-    (replace-string FromString ToString)
-    ))
-(define-key global-map [f8] 'casey-replace-string)
+    (replace-string FromString ToString)))
 
 (defun casey-save-buffer ()
   "Save the buffer after untabifying it."
@@ -290,8 +274,6 @@
   (when casey-aquamacs (aquamacs-toggle-full-frame))
   (when casey-win32 (w32-send-sys-command 61488)))
 
-(define-key global-map "\ep" 'quick-calc)
-
 ;; Navigation
 (defun previous-blank-line ()
   "Moves to the previous line containing nothing but whitespace."
@@ -304,18 +286,6 @@
   (forward-line)
   (search-forward-regexp "^[ \t]*\n")
   (forward-line -1))
-
-(define-key global-map [C-right] 'forward-word)
-(define-key global-map [C-left] 'backward-word)
-(define-key global-map [C-up] 'previous-blank-line)
-(define-key global-map [C-down] 'next-blank-line)
-;;(define-key global-map [home] 'beginning-of-line)
-;;(define-key global-map [home] 'back-to-indentation)
-(define-key global-map [end] 'end-of-line)
-(define-key global-map [pgup] 'forward-page)
-(define-key global-map [pgdown] 'backward-page)
-(define-key global-map [C-next] 'scroll-other-window)
-(define-key global-map [C-prior] 'scroll-other-window-down)
 
 ;; ALT-alternatives
 ;; (defadvice set-mark-command (after no-bloody-t-m-m activate)
@@ -330,39 +300,7 @@
   "Performs copy-region-as-kill as an append."
   (interactive)
   (append-next-kill)
-  (copy-region-as-kill (mark) (point))
-  )
-(define-key global-map "\e " 'set-mark-command)
-(define-key global-map "\eq" 'jcs-other-window-prev)
-(define-key global-map "\ea" 'yank)
-(define-key global-map "\ez" 'kill-region)
-(define-key global-map [M-up] 'previous-blank-line)
-(define-key global-map [M-down] 'next-blank-line)
-(define-key global-map [M-right] 'forward-word)
-(define-key global-map [M-left] 'backward-word)
-
-(define-key global-map "\e:" 'View-back-to-mark)
-(define-key global-map "\e;" 'exchange-point-and-mark)
-
-(define-key global-map [f9] 'first-error)
-(define-key global-map [f10] 'previous-error)
-(define-key global-map [f11] 'next-error)
-
-(define-key global-map "\en" 'next-error)
-(define-key global-map "\eN" 'previous-error)   ;; 'ALT-SHIFT-n' = '\eN'
-
-(define-key global-map "\eg" 'goto-line)
-(define-key global-map "\ej" 'imenu)
-
-;; Editting
-(define-key global-map "" 'copy-region-as-kill)
-(define-key global-map "" 'yank)
-(define-key global-map "" 'nil)
-(define-key global-map "" 'rotate-yank-pointer)
-(define-key global-map "\eu" 'undo)
-(define-key global-map "\e6" 'upcase-word)
-(define-key global-map "\e^" 'captilize-word)
-(define-key global-map "\e." 'fill-paragraph)
+  (copy-region-as-kill (mark) (point)))
 
 (defun casey-replace-in-region (old-word new-word)
   "Perform a replace-string in the current region."
@@ -371,25 +309,7 @@
                     (narrow-to-region (mark) (point))
                     (beginning-of-buffer)
                     (replace-string old-word new-word)
-                    ))
-  )
-(define-key global-map "\el" 'casey-replace-in-region)
-
-(define-key global-map "\eo" 'query-replace)
-(define-key global-map "\eO" 'casey-replace-string)
-
-;; \377 is alt-backspace
-(define-key global-map "\377" 'backward-kill-word)
-(define-key global-map [M-delete] 'kill-word)
-
-(define-key global-map "\e[" 'start-kbd-macro)
-(define-key global-map "\e]" 'end-kbd-macro)
-(define-key global-map "\e'" 'call-last-kbd-macro)
-
-;; Buffers
-(define-key global-map "\er" 'revert-buffer)
-(define-key global-map "\ek" 'kill-this-buffer)
-(define-key global-map "\es" 'save-buffer)
+                    )))
 
 ;; Compilation
 (setq compilation-context-lines t)
@@ -455,13 +375,6 @@
 (setq-default truncate-lines t)
 (setq truncate-partial-width-windows nil)
 (split-window-horizontally)
-
-(define-key global-map "\t" 'dabbrev-expand)
-(define-key global-map [S-tab] 'indent-for-tab-command)
-(define-key global-map [backtab] 'indent-for-tab-command)
-(define-key global-map "\C-y" 'indent-for-tab-command)
-(define-key global-map [C-tab] 'indent-region)
-;;(define-key global-map "      " 'indent-region)
 
 (defun casey-never-split-a-window
     "Never, ever split a window.  Why would anyone EVER want you to do that??"
