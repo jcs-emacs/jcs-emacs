535bb925d11e18522c30d6beff9aa90d993f711e
jenchieh <jcs090218@gmail.com>
2017-12-16 05:25:07 +0800

    move move empty line up and down.

--- .emacs.d/elisp/jcs-ex/ex-func/jcs-nav.el
+++ .emacs.d/elisp/jcs-ex/ex-func/jcs-nav.el
@@ -28,6 +28,23 @@
 
 
 ;;----------------------------------------------
+;; Navigating Blank Line
+;;----------------------------------------------
+
+;; Navigation
+(defun previous-blank-line ()
+  "Moves to the previous line containing nothing but whitespace."
+  (interactive)
+  (search-backward-regexp "^[ \t]*\n"))
+
+(defun next-blank-line ()
+  "Moves to the next line containing nothing but whitespace."
+  (interactive)
+  (forward-line)
+  (search-forward-regexp "^[ \t]*\n")
+  (forward-line -1))
+
+;;----------------------------------------------
 ;; Navigating Parentheses
 ;;----------------------------------------------
 
