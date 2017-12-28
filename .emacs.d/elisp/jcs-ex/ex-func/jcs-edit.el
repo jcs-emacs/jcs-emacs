563391fc53b0f26af1d5b26d688a01bd129206d5
jenchieh <jcs090218@gmail.com>
2017-12-29 07:32:52 +0800

    Refactor for buffer menu mode.

--- .emacs.d/elisp/jcs-ex/ex-func/jcs-edit.el
+++ .emacs.d/elisp/jcs-ex/ex-func/jcs-edit.el
@@ -562,57 +562,6 @@ False: return nil."
     (switch-to-previous-buffer))
   )
 
-(defun jcs-buffer-menu ()
-  "Open Buffer Menu."
-  (interactive)
-
-  ;; NOTE(jenchieh): even you reopen the `buffer-menu'
-  ;; this will not sort.
-  (buffer-menu)
-
-  ;; Sort it?
-  ;;(jcs-buffer-menu-sort)
-  )
-
-;; TOPIC(jenchieh): BufferMenuPlus
-;; URL(jenchieh): https://www.emacswiki.org/emacs/BufferMenuPlus
-;; Sorted by (1) visit, (2) buffer, (3) size, (4) time, (5) mode, (6) file. More
-(defun jcs-buffer-menu-sort-by-visit ()
-  "Sort the Buffer Menu List by visit."
-  (interactive)
-  (Buffer-menu-sort 1)
-  (beginning-of-buffer))
-
-(defun jcs-buffer-menu-sort-by-buffer ()
-  "Sort the Buffer Menu List by buffer."
-  (interactive)
-  (Buffer-menu-sort 2)
-  (beginning-of-buffer))
-
-(defun jcs-buffer-menu-sort-by-size ()
-  "Sort the Buffer Menu List by size."
-  (interactive)
-  (Buffer-menu-sort 3)
-  (beginning-of-buffer))
-
-(defun jcs-buffer-menu-sort-by-time ()
-  "Sort the Buffer Menu List by time."
-  (interactive)
-  (Buffer-menu-sort 4)
-  (beginning-of-buffer))
-
-(defun jcs-buffer-menu-sort-by-mode ()
-  "Sort the Buffer Menu List by mode."
-  (interactive)
-  (Buffer-menu-sort 5)
-  (beginning-of-buffer))
-
-(defun jcs-buffer-menu-sort-by-file ()
-  "Sort the Buffer Menu List by file name."
-  (interactive)
-  (Buffer-menu-sort 6)
-  (beginning-of-buffer))
-
 ;;;###autoload
 (defun jcs-maybe-kill-this-buffer ()
   "Kill the buffer if this file is the only file. Otherwise just
