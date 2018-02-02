feeaa253dd33c1930bc3e30ef436dec9ab6c0f5c
jenchieh <jcs090218@gmail.com>
2018-02-02 18:15:06 +0800

    Add org table navigation key bindings.

--- .emacs.d/elisp/jcs-ex/ex-mode/jcs-txt-mode.el
+++ .emacs.d/elisp/jcs-ex/ex-mode/jcs-txt-mode.el
@@ -125,6 +125,12 @@
   (define-key org-mode-map "\C-c\C-c" 'kill-ring-save)
   (define-key org-mode-map "\C-a" 'mark-whole-buffer)
   (define-key org-mode-map [tab] 'jcs-tab-key)
+  (define-key org-mode-map [C-tab] 'org-cycle)
+
+  (define-key org-mode-map (kbd "S-<up>") 'jcs-org-table-up)
+  (define-key org-mode-map (kbd "S-<down>") 'jcs-org-table-down)
+  (define-key org-mode-map (kbd "S-<left>") 'jcs-org-table-left)
+  (define-key org-mode-map (kbd "S-<right>") 'jcs-org-table-right)
   )
 (add-hook 'org-mode-hook 'jcs-org-mode)
 
