264de3d63c23650f78679ca730ac2b00cd241e69
jenchieh <jcs090218@gmail.com>
2018-04-07 20:43:42 +0800

    change java imports key binding to self impl function.

--- .emacs.d/elisp/jcs-ex/ex-mode/jcs-java-mode.el
+++ .emacs.d/elisp/jcs-ex/ex-mode/jcs-java-mode.el
@@ -32,15 +32,6 @@
 ;;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 
-;;; Java Imports
-(require 'java-imports)
-;; See customization below for where to put java imports
-(setq
- java-imports-find-block-function
- 'java-imports-find-place-sorted-block)
-
-(add-hook 'jdee-mode-hook 'java-imports-scan-file)
-
 ;;; Minor mode for Java Development
 ;; SOURCE: https://github.com/mopemope/meghanada-emacs
 (require 'meghanada)
@@ -64,10 +55,15 @@
   ;; URL(jenchieh): https://emacs.stackexchange.com/questions/9583/how-to-treat-underscore-as-part-of-the-word
   (modify-syntax-entry ?_ "w")
 
-
   (defun jcs-java-class-format ()
     "Format the given file as a class. - JenChieh Java class"
     (when (is-current-file-empty-p)
+      ;; insert the package declaration.
+      (jcs-java-insert-package-from-src)
+
+      ;; Leave one empty line between header.
+      (insert "\n")
+
       (jcs-insert-java-template)))
 
   (cond ((file-exists-p buffer-file-name) t)
@@ -76,6 +72,10 @@
 
   ;; Set Faces.
   (jcs-oop-init-set-face)
+
+  ;; NOTE(jenchieh): change the face locally to this mode.
+  (face-remap-add-relative 'font-lock-constant-face
+                           '((:foreground "#D2D2D2")))
 
   ;; jcs java key binding
   (define-key java-mode-map (kbd "C-d") 'jcs-kill-whole-line)
@@ -89,8 +89,8 @@
   (define-key java-mode-map "\ew" 'jcs-other-window-next)
   (define-key java-mode-map (kbd "M-q") 'jcs-other-window-prev)
 
-  ;; `java-imports'
-  (define-key java-mode-map (kbd "C-S-o") 'java-imports-add-import-dwim)
+  ;; imports/package declaration.
+  (define-key java-mode-map (kbd "C-S-o") 'jcs-java-organize-imports)
   )
 (add-hook 'java-mode-hook 'jcs-java-mode-hook)
 (add-to-list 'auto-mode-alist '("\\.java?\\'" . java-mode))
@@ -116,6 +116,8 @@
 (set-face-attribute 'jdee-font-lock-number-face nil
                     :foreground "olive drab")
 
+(set-face-attribute 'jdee-font-lock-constant-face nil
+                    :foreground "#D2D2D2")
 
 ;;------------------------------------------------------------------------------------------------------
 ;; This is the end of jcs-java-mode.el file
