1c42050bfd65d02494d3d50813a5b72fc6e8ea71
Jen-Chieh <jcs090218@gmail.com>
2018-05-07 16:46:18 +0800

    Set default major mode to org mode.

--- .emacs.d/elisp/jcs-ex/jcs-env.el
+++ .emacs.d/elisp/jcs-ex/jcs-env.el
@@ -1,30 +1,11 @@
-;; This is the start of jcs-env.el file
-;;------------------------------------------------------------------------------------------------------
-
-;; jcs-env.el             -*- Emacs-Lisp -*-
-
-;; Mode for editing JayCeS code
-
-;; Created:    <Fri Oct 21 13:51:49 EST 2016>
-;; Time-stamp: <2017-05-29 10:21:39>
-;; Author:     Jen-Chieh Shen <jcs090218@gmail.com>
-;; Version:    0.1
-;; Keywords:   JayCeS, languages, os, operating system
-
-;; Copyright (C) 2017 Jen-Chieh Shen
-
-;; jcs-env is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; jcs-env is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; ========================================================================
+;; $File: jcs-env.el $
+;; $Date: 2017-05-29 10:21:39 $
+;; $Revision: $
+;; $Creator: Jen-Chieh Shen $
+;; $Notice: See LICENSE.txt for modification and distribution information
+;;                   Copyright © 2017 by Shen, Jen-Chieh $
+;; ========================================================================
 
 
 ;;================================================
@@ -508,5 +489,5 @@ NEW-WORD : new word ready to be insert."
 ;; Enable address mode.
 (goto-address-mode t)
 
-;;------------------------------------------------------------------------------------------------------
-;; This is the end of jcs-env.el file
+;; Set default major mode to `org-mode'.
+(setq-default major-mode 'org-mode)
