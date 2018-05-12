9defbe200fd76e2e33a439f082c88bfdaf2f82b9
jenchieh <jcs090218@gmail.com>
2018-05-12 19:31:08 +0800

    Add specific hool env/Emacs settings.

---
+++ .emacs.d/elisp/jcs-ex/jcs-hook.el
@@ -0,0 +1,49 @@
+;; ========================================================================
+;; $File: jcs-hook.el $
+;; $Date: 2018-05-12 08:53:47 $
+;; $Revision: $
+;; $Creator: Jen-Chieh Shen $
+;; $Notice: See LICENSE.txt for modification and distribution information
+;;                   Copyright © 2018 by Shen, Jen-Chieh $
+;; ========================================================================
+
+
+;;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+;; All the hook event do here..
+;;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+;;
+;; All hook listed.
+;; URL(jenchieh): https://www.gnu.org/software/emacs/manual/html_node/elisp/Standard-Hooks.html
+;;
+
+;;-----------------------------------------------------------
+;;-----------------------------------------------------------
+
+;;;###autoload
+(defun jcs-focus-in-hook ()
+  "When window is focus."
+  (jcs-revert-all-file-buffers)
+  )
+(add-hook 'focus-in-hook 'jcs-focus-in-hook)
+
+;;;###autoload
+(defun jcs-focus-out-hook ()
+  "When window is not focus."
+  )
+(add-hook 'focus-out-hook 'jcs-focus-out-hook)
+
+;;-----------------------------------------------------------
+;;-----------------------------------------------------------
+
+;;;###autoload
+(defun jcs-find-file-hook ()
+  "When temporary buffer shown."
+
+  (save-selected-window
+    (ignore-errors
+      (jcs-jump-to-window "*Buffer List*"))
+    (when (jcs-is-current-major-mode-p "Buffer-menu-mode")
+      (jcs-buffer-menu)))
+  )
+(add-hook 'find-file-hook 'jcs-find-file-hook)
