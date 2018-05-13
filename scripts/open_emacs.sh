d4188ac108347636c7e0874e667634a2e8334abd
jenchieh <jcs090218@gmail.com>
2018-05-14 04:29:16 +0800

    add shell version open emacs.

---
+++ scripts/open_emacs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# ========================================================================
+# $File: open_emacs.sh $
+# $Date: 2018-05-14 04:27:50 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2018 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+EMACS_INIT_PATH=
+EMACS_PROGRAM_PATH=
+
+
+# Run emacs with initialize setting file.
+$EMACS_PROGRAM_PATH $EMACS_INIT_PATH
