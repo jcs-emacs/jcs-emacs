24fcbaeea51643f7caf656e60caeab8bf4b4f670
jenchieh <jcs090218@gmail.com>
2018-05-16 04:48:32 +0800

    save excursion for revert all buffer.

--- .emacs.d/elisp/jcs-ex/ex-func/jcs-util.el
+++ .emacs.d/elisp/jcs-ex/ex-func/jcs-util.el
@@ -41,23 +41,24 @@ Buffers visiting files which do not exist any more or are no longer readable
 will be killed.
 SOURCE(jenchieh): https://emacs.stackexchange.com/questions/24459/revert-all-open-buffers-and-ignore-errors"
   (interactive)
-  (dolist (buf (buffer-list))
-    (let ((filename (buffer-file-name buf)))
-      ;; Revert only buffers containing files, which are not modified;
-      ;; do not try to revert non-file buffers like *Messages*.
-      (when (and filename
-                 (not (buffer-modified-p buf)))
-        (if (file-readable-p filename)
-            ;; If the file exists and is readable, revert the buffer.
-            (with-current-buffer buf
-              (revert-buffer :ignore-auto :noconfirm :preserve-modes))
-          ;; Otherwise, kill the buffer.
-          (let (kill-buffer-query-functions) ; No query done when killing buffer
-            (kill-buffer buf)
-            ;;(message "Killed non-existing/unreadable file buffer: %s" filename)
-            )))))
-  ;;(message "Finished reverting buffers containing unmodified files.")
-  )
+  (save-excursion
+    (dolist (buf (buffer-list))
+      (let ((filename (buffer-file-name buf)))
+        ;; Revert only buffers containing files, which are not modified;
+        ;; do not try to revert non-file buffers like *Messages*.
+        (when (and filename
+                   (not (buffer-modified-p buf)))
+          (if (file-readable-p filename)
+              ;; If the file exists and is readable, revert the buffer.
+              (with-current-buffer buf
+                (revert-buffer :ignore-auto :noconfirm :preserve-modes))
+            ;; Otherwise, kill the buffer.
+            (let (kill-buffer-query-functions) ; No query done when killing buffer
+              (kill-buffer buf)
+              ;;(message "Killed non-existing/unreadable file buffer: %s" filename)
+              )))))
+    ;;(message "Finished reverting buffers containing unmodified files.")
+    ))
 
 ;;---------------------------------------------
 ;; Make the time stamp base on the format
