997abdf8ab3d00c8425d4de4632198be2b7e3ad0
Jen-Chieh <jcs090218@gmail.com>
2018-05-15 16:56:27 +0800

    Add package for package management.

---
+++ .emacs.d/elisp/jcs-ex/jcs-package.el
@@ -0,0 +1,60 @@
+;; ========================================================================
+;; $File: jcs-package.el $
+;; $Date: 2018-05-15 16:37:28 $
+;; $Revision: $
+;; $Creator: Jen-Chieh Shen $
+;; $Notice: See LICENSE.txt for modification and distribution information
+;;                   Copyright © 2018 by Shen, Jen-Chieh $
+;; ========================================================================
+
+
+;;;###autoload
+(defun package-upgrade-all ()
+  "Upgrade all packages automatically without showing *Packages* buffer."
+  (interactive)
+  (package-refresh-contents)
+  (let (upgrades)
+    (cl-flet ((get-version (name where)
+                (let ((pkg (cadr (assq name where))))
+                  (when pkg
+                    (package-desc-version pkg)))))
+      (dolist (package (mapcar #'car package-alist))
+        (let ((in-archive (get-version package package-archive-contents)))
+          (when (and in-archive
+                     (version-list-< (get-version package package-alist)
+                                     in-archive))
+            (push (cadr (assq package package-archive-contents))
+                  upgrades)))))
+    (if upgrades
+        (when (yes-or-no-p
+               (message "Upgrade %d package%s (%s)? "
+                        (length upgrades)
+                        (if (= (length upgrades) 1) "" "s")
+                        (mapconcat #'package-desc-full-name upgrades ", ")))
+          (save-window-excursion
+            (dolist (package-desc upgrades)
+              (let ((old-package (cadr (assq (package-desc-name package-desc)
+                                             package-alist))))
+                (package-install package-desc)
+                (package-delete  old-package)))))
+      (message "All packages are up to date"))))
+
+;; NOTE(jenchieh): Only in Emacs 25.1+
+(defun package-menu-filter-by-status (status)
+  "Filter the *Packages* buffer by status."
+  (interactive
+   (list (completing-read
+          "Status: " '(".."
+                       "available"
+                       "built-in"
+                       "dependency"
+                       "incompat"
+                       "installed"
+                       "new"
+                       "obsolete"))))
+
+  (if (string= status "..")
+      (package-list-packages)
+    (package-menu-filter (concat "status:" status))))
+
+(define-key package-menu-mode-map "s" #'package-menu-filter-by-status)
