5def108a4510591bca35e9b36b25c625db001420
Jen-Chieh <jcs090218@gmail.com>
2018-05-17 15:45:19 +0800

    Add deletetion series of key bindgins.

--- .emacs.d/elisp/jcs-ex/ex-mode/jcs-shell-mode.el
+++ .emacs.d/elisp/jcs-ex/ex-mode/jcs-shell-mode.el
@@ -12,6 +12,14 @@
 (defun jcs-shell-mode-hook ()
   "Shell mode hook."
 
+  (define-key shell-mode-map (kbd "RET") 'jcs-shell-return)
+
+  (define-key shell-mode-map (kbd "C-<backspace>") 'jcs-shell-backward-delete-word)
+  (define-key shell-mode-map (kbd "C-S-<backspace>") 'jcs-shell-forward-delete-word)
+  (define-key shell-mode-map (kbd "M-<backspace>") 'jcs-shell-backward-kill-word-capital)
+  (define-key shell-mode-map (kbd "M-S-<backspace>") 'jcs-shell-forward-kill-word-capital)
+
+  (define-key shell-mode-map (kbd "C-d") 'jcs-shell-kill-whole-line)
   (define-key shell-mode-map (kbd "<backspace>") 'jcs-shell-backspace)
   )
 (add-hook 'shell-mode-hook 'jcs-shell-mode-hook)
