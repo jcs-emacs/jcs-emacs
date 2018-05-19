4ca8ece78bbadd32383cec669bdf2bd260e1c34f
jenchieh <jcs090218@gmail.com>
2018-05-19 19:58:56 +0800

    Remap shell-mode key bindings for trying simulate the real shell action.

--- .emacs.d/elisp/jcs-ex/ex-mode/jcs-shell-mode.el
+++ .emacs.d/elisp/jcs-ex/ex-mode/jcs-shell-mode.el
@@ -17,16 +17,26 @@
   (define-key shell-mode-map [tab] #'company-manual-begin)
 
   ;; Command Input
-  (define-key shell-mode-map (kbd "RET") 'jcs-shell-return)
+  (define-key shell-mode-map (kbd "RET") #'jcs-shell-return)
+
+  ;; Navigation
+  (define-key shell-mode-map (kbd "<up>") 'jcs-shell-up-key)
+  (define-key shell-mode-map (kbd "<down>") 'jcs-shell-down-key)
+  (define-key shell-mode-map [C-up] #'previous-blank-line)
+  (define-key shell-mode-map [C-down] #'next-blank-line)
+
+  ;; Editing
+  (define-key shell-mode-map "\C-c\C-c" #'kill-ring-save)
+  (define-key shell-mode-map "\C-x\C-x" #'kill-ring-save)
 
   ;; Deletion
-  (define-key shell-mode-map (kbd "C-<backspace>") 'jcs-shell-backward-delete-word)
-  (define-key shell-mode-map (kbd "C-S-<backspace>") 'jcs-shell-forward-delete-word)
-  (define-key shell-mode-map (kbd "M-<backspace>") 'jcs-shell-backward-kill-word-capital)
-  (define-key shell-mode-map (kbd "M-S-<backspace>") 'jcs-shell-forward-kill-word-capital)
+  (define-key shell-mode-map (kbd "C-<backspace>") #'jcs-shell-backward-delete-word)
+  (define-key shell-mode-map (kbd "C-S-<backspace>") #'jcs-shell-forward-delete-word)
+  (define-key shell-mode-map (kbd "M-<backspace>") #'jcs-shell-backward-kill-word-capital)
+  (define-key shell-mode-map (kbd "M-S-<backspace>") #'jcs-shell-forward-kill-word-capital)
 
-  (define-key shell-mode-map (kbd "C-d") 'jcs-shell-kill-whole-line)
-  (define-key shell-mode-map (kbd "<backspace>") 'jcs-shell-backspace)
+  (define-key shell-mode-map (kbd "C-d") #'jcs-shell-kill-whole-line)
+  (define-key shell-mode-map (kbd "<backspace>") #'jcs-shell-backspace)
   )
-(add-hook 'shell-mode-hook 'jcs-shell-mode-hook)
+(add-hook 'shell-mode-hook #'jcs-shell-mode-hook)
 (add-hook 'shell-mode-hook #'company-mode)
