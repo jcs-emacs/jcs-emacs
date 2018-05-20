54578c79be3f87290ac4e128eb02ea8455049c2d
jenchieh <jcs090218@gmail.com>
2018-05-20 17:05:23 +0800

    Add jump to buffer functionality.

--- .emacs.d/elisp/jcs-ex/ex-func/jcs-window.el
+++ .emacs.d/elisp/jcs-ex/ex-func/jcs-window.el
@@ -50,7 +50,7 @@ Because sometime first time switching the buffer would not success."
 ;; Author: ChrisDone
 ;;
 (defun jcs-jump-to-window (buffer-name)
-  "Jump to window.
+  "Jump to window if the window is currently shown in the window.
 BUFFER-NAME : buffer name."
   (interactive "bEnter buffer to jump to: ")
   (let ((visible-buffers (mapcar '(lambda (window) (buffer-name (window-buffer window))) (window-list)))
@@ -62,3 +62,28 @@ BUFFER-NAME : buffer name."
                                  (if (equal buffer-name (buffer-name (window-buffer window)))
                                      window nil)) (window-list))))
       (select-window (car window-of-buffer)))))
+
+(defun jcs-jump-to-buffer (in-buffer-name)
+  "Jump to the buffer if the buffer current shown in the window.
+If there is two window shown the same buffer/file, then it will
+choose the one which is close to the next buffer. I think this
+is the better version compare to `jcs-jump-to-window' function."
+  (interactive "bEnter buffer to jump to: ")
+  (let ((win-len (length (window-list)))
+        (index 0)
+        (found nil))
+    (while (< index win-len)
+      ;; NOTE(jenchieh): we use `string-match-p' instead
+      ;; of `string=' because some buffer cannot be detected
+      ;; in the buffer list. For instance, `*undo-tree*' is
+      ;; buffer that cannot be detected for some reason.
+      (if (string-match-p in-buffer-name (buffer-name))
+          (setq found t)
+        (jcs-other-window-next))
+      (setq index (1+ index)))
+
+    ;; If not found, prompt error.
+    (unless found
+      (error "'%s' does not shown in any window" in-buffer-name))
+    ;; Nothing happend return the value.
+    found))
