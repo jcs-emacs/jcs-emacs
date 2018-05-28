35e26ef9c2aa95e2a7abd81011191cac20b4ad3b
Jen-Chieh <jcs090218@gmail.com>
2018-05-28 13:27:55 +0800

    Fixed Lua comment active docstring error.

--- .emacs.jcs/func/jcs-lua-func.el
+++ .emacs.jcs/func/jcs-lua-func.el
@@ -12,7 +12,27 @@
 ;; When editing the Lua related file.
 ;;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-;;; Code:
+(defun jcs-lua-comment-prefix-p ()
+  "Check if current line is a Lua style comment prefix."
+  (jcs-triple-char-comment-prefix-p "-"))
+
+(defun jcs-lua-comment-prefix-at-current-point-p ()
+  "Check if the current point is Lua style comment prefix."
+  (jcs-tripple-char-comment-prefix-at-current-point-p "-"))
+
+(defun jcs-only-lua-comment-prefix-this-line-p ()
+  "Check if there is only comment in this line."
+  (save-excursion
+    (let ((only-comment-this-line nil))
+      (when (jcs-lua-comment-prefix-p)
+        (jcs-goto-first-char-in-line)
+        (forward-char 1)
+        (forward-char 1)
+        (forward-char 1)
+        (when (not (jcs-is-there-char-forward-until-end-of-line-p))
+          (setq only-comment-this-line t)))
+      only-comment-this-line)))
+
 
 (defun jcs-lua-do-doc-string ()
   "Check if should insert the doc string by checking only \
@@ -33,7 +53,6 @@ comment character on the same line."
     ;; return true.
     (equal do-doc-string t)))
 
-
 (defun jcs-lua-maybe-insert-codedoc ()
   "Insert common Lua document/comment string.
 
@@ -45,14 +64,12 @@ URL(jenchieh): http://lua-users.org/wiki/LuaStyleGuide"
   (let ((active-comment nil)
         (next-line-not-empty nil))
     (save-excursion
-      (backward-char 1)
-      (when (jcs-current-char-equal-p "-")
-        (backward-char 1)
-        (when (jcs-current-char-equal-p "-")
-          (backward-char 1)
-          (when (not (jcs-current-char-equal-p "-"))
-            (when (jcs-lua-do-doc-string)
-              (setq active-comment t)))))
+      (when (and
+             ;; Line can only have Lua comment prefix.
+             (jcs-only-lua-comment-prefix-this-line-p)
+             ;; Only enable when `---' at current point.
+             (jcs-lua-comment-prefix-at-current-point-p))
+        (setq active-comment t))
 
       ;; check if next line empty.
       (jcs-next-line)
