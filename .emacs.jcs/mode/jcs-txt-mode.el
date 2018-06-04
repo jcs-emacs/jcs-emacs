b6549e98d4bcc8a585bce32b6184402589db14e0
jenchieh <jcs090218@gmail.com>
2018-06-04 22:58:27 +0800

    Add caseu hook to txt modef file and rename to jcs text hook.

--- .emacs.jcs/mode/jcs-txt-mode.el
+++ .emacs.jcs/mode/jcs-txt-mode.el
@@ -15,6 +15,24 @@
 ;;====================================
 ;; normal text mode
 ;;---------------------------
+
+(defun jcs-big-fun-text-hook ()
+  "4-space tabs."
+  (setq tab-width 4
+        indent-tabs-mode nil)
+
+  ;; Newline indents, semi-colon doesn't
+  (define-key text-mode-map "\C-m" 'newline-and-indent)
+
+  ;; Prevent overriding of alt-s
+  (define-key text-mode-map "\es" 'jcs-untabify-save-buffer)
+  )
+(add-hook 'text-mode-hook 'jcs-big-fun-text-hook)
+
+;;====================================
+;; Gitignore
+;;---------------------------
+
 (require 'gitignore-mode)
 (defun jcs-gitignore-mode-hook ()
   "JayCeS Gitignore mode."
