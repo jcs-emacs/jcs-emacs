8a36d288212ef5f27f3b8137a4fb08f395f46fd5
jenchieh <jcs090218@gmail.com>
2018-06-16 12:30:12 +0800

    Separate helm function to individual helm-func file.

---
+++ .emacs.jcs/func/jcs-helm-func.el
@@ -0,0 +1,90 @@
+;; ========================================================================
+;; $File: jcs-helm-func.el $
+;; $Date: 2018-06-16 12:18:54 $
+;; $Revision: $
+;; $Creator: Jen-Chieh Shen $
+;; $Notice: See LICENSE.txt for modification and distribution information
+;;                   Copyright © 2018 by Shen, Jen-Chieh $
+;; ========================================================================
+
+
+;;;###autoload
+(defun jcs-helm-before-initialize-hook ()
+  "Do the helm mx and change theme"
+  (interactive)
+
+  ;; NOTE(jenchieh): Change theme so we know which mode
+  ;; we are in visually.
+  (jcs-dark-blue-theme))
+
+;;;###autoload
+(defun jcs-helm-gtags-to-def-dec ()
+  "Goto the declaration / definition depends on the cursor position."
+  (interactive)
+
+  (ignore-errors
+    ;; Update TAG file. Default is update only current file, You
+    ;; can update all files with C-u prefix.
+    (helm-gtags-update-tags)
+
+    ;; goto definition or declaration.
+    (helm-gtags-find-tag-from-here))
+
+  (jcs-reload-active-mode))
+
+;;;###autoload
+(defun jcs-helm-gtags-to-def-dec-other-window ()
+  "Goto the declaration / definition depends on the cursor position,
+in other window."
+  (interactive)
+
+  (ignore-errors
+    ;; Update TAG file. Default is update only current file, You
+    ;; can update all files with C-u prefix.
+    (helm-gtags-update-tags)
+
+    ;; NOTE(jenchieh): this will make it jump to next window.
+    ;; Is stupid, but work.
+    (ignore-errors (helm-gtags-find-tag-other-window nil))
+
+    ;; goto definition or declaration.
+    (helm-gtags-find-tag-from-here))
+
+  (jcs-reload-active-mode))
+
+;;;###autoload
+(defun jcs-helm-find-files ()
+  "Find the file with Helm"
+  (interactive)
+
+  (put 'jcs-helm-execute-persistent-action 'state nil)
+
+  (helm-find-files nil))
+
+;;;###autoload
+(defun jcs-helm-find-files-other-window ()
+  "Find the file with Helm and open another window."
+  (interactive)
+
+  ;; set the flag, so when next time run 'jcs-helm-execute-
+  ;; persistent-action', he will know what to do instead of
+  ;; normal 'helm-execute-persistent-action' action.
+  (put 'jcs-helm-execute-persistent-action 'state t)
+
+  (helm-find-files nil))
+
+;;;###autoload
+(defun jcs-helm-execute-persistent-action ()
+  "Rewrap 'helm-execute-presistent-action' function to my
+own preferences."
+  (interactive)
+
+  (if (get 'jcs-helm-execute-persistent-action 'state)
+      (progn
+        ;; switch the buffer to another window
+        (helm-ff-run-switch-other-window)
+        (put 'jcs-helm-execute-persistent-action 'state nil)
+        )
+    ;; NOTE(jenchieh): no longer needed.
+    ;;(helm-execute-persistent-action)
+    ))
