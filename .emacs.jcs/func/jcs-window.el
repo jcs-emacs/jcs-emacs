e3807058ccc6ca2fc2d2dbe3062cf8638c65a63e
jenchieh <jcs090218@gmail.com>
2018-06-16 10:51:38 +0800

    Add few balance window functions and enlarget current selected window function.

--- .emacs.jcs/func/jcs-window.el
+++ .emacs.jcs/func/jcs-window.el
@@ -87,3 +87,88 @@ is the better version compare to `jcs-jump-shown-to-window' function."
       (error "'%s' does not shown in any window" in-buffer-name))
     ;; Nothing happend return the value.
     found))
+
+;;-----------------------------------------------------------
+;; Deleting
+;;-----------------------------------------------------------
+
+;;;###autoload
+(defun jcs-balance-delete-window ()
+  "Balance windows after deleting a window."
+  (interactive)
+  (delete-window)
+  (balance-windows))
+
+;;;###autoload
+(defun jcs-balance-split-window-horizontally ()
+  "Balance windows after split window horizontally."
+  (interactive)
+  (split-window-horizontally)
+  (balance-windows))
+
+;;-----------------------------------------------------------
+;; Splitting
+;;-----------------------------------------------------------
+
+(defvar jcs-is-enlarge-buffer nil
+  "Is any buffer in the frame enlarge already?")
+
+(defvar-local jcs-is-enlarge-current-buffer nil
+  "Is the current buffer enlarge already?")
+
+;;;###autoload
+(defun jcs-toggle-enlarge-window-selected ()
+  "Toggle between show the whole buffer and current window state."
+  (interactive)
+  (let ((delta-win-size 1000))
+    (if (and jcs-is-enlarge-current-buffer
+             jcs-is-enlarge-buffer)
+        (progn
+          (balance-windows)
+          (setq jcs-is-enlarge-buffer nil))
+      (progn
+        (enlarge-window delta-win-size)               ;; Vertical enlarge
+        (enlarge-window-horizontally delta-win-size)  ;; Horizontal enlarge
+
+        ;; Set all local enlarge to false.
+        (jcs-setq-all-local-buffer 'jcs-is-enlarge-current-buffer
+                                   nil)
+
+        ;; Current buffer is enlarge.
+        (setq-local jcs-is-enlarge-current-buffer t)
+
+        ;; One buffer in the frame is enlarge.
+        (setq jcs-is-enlarge-buffer t)))))
+
+
+(require 'windmove)
+
+;;;###autoload
+(defun jcs-toggle-window-split-hv ()
+  "Switch window split from horizontally to vertically, or vice versa.
+
+i.e. change right window to bottom, or change bottom window to right."
+  (interactive)
+
+  ;; TOPIC: Toggle Window Split
+  ;; URL: https://www.emacswiki.org/emacs/ToggleWindowSplit
+  (let ((done))
+    (dolist (dirs '((right . down) (down . right)))
+      (unless done
+        (let* ((win (selected-window))
+               (nextdir (car dirs))
+               (neighbour-dir (cdr dirs))
+               (next-win (windmove-find-other-window nextdir win))
+               (neighbour1 (windmove-find-other-window neighbour-dir win))
+               (neighbour2 (if next-win (with-selected-window next-win
+                                          (windmove-find-other-window neighbour-dir next-win)))))
+          ;;(message "win: %s\nnext-win: %s\nneighbour1: %s\nneighbour2:%s" win next-win neighbour1 neighbour2)
+          (setq done (and (eq neighbour1 neighbour2)
+                          (not (eq (minibuffer-window) next-win))))
+          (if done
+              (let* ((other-buf (window-buffer next-win)))
+                (delete-window next-win)
+                (if (eq nextdir 'right)
+                    (split-window-vertically)
+                  (split-window-horizontally))
+                (set-window-buffer (windmove-find-other-window neighbour-dir) other-buf))))))))
