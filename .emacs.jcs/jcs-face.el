c9fd8842f1e0245ee5d2dfed60553a3a3a50b1b0
jenchieh <jcs090218@gmail.com>
2018-06-22 12:16:11 +0800

    Add double dash comment font lock face.

--- .emacs.jcs/jcs-face.el
+++ .emacs.jcs/jcs-face.el
@@ -25,3 +25,28 @@
   "JCS local preprocessor face."
   :group 'basic-faces)
 (defvar jcs-font-lock-preprocessor-face 'jcs-font-lock-preprocessor-face)
+
+
+;;----------------------------------------------
+;; Double Dash Comment Face
+;;----------------------------------------------
+
+(defface jcs-double-dash-comment-face
+  '((t (:inherit jcs-font-lock-comment-face)))
+  "Comment face for double dash."
+  :group 'comment-faces)
+(defvar jcs-double-dash-comment-face 'jcs-double-dash-comment-face)
+
+
+(defvar jcs-double-dash-comment-missing-modes '(haskell-mode
+                                                lua-mode)
+  "Modes that does not apply comment with double dash style.")
+
+
+(mapc (lambda (mode)
+        (font-lock-add-keywords
+         mode
+         '(;; Only single line command.
+           ("\\(--[a-zA-Z0-9\t -.<>?,*'`@\"=_(){}:;&^%$#!~©]*\\)" 1 'jcs-double-dash-comment-face t)
+           )'end))
+      jcs-double-dash-comment-missing-modes)
