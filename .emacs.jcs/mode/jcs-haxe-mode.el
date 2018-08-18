71cdb009062ca2ceea16d39110f091fb88a764b7
jenchieh <jcs090218@gmail.com>
2018-08-18 18:43:39 +0800

    Start support haxe file. Add it own mode.

---
+++ .emacs.jcs/mode/jcs-haxe-mode.el
@@ -0,0 +1,49 @@
+;; ========================================================================
+;; $File: jcs-haxe-mode.el $
+;; $Date: 2018-08-18 18:22:41 $
+;; $Revision: $
+;; $Creator: Jen-Chieh Shen $
+;; $Notice: See LICENSE.txt for modification and distribution information
+;;                   Copyright © 2018 by Shen, Jen-Chieh $
+;; ========================================================================
+
+
+(require 'haxe-mode)
+(defun jcs-haxe-mode-hook ()
+  "Haxe mode hook."
+
+  ;; Abbrevation expansion
+  (abbrev-mode 1)
+
+  ;; enable the stuff you want for Lua here
+  (electric-pair-mode 1)
+
+  ;; highlight URL and clickable.
+  (goto-address-mode 1)
+
+  ;; Auto highlight the same word.
+  (auto-highlight-symbol-mode t)
+
+  ;; TOPIC(jenchieh): Treat underscore as word.
+  ;; URL(jenchieh): https://emacs.stackexchange.com/questions/9583/how-to-treat-underscore-as-part-of-the-word
+  (modify-syntax-entry ?_ "w")
+
+  (defun jcs-haxe-script-format ()
+    "Format the given file as a class. - JenChieh Haxe Script"
+    (when (jcs-is-current-file-empty-p)
+      (jcs-insert-haxe-template)))
+
+  (cond ((file-exists-p buffer-file-name) t)
+        ((string-match "[.]hx" buffer-file-name) (jcs-haxe-script-format))
+        )
+
+  ;; Set Faces.
+  (jcs-oop-init-set-face)
+
+  ;; jcs Haxe key binding
+  (define-key haxe-mode-map (kbd "C-d") 'jcs-kill-whole-line)
+  (define-key haxe-mode-map "\C-c\C-c" 'kill-ring-save)
+  )
+(add-hook 'haxe-mode-hook 'jcs-haxe-mode-hook)
+
+(add-to-list 'auto-mode-alist '("\\.hx?\\'" . haxe-mode))
