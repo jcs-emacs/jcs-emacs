a32448f53d61046b7c26d8cf13b26f6504abc243
jenchieh <jcs090218@gmail.com>
2018-09-27 21:39:27 +0800

    Change default-directory when compile a script!

--- .emacs.jcs/jcs-dev.el
+++ .emacs.jcs/jcs-dev.el
@@ -15,8 +15,26 @@
 (defun jcs-compile (in-op)
   "Compile command rewrapper.
 IN-OP : inpuit operation script."
-  (compile in-op)
-  (global-linum-mode t))
+  ;; NOTE(jenchieh): First we need to get the script directory. In order
+  ;; to change execute/workspace directory to the current target script's
+  ;; directory path.
+  (let ((script-dir (f-dirname in-op)))
+    ;; NOTE(jenchieh): Change the current execute/workspace directory
+    ;; to the script directory temporary. So the script will execute
+    ;; within the current directory the script is currently in.
+    ;;
+    ;; Without these lines of code, the script will execute in the
+    ;; `default-directory' variables. The `default-directory' variables
+    ;; will be the directory path where you start the Emacs. For instance,
+    ;; if you start Emacs at path `/usr/home', then the default directory
+    ;; will be at `usr/home' directory.
+    ;;
+    ;; Adding these lines of code if your scirpt is at `/usr/home/project/some-script.sh',
+    ;; Then your `default-directory' became `usr/home/project'. Hurray!
+    (let ((default-directory script-dir))
+      ;; Compile/Execute the target script.
+      (compile in-op)
+      (global-linum-mode t))))
 
 
 (defun jcs-select-find-file-current-dir (in-filename)
