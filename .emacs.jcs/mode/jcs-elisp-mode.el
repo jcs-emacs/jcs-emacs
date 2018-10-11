06e495c07aed3453b405752b213789f4fb84262e
jenchieh <jcs090218@gmail.com>
2018-10-11 23:27:05 +0800

    Remove outdated comment.

--- .emacs.jcs/mode/jcs-elisp-mode.el
+++ .emacs.jcs/mode/jcs-elisp-mode.el
@@ -27,10 +27,8 @@
   ;; Auto highlight the same word.
   (auto-highlight-symbol-mode t)
 
-  ;; TOPIC(jenchieh): Treat underscore as word.
-  ;; URL(jenchieh): https://emacs.stackexchange.com/questions/9583/how-to-treat-underscore-as-part-of-the-word
+  ;; Treat underscore as word.
   (modify-syntax-entry ?_ "w")
-
 
   (defun jcs-emacs-lisp-format ()
     "Format the given file as a Emacs Lisp file. - JenChieh emacs lisp file."
@@ -51,4 +49,4 @@
   )
 (add-hook 'emacs-lisp-mode-hook 'jcs-emacs-lisp-mode-hook)
 
-(add-to-list 'auto-mode-alist '("\\.el?\\'" . emacs-lisp-mode))
+(add-to-list 'auto-mode-alist '("\\.el'?\\'" . emacs-lisp-mode))
