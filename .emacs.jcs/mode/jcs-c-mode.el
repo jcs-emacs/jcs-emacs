e10f88e8999534301b87ad0fb6f6e1882936787b
jenchieh <jcs090218@gmail.com>
2018-10-11 22:46:39 +0800

    Remove indentation key bindings.

--- .emacs.jcs/mode/jcs-c-mode.el
+++ .emacs.jcs/mode/jcs-c-mode.el
@@ -41,12 +41,6 @@
   (define-key c-mode-map "\ec" #'jcs-find-corresponding-file)
   (define-key c-mode-map "\eC" #'jcs-find-corresponding-file-other-window)
 
-  (define-key c-mode-map "\t" #'dabbrev-expand)
-  (define-key c-mode-map [S-tab] #'indent-for-tab-command)
-  (define-key c-mode-map "\C-y" #'indent-for-tab-command)
-  (define-key c-mode-map [C-tab] #'indent-region)
-  (define-key c-mode-map "  " #'indent-region)
-
   (define-key c-mode-map "\ej" #'imenu)
 
   (define-key c-mode-map "\e." #'c-fill-paragraph)
