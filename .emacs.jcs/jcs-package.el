0f613f4b354927fe64be98e57ef37db18b8b0190
jenchieh <jcs090218@gmail.com>
2018-11-25 21:08:22 +0800

    Move ensure install package util function to here.

--- .emacs.jcs/jcs-package.el
+++ .emacs.jcs/jcs-package.el
@@ -8,6 +8,144 @@
 ;; ========================================================================
 
 
+;; Ensure all the package installed
+;; SOURCE: http://stackoverflow.com/questions/10092322/how-to-automatically-install-emacs-packages-by-specifying-a-list-of-package-name
+(defun jcs-ensure-package-installed (packages &optional without-asking)
+  "Assure every package is installed, ask for installation if it’s not.
+
+Return a list of installed packages or nil for every skipped package."
+  (mapcar
+   (lambda (package)
+     ;; (package-installed-p 'evil)
+     (if (package-installed-p package)
+         nil
+       (if without-asking
+           (package-install package)
+         (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+             (package-install package)
+           package))))
+   packages))
+
+
+;; List of package you want to installed.
+(defvar jcs-package-install-list '(ac-emmet
+                                   ac-html            ;; auto complete html
+                                   ac-js2
+                                   ac-php             ;; auto complete php
+                                   ace-window
+                                   actionscript-mode
+                                   adaptive-wrap
+                                   ag
+                                   all-the-icons
+                                   apache-mode
+                                   auto-complete
+                                   auto-complete-c-headers
+                                   auto-highlight-symbol
+                                   auto-package-update
+                                   basic-mode
+                                   better-defaults
+                                   clojure-mode
+                                   cmake-font-lock
+                                   cmake-ide
+                                   cmake-mode
+                                   cmake-project
+                                   cobol-mode
+                                   com-css-sort
+                                   company
+                                   cpputils-cmake
+                                   csharp-mode
+                                   dash
+                                   diminish
+                                   ein
+                                   elpy
+                                   emmet-mode
+                                   exec-path-from-shell
+                                   find-file-in-project
+                                   floobits
+                                   flycheck
+                                   flymake-cursor
+                                   flymake-easy
+                                   flymake-google-cpplint
+                                   git-link
+                                   git-messenger
+                                   git-timemachine
+                                   gitattributes-mode
+                                   gitconfig-mode
+                                   github-notifier
+                                   gitignore-mode
+                                   gitlab
+                                   go-mode
+                                   google-c-style
+                                   google-maps
+                                   google-this
+                                   google-translate
+                                   haskell-mode
+                                   helm
+                                   helm-ag
+                                   helm-gtags
+                                   javadoc-lookup
+                                   ;;;
+                                   ;; TEMPORARY(jenchieh): Hopefully melpa will let me push
+                                   ;; my package `jayces-mode' to their package system.
+                                   ;; Then we can add this line under directly.
+                                   ;;
+                                   ;;jayces-mode
+                                   ;;;
+                                   ;; TEMPORARY(jenchieh): Hopefully melpa will let me push
+                                   ;; my package `jcs-ex-pkg' to their package system.
+                                   ;; Then we can add this line under directly.
+                                   ;;
+                                   ;;jcs-ex-pkg
+                                   jdee
+                                   js2-mode
+                                   js2-refactor
+                                   json-mode
+                                   line-reminder
+                                   lua-mode
+                                   magit
+                                   markdown-mode
+                                   markdown-preview-mode
+                                   meghanada
+                                   multiple-cursors
+                                   nasm-mode
+                                   nhexl-mode
+                                   organize-imports-java
+                                   package-build
+                                   package-lint
+                                   pdf-tools
+                                   php-auto-yasnippets
+                                   powerline
+                                   processing-mode
+                                   project-abbrev
+                                   py-autopep8
+                                   python-mode
+                                   rainbow-mode
+                                   rust-mode
+                                   scala-mode
+                                   shader-mode
+                                   sr-speedbar
+                                   ssass-mode
+                                   scss-mode
+                                   sublimity
+                                   sql-indent
+                                   tabbar
+                                   togetherly
+                                   typescript-mode
+                                   undo-tree
+                                   use-package
+                                   use-ttf
+                                   vimrc-mode
+                                   visual-regexp
+                                   impatient-mode
+                                   web-mode
+                                   which-key
+                                   wgrep-ag
+                                   wgrep-helm
+                                   xwidgete
+                                   yasnippet)
+  "List of packages this config needs.")
+
+
 ;;;###autoload
 (defun package-upgrade-all ()
   "Upgrade all packages automatically without showing *Packages* buffer."
