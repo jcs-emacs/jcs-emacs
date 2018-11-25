098ce17b7674c8d4e0862291878d7f0e7f553fa0
jenchieh <jcs090218@gmail.com>
2018-11-25 20:20:49 +0800

    Update test startup.

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -20,7 +20,7 @@ ${EMACS:=emacs} -nw --batch \
                 --eval '(let ((debug-on-error t)
                               (url-show-status nil)
                               (user-emacs-directory default-directory)
-                              (user-init-file (expand-file-name "init.el"))
+                              (user-init-file (expand-file-name ".emacs"))
                               (load-path (delq default-directory load-path)))
                            (load-file user-init-file)
                            (run-hooks (quote after-init-hook)))'
