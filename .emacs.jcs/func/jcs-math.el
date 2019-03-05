54f53f6a72eb46c32d7553ad9bf960b4032b799e
jenchieh <jcs090218@gmail.com>
2019-03-05 22:28:20 +0800

    Add to pos and to neg util functions.

--- .emacs.jcs/func/jcs-math.el
+++ .emacs.jcs/func/jcs-math.el
@@ -8,11 +8,27 @@
 ;; ========================================================================
 
 
+(defun jcs-to-positive (in-val)
+  "To positive value.
+IN-VAL : Input value to set to positive."
+  (when (and in-val
+             (< in-val 0))
+    (setq in-val (- 0 in-val)))
+  in-val)
+
+(defun jcs-to-negative (in-val)
+  "To negative value.
+IN-VAL : Input value to set to negative."
+  (when (and in-val
+             (> in-val 0))
+    (setq in-val (- 0 in-val)))
+  in-val)
+
 (defun jcs-clamp-integer (in-val in-min in-max)
   "Make sure the value in the range.
-IN-VAL : input value.
-IN-MIN : input minimum value.
-IN-MAX : input maximum value"
+IN-VAL : Input value.
+IN-MIN : Input minimum value.
+IN-MAX : Input maximum value."
   (let ((out-result in-val))
     (cond ((<= in-val in-min) (progn (setq out-result in-min)))
           ((>= in-val in-max) (progn (setq out-result in-max))))
