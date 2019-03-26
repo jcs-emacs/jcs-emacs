3b71c0f4638ca194ae2fdc380e79f4a96d6f129a
jenchieh <jcs090218@gmail.com>
2019-03-26 17:59:03 +0800

    Move jcs-helm.el function to jcs-helm-func.el.

--- .emacs.jcs/func/jcs-helm-func.el
+++ .emacs.jcs/func/jcs-helm-func.el
@@ -3,14 +3,48 @@
 ;;; Code:
 
 
-;;;###autoload
 (defun jcs-helm-before-initialize-hook ()
   "Do the helm mx and change theme"
-  (interactive)
-
   ;; NOTE(jenchieh): Change theme so we know which mode
   ;; we are in visually.
   (jcs-dark-blue-theme))
+(add-hook 'helm-before-initialize-hook 'jcs-helm-before-initialize-hook)
+
+
+;; TOPIC(jenchieh): How do I make pressing <RET> in helm-find-files open the directory?
+;; SOURCE(jenchieh): http://emacs.stackexchange.com/questions/3798/how-do-i-make-pressing-ret-in-helm-find-files-open-the-directory
+
+(defun jcs-helm-find-files-navigate-forward (orig-fun &rest args)
+  (if (and (equal "Find Files" (assoc-default 'name (helm-get-current-source)))
+           (equal args nil)
+           (stringp (helm-get-selection))
+           (not (file-directory-p (helm-get-selection))))
+      (progn
+        (jcs-helm-execute-persistent-action)
+        (helm-maybe-exit-minibuffer))
+    (apply orig-fun args)))
+(advice-add 'helm-execute-persistent-action :around #'jcs-helm-find-files-navigate-forward)
+
+(defun jcs-helm-find-files-navigate-back (orig-fun &rest args)
+  (if (looking-back "/")
+      (helm-find-files-up-one-level 1)
+    (apply orig-fun args)))
+(advice-add 'helm-ff-delete-char-backward :around #'jcs-helm-find-files-navigate-back)
+
+
+;;;
+;; `helm-find-files-hook'
+;;
+(defvar jcs-helm-find-files-active nil
+  "Helm find file flag.")
+
+(defun jcs-helm-find-files-hook ()
+  "Hook after `helm-find-files' initialized."
+  ;; SEE(jenchieh): `jcs-global-key.el' file, and `minibuffer-setup-hook'.
+  (setq jcs-helm-find-files-active t)
+  )
+(add-hook 'helm-find-files-after-init-hook 'jcs-helm-find-files-hook)
+
 
 ;;;###autoload
 (defun jcs-helm-gtags-to-def-dec ()
