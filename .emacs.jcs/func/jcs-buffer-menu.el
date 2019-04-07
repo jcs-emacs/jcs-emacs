8c2036a495138a432983a77e43b13f6f46077cfa
jenchieh <jcs090218@gmail.com>
2019-04-07 13:24:11 +0800

    Add autoload prefix.

--- .emacs.jcs/func/jcs-buffer-menu.el
+++ .emacs.jcs/func/jcs-buffer-menu.el
@@ -19,36 +19,43 @@
 ;; TOPIC(jenchieh): BufferMenuPlus
 ;; URL(jenchieh): https://www.emacswiki.org/emacs/BufferMenuPlus
 ;; Sorted by (1) visit, (2) buffer, (3) size, (4) time, (5) mode, (6) file. More
+
+;;;###autoload
 (defun jcs-buffer-menu-sort-by-visit ()
   "Sort the Buffer Menu List by visit."
   (interactive)
   (Buffer-menu-sort 1)
   (goto-char (point-min)))
 
+;;;###autoload
 (defun jcs-buffer-menu-sort-by-buffer ()
   "Sort the Buffer Menu List by buffer."
   (interactive)
   (Buffer-menu-sort 2)
   (goto-char (point-min)))
 
+;;;###autoload
 (defun jcs-buffer-menu-sort-by-size ()
   "Sort the Buffer Menu List by size."
   (interactive)
   (Buffer-menu-sort 3)
   (goto-char (point-min)))
 
+;;;###autoload
 (defun jcs-buffer-menu-sort-by-time ()
   "Sort the Buffer Menu List by time."
   (interactive)
   (Buffer-menu-sort 4)
   (goto-char (point-min)))
 
+;;;###autoload
 (defun jcs-buffer-menu-sort-by-mode ()
   "Sort the Buffer Menu List by mode."
   (interactive)
   (Buffer-menu-sort 5)
   (goto-char (point-min)))
 
+;;;###autoload
 (defun jcs-buffer-menu-sort-by-file ()
   "Sort the Buffer Menu List by file name."
   (interactive)
