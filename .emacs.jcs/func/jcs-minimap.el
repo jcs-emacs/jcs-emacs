3736dcea197697ed304c91ce20e501c23828a3f4
jenchieh <jcs090218@gmail.com>
2019-04-08 17:03:15 +0800

    Fixed minimap toggle key.

--- .emacs.jcs/func/jcs-minimap.el
+++ .emacs.jcs/func/jcs-minimap.el
@@ -7,20 +7,20 @@
 (defun jcs-toggle-minimap ()
   "Toggle minimap. (sublimity)"
   (interactive)
-
-  (if (get 'jcs-toggle-minimap 'state)
+  ;; NOTE(jenchieh): Only when `sublimity-mode' is on.
+  (when sublimity-mode
+    (if (get 'jcs-toggle-minimap 'state)
+        (progn
+          (setq sublimity-map-size 0)
+          ;; ATTENTION(jenchieh): Set it to very hight so it
+          ;; will never reach the timer error.
+          (sublimity-map-set-delay 40000000)
+          (put 'jcs-toggle-minimap 'state nil))
       (progn
-        (setq sublimity-map-size 0)
-        ;; ATTENTION(jenchieh): Set it to very hight so it
-        ;; will never reach the timer error.
-        (sublimity-map-set-delay 40000000)
-        (put 'jcs-toggle-minimap 'state nil))
-    (progn
-      (setq sublimity-map-size 10)
-      ;; NOTE: Set it to nil cost too many performanc...
-      (sublimity-map-set-delay 0)
-      (put 'jcs-toggle-minimap 'state t)))
-  )
+        (setq sublimity-map-size 10)
+        ;; NOTE: Set it to nil cost too many performanc...
+        (sublimity-map-set-delay 0)
+        (put 'jcs-toggle-minimap 'state t)))))
 
 
 (provide 'jcs-minimap)
