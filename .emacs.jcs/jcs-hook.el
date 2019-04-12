31d45d272d398812bb3301bd39509d84889ced1f
jenchieh <jcs090218@gmail.com>
2019-04-12 18:46:12 +0800

    Try reactive flycheck after revert file.

--- .emacs.jcs/jcs-hook.el
+++ .emacs.jcs/jcs-hook.el
@@ -14,6 +14,8 @@
 (defun jcs-focus-in-hook ()
   "When window is focus."
   (jcs-revert-all-file-buffers)
+
+  (jcs-reactive-flycheck-after-revert)
   )
 (add-hook 'focus-in-hook 'jcs-focus-in-hook)
 
