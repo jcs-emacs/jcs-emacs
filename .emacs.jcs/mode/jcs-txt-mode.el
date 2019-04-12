f0793e1275988e85864af0d6c13e0fb2f6705180
jenchieh <jcs090218@gmail.com>
2019-04-13 00:03:12 +0800

    Sort txt related mode hook order.

--- .emacs.jcs/mode/jcs-txt-mode.el
+++ .emacs.jcs/mode/jcs-txt-mode.el
@@ -3,6 +3,25 @@
 ;;; Code:
 
 
+(require 'gitattributes-mode)
+(defun jcs-gitattributes-mode-hook ()
+  "Gitattributes mode hook."
+  (electric-pair-mode nil)
+  (goto-address-mode 1)
+  (auto-highlight-symbol-mode t)
+
+  ;; Normal
+  (define-key gitattributes-mode-map (kbd "<up>") 'previous-line)
+  (define-key gitattributes-mode-map (kbd "<down>") 'next-line)
+  (define-key gitattributes-mode-map (kbd "C-d") 'jcs-kill-whole-line)
+  (define-key gitattributes-mode-map "\C-c\C-c" 'kill-ring-save)
+  (define-key gitattributes-mode-map (kbd "<up>") 'previous-line)
+  (define-key gitattributes-mode-map (kbd "<down>") 'next-line)
+  )
+(add-hook 'gitattributes-mode-hook 'jcs-gitattributes-mode-hook)
+
+
+
 (require 'gitconfig-mode)
 (defun jcs-gitconfig-mode-hook ()
   "Gitconfig mode hook."
@@ -38,25 +57,6 @@
   (define-key gitignore-mode-map (kbd "<down>") 'next-line)
   )
 (add-hook 'gitignore-mode-hook 'jcs-gitignore-mode-hook)
-
-
-
-(require 'gitattributes-mode)
-(defun jcs-gitattributes-mode-hook ()
-  "Gitattributes mode hook."
-  (electric-pair-mode nil)
-  (goto-address-mode 1)
-  (auto-highlight-symbol-mode t)
-
-  ;; Normal
-  (define-key gitattributes-mode-map (kbd "<up>") 'previous-line)
-  (define-key gitattributes-mode-map (kbd "<down>") 'next-line)
-  (define-key gitattributes-mode-map (kbd "C-d") 'jcs-kill-whole-line)
-  (define-key gitattributes-mode-map "\C-c\C-c" 'kill-ring-save)
-  (define-key gitattributes-mode-map (kbd "<up>") 'previous-line)
-  (define-key gitattributes-mode-map (kbd "<down>") 'next-line)
-  )
-(add-hook 'gitattributes-mode-hook 'jcs-gitattributes-mode-hook)
 
 
 
