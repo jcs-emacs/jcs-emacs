bc323b5c4bc2a550176a7e6c22c007fe49f0220f
jenchieh <jcs090218@gmail.com>
2019-04-12 21:23:11 +0800

    Sort plugin into alphabetic order.

--- .emacs.jcs/jcs-plugin.el
+++ .emacs.jcs/jcs-plugin.el
@@ -8,6 +8,39 @@
   (require 'use-package))
 
 
+(use-package auto-highlight-symbol
+  :ensure t
+  :config
+  (global-auto-highlight-symbol-mode t)
+
+  ;; Current highlight. (Cursor point currently on.)
+  (custom-set-faces
+   '(ahs-plugin-defalt-face ((t (:foreground nil :background "#123E70"))))
+   '(ahs-face ((t (:foreground nil :background "#113D6F"))))
+   '(ahs-definition-face ((t (:foreground nil :background "#113D6F"))))
+   )
+
+  ;; Current highlight. (Cursor point currently on.)
+  (set-face-attribute 'ahs-plugin-defalt-face nil
+                      :box '(:line-width -1 :color "#525D68" :style pressed-button)
+                      ;;:underline nil
+                      )
+
+  ;; Other highlight. (Same words in the buffer)
+  (set-face-attribute 'ahs-face nil
+                      :box '(:line-width -1 :color "#525D68" :style pressed-button)
+                      ;;:underline nil
+                      )
+
+  (set-face-attribute 'ahs-definition-face nil
+                      :box '(:line-width -1 :color "#525D68" :style pressed-button)
+                      ;;:underline nil
+                      )
+
+  ;; Number of seconds to wait before highlighting symbol.
+  (custom-set-variables '(ahs-idle-interval 0.3)))
+
+
 (use-package beacon
   :ensure t
   :config
@@ -65,16 +98,39 @@
   (global-company-mode t))
 
 
-(use-package flycheck
+(use-package dashboard
+  :ensure t
+  :defer t
+  :config
+  (setq dashboard-banner-logo-title "Welcome to J-Emacs!")
+  (setq dashboard-startup-banner "~/.emacs.jcs/banner/sink_white.png")
+  (setq dashboard-items '((recents  . 10)
+                          (bookmarks . 10)))
+
+  (custom-set-faces
+   '(dashboard-banner-logo-title
+     ((t (:foreground "cyan1"))))
+   '(dashboard-heading
+     ((t (:foreground "#17A0FB"))))
+   '(widget-button
+     ((t (:foreground "light steel blue")))))
+
+  (dashboard-setup-startup-hook))
+
+
+(use-package dimmer
+  :ensure t
+  :defer t
+  :config
+  (dimmer-mode)
+  (setq dimmer-fraction 0.2))
+
+
+(use-package exec-path-from-shell
   :ensure t
   :config
-  (use-package flycheck-popup-tip
-    :ensure t
-    :config
-    ;;(flycheck-popup-tip-mode t)
-    )
-  ;;(global-flycheck-mode t)
-  )
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
 
 
 (use-package find-file-in-project
@@ -93,228 +149,16 @@
   )
 
 
-(use-package sublimity
+(use-package flycheck
   :ensure t
-  :defer t
   :config
-  (require 'sublimity-scroll)
-  (require 'sublimity-map)
-  (require 'sublimity-attractive)
-
-  ;; default on or off?
-  ;; NOTE(jenchieh): This also trigger the animate scrolling too.
-  (sublimity-mode 1)
-
-  ;; Scroll Speed.
-  (setq sublimity-scroll-weight 2  ;; [Default : 2]
-        sublimity-scroll-drift-length 2)  ;; [Default : 2]
-
-
-  (setq sublimity-map-size 0)  ;; [Default : 10]
-  (setq sublimity-map-fraction 0.3)  ;; [Default : 0.3]
-  (setq sublimity-map-text-scale -7)  ;; [Default: -7]
-
-  ;; NOTE(jenchieh): When a positive integer is set, buffer
-  ;; width is truncated to this value and drawn centered. To
-  ;; cancel this feature, set this value nil.
-  (setq sublimity-attractive-centering-width nil)  ;; [Default : 110]
-
-  ;; NOTE(jenchieh): With the setting above, minimap is displayed
-  ;; after 5 seconds of idle time. When sublimity-map-set-delay
-  ;; is called with nil, then minimap is shown with no delay. This
-  ;; defers from setting delay to 0, especially when used with
-  ;; sublimity-scroll, in the sense that minimap looks not deleted
-  ;; at all but gets worse performance.
-
-  ;; ATTENTION(jenchieh): Set it to very hight so it will never
-  ;; reach the timer error.
-  (sublimity-map-set-delay 40000000)
-
-  ;; NOTE(jenchieh): sublimity-map-setup-hook will run when
-  ;; minimap is created.
-  (add-hook 'sublimity-map-setup-hook
-            (lambda ()
-              (setq buffer-face-mode-face '(:family "Monospace"))
-              (buffer-face-mode)))
-
-  ;; NOTE(jenchieh): Following functions are available to hide
-  ;; some UI parts.
-  ;;(sublimity-attractive-hide-bars)
-  ;;(sublimity-attractive-hide-vertical-border)
-  ;;(sublimity-attractive-hide-fringes)
-  ;;(sublimity-attractive-hide-modelines)
+  (use-package flycheck-popup-tip
+    :ensure t
+    :config
+    ;;(flycheck-popup-tip-mode t)
+    )
+  ;;(global-flycheck-mode t)
   )
-
-
-;;; Preprocessor/Marcos highlight.
-(use-package preproc-font-lock
-  :ensure t
-  :config
-  (preproc-font-lock-global-mode t)
-  (preproc-font-lock-mode t)
-  (set-face-attribute 'preproc-font-lock-preprocessor-background
-                      nil
-                      :background "#333333"
-                      :inherit nil))
-
-
-(use-package whitespace
-  :config
-  (autoload 'whitespace-mode "whitespace-mode" "Toggle whitespace visualization." t)
-  (autoload 'whitespace-toggle-options "whitespace-mode" "Toggle local `whitespace-mode' options." t)
-  ;; All the face can be find here.
-  ;; URL: https://www.emacswiki.org/emacs/BlankMode
-  (set-face-attribute 'whitespace-indentation
-                      nil
-                      :background "grey20"
-                      :foreground "aquamarine3")
-  (set-face-attribute 'whitespace-trailing
-                      nil
-                      :background "grey20"
-                      :foreground "red"))
-
-
-(use-package sr-speedbar
-  :ensure t
-  :config
-  ;;(setq sr-speedbar-auto-refresh nil)
-  (setq speedbar-show-unknown-files t) ; show all files
-  (setq speedbar-use-images nil) ; use text for buttons
-  ;;(setq sr-speedbar-right-side nil) ; put on left side
-  )
-
-
-(use-package exec-path-from-shell
-  :ensure t
-  :config
-  (when (memq window-system '(mac ns x))
-    (exec-path-from-shell-initialize)))
-
-
-(use-package wgrep
-  :ensure t
-  :config
-  (setq wgrep-auto-save-buffer t))
-
-
-(use-package powerline
-  :ensure t
-  :config
-  (powerline-default-theme)
-  ;;(powerline-center-theme)
-  ;;(powerline-center-evil-theme)
-  ;;(powerline-vim-theme)
-  ;;(powerline-nano-theme)
-
-  ;; NOTE(jenchieh):
-  ;; The separator to use for the default theme.
-  ;; Valid Values: alternate, arrow, arrow-fade, bar, box, brace,
-  ;; butt, chamfer, contour, curve, rounded, roundstub, wave, zigzag,
-  ;; utf-8.
-  (setq powerline-default-separator 'wave))
-
-
-(use-package auto-highlight-symbol
-  :ensure t
-  :config
-  (global-auto-highlight-symbol-mode t)
-
-  ;; Current highlight. (Cursor point currently on.)
-  (custom-set-faces
-   '(ahs-plugin-defalt-face ((t (:foreground nil :background "#123E70"))))
-   '(ahs-face ((t (:foreground nil :background "#113D6F"))))
-   '(ahs-definition-face ((t (:foreground nil :background "#113D6F"))))
-   )
-
-  ;; Current highlight. (Cursor point currently on.)
-  (set-face-attribute 'ahs-plugin-defalt-face nil
-                      :box '(:line-width -1 :color "#525D68" :style pressed-button)
-                      ;;:underline nil
-                      )
-
-  ;; Other highlight. (Same words in the buffer)
-  (set-face-attribute 'ahs-face nil
-                      :box '(:line-width -1 :color "#525D68" :style pressed-button)
-                      ;;:underline nil
-                      )
-
-  (set-face-attribute 'ahs-definition-face nil
-                      :box '(:line-width -1 :color "#525D68" :style pressed-button)
-                      ;;:underline nil
-                      )
-
-  ;; Number of seconds to wait before highlighting symbol.
-  (custom-set-variables '(ahs-idle-interval 0.3)))
-
-
-(use-package visual-regexp
-  :ensure t
-  :defer t)
-
-
-(use-package which-key
-  :ensure t
-  :config
-  (which-key-mode)
-
-  ;; Provide following type: `minibuffer', `side-window', `frame'.
-  (setq which-key-popup-type 'side-window)
-
-  ;; location of which-key window. valid values: top, bottom, left, right,
-  ;; or a list of any of the two. If it's a list, which-key will always try
-  ;; the first location first. It will go to the second location if there is
-  ;; not enough room to display any keys in the first location
-  (setq which-key-side-window-location 'bottom)
-
-  ;; max width of which-key window, when displayed at left or right.
-  ;; valid values: number of columns (integer), or percentage out of current
-  ;; frame's width (float larger than 0 and smaller than 1)
-  (setq which-key-side-window-max-width 0.33)
-
-  ;; max height of which-key window, when displayed at top or bottom.
-  ;; valid values: number of lines (integer), or percentage out of current
-  ;; frame's height (float larger than 0 and smaller than 1)
-  (setq which-key-side-window-max-height 0.25)
-
-  ;; Set the time delay (in seconds) for the which-key popup to appear. A value of
-  ;; zero might cause issues so a non-zero value is recommended.
-  (setq which-key-idle-delay 1.0))
-
-
-(use-package undo-tree
-  :ensure t
-  :defer t
-  :config
-  ;; Enable `undo-tree' as default.
-  (global-undo-tree-mode t))
-
-
-(use-package line-reminder
-  :ensure t
-  :config
-  (global-line-reminder-mode t))
-
-
-(use-package tabbar
-  :ensure t
-  :defer t
-  :config
-  ;; Turn-off `tabbar-mode' as default.
-  (tabbar-mode 0)  )
-
-
-(use-package indent-info
-  :ensure t
-  :defer t
-  :config
-  (global-indent-info-mode +1))
-
-
-(use-package right-click-context
-  :ensure t
-  :defer t
-  :config
-  (right-click-context-mode 1))
 
 
 (use-package goto-line-preview
@@ -325,37 +169,6 @@
     "Advice after execute `goto-line-preview' command."
     (call-interactively #'recenter))
   (advice-add 'goto-line-preview :after #'jcs-advice-goto-line-preview-after))
-
-
-(use-package dimmer
-  :ensure t
-  :defer t
-  :config
-  (dimmer-mode)
-  (setq dimmer-fraction 0.2))
-
-
-(use-package reload-emacs
-  :config
-  (defun jcs-advice-reload-emacs-after ()
-    "Advice after execute `reload-emacs' command."
-    ;; Split window horizontally if full width.
-    (when (and (window-full-width-p)
-               (= (length (window-list)) 1))
-      (jcs-balance-split-window-horizontally))
-
-    ;; Restore to what ever state it was.
-    ;;
-    ;; NOTE(jenchieh): we need these two lines because we need it
-    ;; for solving after reloading Emacs, there are some space at
-    ;; the bottom. Which is weird and I have no idea why...
-    (toggle-frame-maximized)
-    (toggle-frame-maximized)
-
-    ;; When frame not maximize we make sure it maximized.
-    (unless (jcs-is-frame-maximize-p)
-      (toggle-frame-maximized)))
-  (advice-add 'reload-emacs :after #'jcs-advice-reload-emacs-after))
 
 
 (use-package helm
@@ -437,14 +250,21 @@
      '(helm-gtags-auto-update t))))
 
 
-(use-package yasnippet
+(use-package indent-info
+  :ensure t
+  :defer t
+  :config
+  (global-indent-info-mode +1))
+
+
+(use-package impatient-mode
+  :ensure t)
+
+
+(use-package line-reminder
   :ensure t
   :config
-  (use-package yasnippet-snippets
-    :ensure t
-    :defer t)
-  (yas-reload-all)
-  (yas-global-mode 1))
+  (global-line-reminder-mode t))
 
 
 (use-package origami
@@ -454,8 +274,63 @@
   (global-origami-mode t))
 
 
-(use-package impatient-mode
-  :ensure t)
+(use-package powerline
+  :ensure t
+  :config
+  (powerline-default-theme)
+  ;;(powerline-center-theme)
+  ;;(powerline-center-evil-theme)
+  ;;(powerline-vim-theme)
+  ;;(powerline-nano-theme)
+
+  ;; NOTE(jenchieh):
+  ;; The separator to use for the default theme.
+  ;; Valid Values: alternate, arrow, arrow-fade, bar, box, brace,
+  ;; butt, chamfer, contour, curve, rounded, roundstub, wave, zigzag,
+  ;; utf-8.
+  (setq powerline-default-separator 'wave))
+
+
+;;; Preprocessor/Marcos highlight.
+(use-package preproc-font-lock
+  :ensure t
+  :config
+  (preproc-font-lock-global-mode t)
+  (preproc-font-lock-mode t)
+  (set-face-attribute 'preproc-font-lock-preprocessor-background
+                      nil
+                      :background "#333333"
+                      :inherit nil))
+
+
+(use-package reload-emacs
+  :config
+  (defun jcs-advice-reload-emacs-after ()
+    "Advice after execute `reload-emacs' command."
+    ;; Split window horizontally if full width.
+    (when (and (window-full-width-p)
+               (= (length (window-list)) 1))
+      (jcs-balance-split-window-horizontally))
+
+    ;; Restore to what ever state it was.
+    ;;
+    ;; NOTE(jenchieh): we need these two lines because we need it
+    ;; for solving after reloading Emacs, there are some space at
+    ;; the bottom. Which is weird and I have no idea why...
+    (toggle-frame-maximized)
+    (toggle-frame-maximized)
+
+    ;; When frame not maximize we make sure it maximized.
+    (unless (jcs-is-frame-maximize-p)
+      (toggle-frame-maximized)))
+  (advice-add 'reload-emacs :after #'jcs-advice-reload-emacs-after))
+
+
+(use-package right-click-context
+  :ensure t
+  :defer t
+  :config
+  (right-click-context-mode 1))
 
 
 (use-package skewer-mode
@@ -467,24 +342,149 @@
   (add-hook 'html-mode-hook 'skewer-html-mode))
 
 
-(use-package dashboard
+(use-package sr-speedbar
+  :ensure t
+  :config
+  ;;(setq sr-speedbar-auto-refresh nil)
+  (setq speedbar-show-unknown-files t) ; show all files
+  (setq speedbar-use-images nil) ; use text for buttons
+  ;;(setq sr-speedbar-right-side nil) ; put on left side
+  )
+
+
+(use-package sublimity
   :ensure t
   :defer t
   :config
-  (setq dashboard-banner-logo-title "Welcome to J-Emacs!")
-  (setq dashboard-startup-banner "~/.emacs.jcs/banner/sink_white.png")
-  (setq dashboard-items '((recents  . 10)
-                          (bookmarks . 10)))
+  (require 'sublimity-scroll)
+  (require 'sublimity-map)
+  (require 'sublimity-attractive)
 
-  (custom-set-faces
-   '(dashboard-banner-logo-title
-     ((t (:foreground "cyan1"))))
-   '(dashboard-heading
-     ((t (:foreground "#17A0FB"))))
-   '(widget-button
-     ((t (:foreground "light steel blue")))))
+  ;; default on or off?
+  ;; NOTE(jenchieh): This also trigger the animate scrolling too.
+  (sublimity-mode 1)
 
-  (dashboard-setup-startup-hook))
+  ;; Scroll Speed.
+  (setq sublimity-scroll-weight 2  ;; [Default : 2]
+        sublimity-scroll-drift-length 2)  ;; [Default : 2]
+
+
+  (setq sublimity-map-size 0)  ;; [Default : 10]
+  (setq sublimity-map-fraction 0.3)  ;; [Default : 0.3]
+  (setq sublimity-map-text-scale -7)  ;; [Default: -7]
+
+  ;; NOTE(jenchieh): When a positive integer is set, buffer
+  ;; width is truncated to this value and drawn centered. To
+  ;; cancel this feature, set this value nil.
+  (setq sublimity-attractive-centering-width nil)  ;; [Default : 110]
+
+  ;; NOTE(jenchieh): With the setting above, minimap is displayed
+  ;; after 5 seconds of idle time. When sublimity-map-set-delay
+  ;; is called with nil, then minimap is shown with no delay. This
+  ;; defers from setting delay to 0, especially when used with
+  ;; sublimity-scroll, in the sense that minimap looks not deleted
+  ;; at all but gets worse performance.
+
+  ;; ATTENTION(jenchieh): Set it to very hight so it will never
+  ;; reach the timer error.
+  (sublimity-map-set-delay 40000000)
+
+  ;; NOTE(jenchieh): sublimity-map-setup-hook will run when
+  ;; minimap is created.
+  (add-hook 'sublimity-map-setup-hook
+            (lambda ()
+              (setq buffer-face-mode-face '(:family "Monospace"))
+              (buffer-face-mode)))
+
+  ;; NOTE(jenchieh): Following functions are available to hide
+  ;; some UI parts.
+  ;;(sublimity-attractive-hide-bars)
+  ;;(sublimity-attractive-hide-vertical-border)
+  ;;(sublimity-attractive-hide-fringes)
+  ;;(sublimity-attractive-hide-modelines)
+  )
+
+
+(use-package tabbar
+  :ensure t
+  :defer t
+  :config
+  ;; Turn-off `tabbar-mode' as default.
+  (tabbar-mode 0))
+
+
+(use-package undo-tree
+  :ensure t
+  :defer t
+  :config
+  ;; Enable `undo-tree' as default.
+  (global-undo-tree-mode t))
+
+
+(use-package visual-regexp
+  :ensure t
+  :defer t)
+
+
+(use-package wgrep
+  :ensure t
+  :config
+  (setq wgrep-auto-save-buffer t))
+
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode)
+
+  ;; Provide following type: `minibuffer', `side-window', `frame'.
+  (setq which-key-popup-type 'side-window)
+
+  ;; location of which-key window. valid values: top, bottom, left, right,
+  ;; or a list of any of the two. If it's a list, which-key will always try
+  ;; the first location first. It will go to the second location if there is
+  ;; not enough room to display any keys in the first location
+  (setq which-key-side-window-location 'bottom)
+
+  ;; max width of which-key window, when displayed at left or right.
+  ;; valid values: number of columns (integer), or percentage out of current
+  ;; frame's width (float larger than 0 and smaller than 1)
+  (setq which-key-side-window-max-width 0.33)
+
+  ;; max height of which-key window, when displayed at top or bottom.
+  ;; valid values: number of lines (integer), or percentage out of current
+  ;; frame's height (float larger than 0 and smaller than 1)
+  (setq which-key-side-window-max-height 0.25)
+
+  ;; Set the time delay (in seconds) for the which-key popup to appear. A value of
+  ;; zero might cause issues so a non-zero value is recommended.
+  (setq which-key-idle-delay 1.0))
+
+
+(use-package whitespace
+  :config
+  (autoload 'whitespace-mode "whitespace-mode" "Toggle whitespace visualization." t)
+  (autoload 'whitespace-toggle-options "whitespace-mode" "Toggle local `whitespace-mode' options." t)
+  ;; All the face can be find here.
+  ;; URL: https://www.emacswiki.org/emacs/BlankMode
+  (set-face-attribute 'whitespace-indentation
+                      nil
+                      :background "grey20"
+                      :foreground "aquamarine3")
+  (set-face-attribute 'whitespace-trailing
+                      nil
+                      :background "grey20"
+                      :foreground "red"))
+
+
+(use-package yasnippet
+  :ensure t
+  :config
+  (use-package yasnippet-snippets
+    :ensure t
+    :defer t)
+  (yas-reload-all)
+  (yas-global-mode 1))
 
 
 (provide 'jcs-plugin)
