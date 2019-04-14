ae709a31eb63ba8943225c747d89f1f182e7ff61
jenchieh <jcs090218@gmail.com>
2019-04-14 20:52:18 +0800

    Add text mode.

---
+++ .emacs.jcs/mode/jcs-text-mode.el
@@ -0,0 +1,32 @@
+;;; jcs-text-mode.el --- Text modes. -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+
+(defun jcs-text-mode-hook ()
+  "Text mode hook."
+  (goto-address-mode 1)
+  (auto-highlight-symbol-mode t)
+
+  (defun jcs-text-mode-format()
+    "Fromat the given file as a normal Text file."
+    (when (jcs-is-current-file-empty-p)
+      (jcs-insert-text-template)))
+
+  (when buffer-file-name
+    (cond ((file-exists-p buffer-file-name) t)
+          ((string-match "[.]txt" buffer-file-name) (jcs-text-mode-format))
+          ))
+
+  ;; Normal
+  (define-key text-mode-map (kbd "C-d") #'jcs-kill-whole-line)
+  (define-key text-mode-map "\C-c\C-c" #'kill-ring-save)
+
+  (define-key text-mode-map (kbd "<up>") #'jcs-previous-line)
+  (define-key text-mode-map (kbd "<down>") #'jcs-next-line)
+  )
+(add-hook 'text-mode-hook 'jcs-text-mode-hook)
+
+
+(provide 'jcs-text-mode)
+;;; jcs-text-mode.el ends here
