6806a16f0b9bfef95dfb6d76fa623dbe5608a532
jenchieh <jcs090218@gmail.com>
2019-04-19 23:23:26 +0800

    Add advice to shift-select pre hook.

--- .emacs.jcs/jcs-plugin.el
+++ .emacs.jcs/jcs-plugin.el
@@ -428,6 +428,15 @@
 (use-package shift-select
   :diminish shift-select-minor-mode
   :config
+  (defun jcs-advice-shift-select-pre-command-hook-after ()
+    "Advice after execute `shift-select-pre-command-hook'."
+    (unless this-command-keys-shift-translated
+      (let ((sym-lst '(jcs-smart-indent-up
+                       jcs-smart-indent-down)))
+        (when (jcs-is-contain-list-symbol sym-lst this-command)
+          (deactivate-mark))
+        )))
+  (advice-add 'shift-select-pre-command-hook :after #'jcs-advice-shift-select-pre-command-hook-after)
   (global-shift-select-mode t))
 
 
