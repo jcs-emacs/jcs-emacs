681df0655aa281fa7b72ccb3e1680098d797b489
jenchieh <jcs090218@gmail.com>
2019-04-29 10:49:58 +0800

    Place font to font.

--- .emacs.jcs/jcs-font.el
+++ .emacs.jcs/jcs-font.el
@@ -3,21 +3,28 @@
 ;;; Code:
 
 
-(require 'use-ttf)
+;; -- Font Size: The value is in 1/10pt, so 100 will give you 10pt, etc.
+(set-face-attribute 'default nil :height 160)
 
-;; List of TTF fonts you want to use in the currnet OS.
-(setq use-ttf-default-ttf-fonts '(;; >> Classic Console <<
-                                  "/.emacs.jcs/fonts/clacon.ttf"
-                                  ;; >> Ubuntu Mono <<
-                                  "/.emacs.jcs/fonts/UbuntuMono-R.ttf"))
 
-;; Name of the font we want to use as default.
-;; This you need to check the font name in the system manually.
-(setq use-ttf-default-ttf-font-name "Ubuntu Mono")
+(use-package use-ttf
+  :ensure t
+  :config
+  ;; List of TTF fonts you want to use in the currnet OS.
+  (setq use-ttf-default-ttf-fonts '(;; >> Classic Console <<
+                                    "/.emacs.jcs/fonts/clacon.ttf"
+                                    ;; >> Ubuntu Mono <<
+                                    "/.emacs.jcs/fonts/UbuntuMono-R.ttf"))
 
-;; Use the font by `use-ttf-default-ttf-font-name` variable. This will actually
-;; set your Emacs to your target font.
-(call-interactively #'use-ttf-set-default-font)
+  ;; Name of the font we want to use as default.
+  ;; This you need to check the font name in the system manually.
+  (setq use-ttf-default-ttf-font-name "Ubuntu Mono")
+
+  ;; Use the font by `use-ttf-default-ttf-font-name` variable. This will actually
+  ;; set your Emacs to your target font.
+  (use-ttf-set-default-font))
+
+
 
 
 (provide 'jcs-font)
