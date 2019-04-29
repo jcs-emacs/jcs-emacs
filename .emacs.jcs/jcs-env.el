0669866cc12895b7847b28f43fad3141d5a4e123
jenchieh <jcs090218@gmail.com>
2019-04-29 10:53:15 +0800

    Move font env config to here.

--- .emacs.jcs/jcs-env.el
+++ .emacs.jcs/jcs-env.el
@@ -116,6 +116,27 @@
 ;;; Electric Pair
 (electric-pair-mode 1)
 
+;;; Font
+;; -- Font Size: The value is in 1/10pt, so 100 will give you 10pt, etc.
+(set-face-attribute 'default nil :height 160)
+
+(use-package use-ttf
+  :ensure t
+  :config
+  ;; List of TTF fonts you want to use in the currnet OS.
+  (setq use-ttf-default-ttf-fonts '(;; >> Classic Console <<
+                                    "/.emacs.jcs/fonts/clacon.ttf"
+                                    ;; >> Ubuntu Mono <<
+                                    "/.emacs.jcs/fonts/UbuntuMono-R.ttf"))
+
+  ;; Name of the font we want to use as default.
+  ;; This you need to check the font name in the system manually.
+  (setq use-ttf-default-ttf-font-name "Ubuntu Mono")
+
+  ;; Use the font by `use-ttf-default-ttf-font-name` variable. This will actually
+  ;; set your Emacs to your target font.
+  (use-ttf-set-default-font))
+
 ;;; Frame Title
 ;; SOURCE(jenchieh): https://stackoverflow.com/questions/3669511/the-function-to-show-current-files-full-path-in-mini-buffer
 (setq frame-title-format
