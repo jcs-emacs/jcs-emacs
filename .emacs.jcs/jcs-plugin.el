4e0b1a859d927b66bfbdfbfbc78d9e30fe59d21e
jenchieh <jcs090218@gmail.com>
2019-04-29 21:23:58 +0800

    Move use-ttf config to plugin file.

--- .emacs.jcs/jcs-plugin.el
+++ .emacs.jcs/jcs-plugin.el
@@ -539,6 +539,24 @@
   (global-undo-tree-mode t))
 
 
+(use-package use-ttf
+  :ensure t
+  :config
+  ;; List of TTF fonts you want to use in the currnet OS.
+  (setq use-ttf-default-ttf-fonts '(;; >> Classic Console <<
+                                    "/.emacs.jcs/fonts/clacon.ttf"
+                                    ;; >> Ubuntu Mono <<
+                                    "/.emacs.jcs/fonts/UbuntuMono-R.ttf"))
+
+  ;; Name of the font we want to use as default.
+  ;; This you need to check the font name in the system manually.
+  (setq use-ttf-default-ttf-font-name "Ubuntu Mono")
+
+  ;; Use the font by `use-ttf-default-ttf-font-name` variable. This will actually
+  ;; set your Emacs to your target font.
+  (use-ttf-set-default-font))
+
+
 (use-package visual-regexp
   :ensure t
   :defer t)
