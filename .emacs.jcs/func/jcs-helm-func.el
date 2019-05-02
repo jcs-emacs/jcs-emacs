82a5682050d7a7911a0a47b8c69a53465f27e2ce
Jen-Chieh <jcs090218@gmail.com>
2019-05-02 16:09:34 +0800

    Rename process.

--- .emacs.jcs/func/jcs-helm-func.el
+++ .emacs.jcs/func/jcs-helm-func.el
@@ -7,7 +7,7 @@
   "Do the helm mx and change theme"
   ;; NOTE(jenchieh): Change theme so we know which mode
   ;; we are in visually.
-  (jcs-dark-blue-theme))
+  (jcs-dark-blue-mode-line))
 (add-hook 'helm-before-initialize-hook 'jcs-helm-before-initialize-hook)
 
 
