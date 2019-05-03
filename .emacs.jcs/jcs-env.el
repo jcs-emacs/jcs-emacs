8774006a2d9efa937667b6e3a2033493d6bd49fb
jenchieh <jcs090218@gmail.com>
2019-05-04 00:10:26 +0800

    Ensure effect with isearch-project backward symbol at point.

--- .emacs.jcs/jcs-env.el
+++ .emacs.jcs/jcs-env.el
@@ -218,10 +218,16 @@
   :config
   (defun jcs-isearch-mode-hook ()
     "Paste the current symbol when `isearch' enabled."
-    (when (use-region-p)
-      (deactivate-mark)
-      (ignore-errors
-        (isearch-yank-string (buffer-substring-no-properties (region-beginning) (region-end))))))
+    (cond ((use-region-p)
+           (progn
+             (deactivate-mark)
+             (ignore-errors
+               (isearch-yank-string (buffer-substring-no-properties (region-beginning) (region-end))))))
+          ((memq this-command '(jcs-isearch-project-backward-symbol-at-point))
+           (when (char-or-string-p isearch-project-thing-at-point)
+             (backward-word 1)
+             (isearch-project-isearch-yank-string isearch-project-thing-at-point)
+             (isearch-repeat-backward)))))
   (add-hook 'isearch-mode-hook #'jcs-isearch-mode-hook))
 
 
