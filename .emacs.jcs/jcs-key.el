785cb2f51076bcf61c05e410e81d330363f1abaf
jenchieh <jcs090218@gmail.com>
2019-05-04 21:17:08 +0800

    Rebind dashboard key.

--- .emacs.jcs/jcs-key.el
+++ .emacs.jcs/jcs-key.el
@@ -369,7 +369,6 @@
 (define-key dashboard-mode-map (kbd "<up>") #'jcs-previous-line)
 (define-key dashboard-mode-map (kbd "<down>") #'jcs-next-line)
 (define-key dashboard-mode-map (kbd "C-p") #'package-list-packages)
-(define-key dashboard-mode-map (kbd "g") #'jcs-refresh-dashboard-buffer)
 (define-key dashboard-mode-map (kbd "M-K") #'jcs-refresh-dashboard-buffer)
 
 ;;; Syntax Check
