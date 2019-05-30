f1f99f39fa5b55bbcc45430abdb0bb1135d7684a
Jen-Chieh <jcs090218@gmail.com>
2019-05-30 13:20:07 +0800

    Add clean.

---
+++ .emacs.jcs/scripts/clean.bat
@@ -0,0 +1,14 @@
+@echo off
+:: ========================================================================
+:: $File: clean.bat $
+:: $Date: 2019-05-30 13:05:46 $
+:: $Revision: $
+:: $Creator: Jen-Chieh Shen $
+:: $Notice: See LICENSE.txt for modification and distribution information
+::                   Copyright © 2019 by Shen, Jen-Chieh $
+:: ========================================================================
+
+
+cd ..
+
+del /s *.elc
---
+++ .emacs.jcs/scripts/clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# ========================================================================
+# $File: clean.sh $
+# $Date: 2019-05-30 13:06:46 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2019 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+cd ..
+
+rm -r **/*.elc
