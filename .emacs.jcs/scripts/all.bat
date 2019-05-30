597b638c6bbbc7a0fbeb2b3dc63f168c063db094
Jen-Chieh <jcs090218@gmail.com>
2019-05-30 14:42:38 +0800

    Add all.

---
+++ .emacs.jcs/scripts/all.bat
@@ -0,0 +1,16 @@
+@echo off
+:: ========================================================================
+:: $File: all.bat $
+:: $Date: 2019-05-30 14:19:00 $
+:: $Revision: $
+:: $Creator: Jen-Chieh Shen $
+:: $Notice: See LICENSE.txt for modification and distribution information
+::                   Copyright © 2019 by Shen, Jen-Chieh $
+:: ========================================================================
+
+
+call ./clean.bat
+cd ./scripts
+
+call ./compile.bat
+cd ./scripts
---
+++ .emacs.jcs/scripts/all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ========================================================================
+# $File: all.sh $
+# $Date: 2019-05-30 14:19:32 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2019 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+sh ./clean.sh
+sh ./compile.sh
