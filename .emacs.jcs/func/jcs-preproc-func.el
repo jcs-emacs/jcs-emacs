76dea79565f009a04ddd38a072105d292e7c68b4
jenchieh <jcs090218@gmail.com>
2019-05-31 13:45:59 +0800

    Format to jcs standard change faces style.

--- .emacs.jcs/func/jcs-preproc-func.el
+++ .emacs.jcs/func/jcs-preproc-func.el
@@ -3,31 +3,24 @@
 ;;; Code:
 
 
-(defvar jcs-preproc-highlight-modes '(cc-mode
-                                      c-mode
-                                      c++-mode
-                                      csharp-mode
-                                      nasm-mode)
-  "Modes to add preprocessor highlighting.")
-
-
-(defface jcs-preproc-variable-name-face
-  '((t (:foreground "#B363BE")))
-  "Highlight OOP tag.")
-(defvar jcs-preproc-variable-name-face 'jcs-preproc-variable-name-face)
-
-
-(mapc (lambda (mode)
-        (font-lock-add-keywords
-         mode
-         '(("[#%][ \t]*define[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
-           ("[#%][ \t]*ifdef[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
-           ("[#%][ \t]*ifndef[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
-           ("[#%][ \t]*elif[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
-           ("[#%][ \t]*if defined[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
-           ("[#%][ \t]*if !defined[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
-           )'end))
-      jcs-preproc-highlight-modes)
+(defun jcs-init-preproc-faces ()
+  "Initialize preprocessor faces highlihgting."
+  (let ((preproc-highlight-modes '(cc-mode
+                                   c-mode
+                                   c++-mode
+                                   csharp-mode
+                                   nasm-mode)))
+    (mapc (lambda (mode)
+            (font-lock-add-keywords
+             mode
+             '(("[#%][ \t]*define[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
+               ("[#%][ \t]*ifdef[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
+               ("[#%][ \t]*ifndef[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
+               ("[#%][ \t]*elif[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
+               ("[#%][ \t]*if defined[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
+               ("[#%][ \t]*if !defined[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
+               )'end))
+          preproc-highlight-modes)))
 
 
 (provide 'jcs-preproc-func)
