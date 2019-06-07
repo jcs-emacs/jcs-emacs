6030522c5f155b4cd10831a340b99ceb01b72d91
jenchieh <jcs090218@gmail.com>
2019-06-07 23:17:20 +0800

    Fixed helm ff other windows functions.

--- .emacs.jcs/func/jcs-helm-func.el
+++ .emacs.jcs/func/jcs-helm-func.el
@@ -20,7 +20,11 @@
            (equal args nil)
            (stringp (helm-get-selection))
            (not (file-directory-p (helm-get-selection))))
-      (helm-maybe-exit-minibuffer)
+      (progn
+        (when jcs-helm-ff-other-window
+          (setq jcs-helm-ff-other-window nil)
+          (helm-ff-run-switch-other-window))
+        (helm-maybe-exit-minibuffer))
     (apply orig-fun args)))
 (advice-add 'helm-execute-persistent-action :around #'jcs-helm-find-files-navigate-forward)
 
@@ -31,6 +35,9 @@
     (apply orig-fun args)))
 (advice-add 'helm-ff-delete-char-backward :around #'jcs-helm-find-files-navigate-back)
 
+
+(defvar jcs-helm-ff-other-window nil
+  "Helm find file other window flag.")
 
 ;;;
 ;; `helm-find-files-hook'
@@ -44,6 +51,32 @@
   (setq jcs-helm-find-files-active t)
   )
 (add-hook 'helm-find-files-after-init-hook 'jcs-helm-find-files-hook)
+
+;;;###autoload
+(defun jcs-helm-find-files-other-window ()
+  "Find the file with Helm and open another window."
+  (interactive)
+  (setq jcs-helm-ff-other-window t)
+  (helm-find-files nil))
+
+;;;###autoload
+(defun jcs-helm-exit-minibuffer ()
+  "The command `helm-exit-minibuffer' calls interactively."
+  (interactive)
+  (helm-exit-minibuffer))
+
+;;;###autoload
+(defun jcs-helm-projectile-find-file-other-window ()
+  "Find file in project to other window."
+  (interactive)
+  (let ((record-dd default-directory)
+        (found-file nil)
+        (starting-window (selected-window)))
+    (jcs-other-window-next 1 t)
+    (setq default-directory record-dd)
+    (setq found-file (helm-projectile-find-file))
+    (unless found-file
+      (select-window starting-window))))
 
 
 ;;;###autoload
@@ -77,32 +110,6 @@ in other window."
     (helm-gtags-find-tag-from-here))
 
   (jcs-reload-active-mode))
-
-;;;###autoload
-(defun jcs-helm-find-files-other-window ()
-  "Find the file with Helm and open another window."
-  (interactive)
-  (let ((record-dd default-directory)
-        (found-file nil)
-        (starting-window (selected-window)))
-    (jcs-other-window-next 1 t)
-    (setq default-directory record-dd)
-    (setq found-file (helm-find-files nil))
-    (unless found-file
-      (select-window starting-window))))
-
-;;;###autoload
-(defun jcs-helm-projectile-find-file-other-window ()
-  "Find file in project to other window."
-  (interactive)
-  (let ((record-dd default-directory)
-        (found-file nil)
-        (starting-window (selected-window)))
-    (jcs-other-window-next 1 t)
-    (setq default-directory record-dd)
-    (setq found-file (helm-projectile-find-file))
-    (unless found-file
-      (select-window starting-window))))
 
 
 (provide 'jcs-helm-func)
