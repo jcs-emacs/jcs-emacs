3ae20b32e75102433630c5bb2e30708239ade61b
Jen-Chieh <jcs090218@gmail.com>
2019-06-14 12:07:12 +0800

    Change using search project technique.

--- .emacs.jcs/jcs-file.el
+++ .emacs.jcs/jcs-file.el
@@ -3,9 +3,70 @@
 ;;; Code:
 
 
+(defun jcs-select-find-file-current-dir (in-filename)
+  "Find the file in the current directory.
+Return the absolute filepath.
+IN-FILENAME : file name to find.
+IN-TITLE : title for `completing-read' function call."
+  (let ((target-filepath "")
+        (current-source-dir default-directory))
+    (setq target-filepath (concat current-source-dir in-filename))
+
+    (if (file-exists-p target-filepath)
+        target-filepath  ;; Return if the target file exists.
+      (error (format "No '%s' file found in the current directory"
+                     in-filename)))))
+
+(defun jcs-select-find-file-in-project (in-filename in-title)
+  "Find the file in the project.  Version Control directory must exists
+in order to make it work.
+Return the absolute filepath.
+IN-FILENAME : file name to find.
+IN-TITLE : title for `completing-read' function call."
+  (let ((target-files '())
+        (project-source-dir (jcs-vc-root-dir)))
+    ;; Do the find file only when the project directory exists.
+    (unless (string= project-source-dir "")
+      (setq target-files (f--files project-source-dir (string-match-p in-filename (f-filename it)) t)))
+
+    (let ((target-files-len (length target-files))
+          (target-filepath ""))
+      (if (= target-files-len 0)
+          (error (format "No '%s' file found in the project, make sure the project directory exists"
+                         in-filename))
+        (if (= target-files-len 1)
+            ;; If only one file found, just get that file.
+            (setq target-filepath (nth 0 target-files))
+          (progn
+            ;; Get the selected file.
+            (setq target-filepath (completing-read
+                                   in-title target-files)))))
+      target-filepath)))
+
+(defun jcs-find-file-in-project-and-current-dir (in-filename in-title)
+  "First find the file from the whole project, if not found find it in the \
+current directory then.
+Return full path if found, else error prompt.
+IN-FILENAME : filename to search in project or current directory.
+TITLE : Search uses regexp, meaning it could found multiple files at a time.
+We need a title to present which file to select."
+  (let ((filepath ""))
+    (unless (ignore-errors
+              (setq filepath (jcs-select-find-file-in-project in-filename
+                                                              in-title)))
+      (unless (ignore-errors
+                (setq filepath (jcs-select-find-file-current-dir in-filename)))
+        (error (format (concat "Cannot find '%s' file either in the project or current "
+                               "directory, make sure the project directory exists or "
+                               "the '%s' file exists in the current directory")
+                       in-filename
+                       in-filename))))
+    ;; Return the path.
+    filepath))
+
+
 ;;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-;; IMPORTANT(jenchieh): Keep core function at the top
-;; of this file.
+;; IMPORTANT: Keep core function at the top of this file.
 ;;
 ;;   * `jcs-find-corresponding-file'
 ;;   * `jcs-find-corresponding-file-other-window'
@@ -15,21 +76,22 @@
 (defun jcs-find-corresponding-file ()
   "Find the file that corresponds to this one."
   (interactive)
-  (let ((corresponding-file-name ""))
+  (let ((corresponding-file-name "")
+        (found-filepath nil))
     ;;;
-    ;; NOTE(jenchieh): Add your corresponding file here.
+    ;; NOTE: Add your corresponding file here.
     ;;;
 
-    ;; NOTE(jenchieh): Find C/C++ corresponding file.
+    ;; NOTE: Find C/C++ corresponding file.
     (when (or (jcs-is-current-major-mode-p "c++-mode")
               (jcs-is-current-major-mode-p "c-mode"))
       (setq corresponding-file-name (jcs-cc-corresponding-file)))
 
-    ;; NOTE(jenchieh): Find Objective-C corresponding file.
+    ;; NOTE: Find Objective-C corresponding file.
     (when (or (jcs-is-current-major-mode-p "objc-mode"))
       (setq corresponding-file-name (jcs-objc-corresponding-file)))
 
-    ;; NOTE(jenchieh): Find WEB corresponding file.
+    ;; NOTE: Find WEB corresponding file.
     (when (or
            ;; For ASP.NET -> [file-name].aspx.cs
            (jcs-is-current-major-mode-p "csharp-mode")
@@ -39,7 +101,11 @@
 
     ;; Error check before return it value.
     (if corresponding-file-name
-        (find-file corresponding-file-name)
+        (progn
+          (setq found-filepath
+                (jcs-find-file-in-project-and-current-dir corresponding-file-name
+                                                          "Corresponding file: "))
+          (find-file found-filepath))
       (error "Unable to find a corresponding file.."))))
 
 ;;;###autoload
@@ -58,7 +124,7 @@
 (defun jcs-cc-corresponding-file ()
   "Find the corresponding file for C/C++ file."
   (let ((corresponding-file-name "")
-        (tmp-base-file-name (file-name-sans-extension buffer-file-name)))
+        (tmp-base-file-name (file-name-sans-extension (buffer-name))))
     (cond ((string-match "\\.hin" buffer-file-name)
            (progn
              (setq corresponding-file-name (concat tmp-base-file-name ".cin"))))
@@ -122,7 +188,7 @@
              (setq corresponding-file-name (concat tmp-base-file-name ".aspx.cs"))))
           )
 
-    ;; NOTE(jenchieh): If is ASP.NET, just open the current file itself.
+    ;; NOTE: If is ASP.NET, just open the current file itself.
     (when (string= corresponding-file-name "")
       (setq corresponding-file-name buffer-file-name))
 
