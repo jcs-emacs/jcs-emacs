85c9b633786d2f8fe3f00931af3e5ca7bac73d33
jenchieh <jcs090218@gmail.com>
2019-06-16 00:38:49 +0800

    Add walk through all frames.

--- .emacs.jcs/func/jcs-frame.el
+++ .emacs.jcs/func/jcs-frame.el
@@ -3,32 +3,32 @@
 ;;; Code:
 
 
-;;;###autoload
-(defun jcs-new-frame ()
-  "Setup dual monitor."
-  (interactive)
-
-  ;; open a new frame
-  (make-frame-command))
-
-;;;###autoload
 (defun jcs-aftermake-frame-functions-hook (frame)
   "Resetting the new frame just created."
-  (interactive)
-
+  (jcs-refresh-theme)
   (select-frame frame)
-
   ;; split the winodw after create the new window
-  (split-window-horizontally))
+  (split-window-horizontally)
+  )
 (add-hook 'after-make-frame-functions 'jcs-aftermake-frame-functions-hook)
-
-;;-----------------------------------------------------------
-;;-----------------------------------------------------------
 
 (defun jcs-is-frame-maximize-p ()
   "Return non-nil, if frame maximized.
 Return nil, if frame not maximized."
   (cdr (assoc 'fullscreen (frame-parameters))))
+
+;;;###autoload
+(defun jcs-walk-through-all-frames-once (&optional fnc)
+  "Walk through all the frames once.
+FNC : Callback apply to each windows."
+  (interactive)
+  (save-selected-window
+    (let ((index 0))
+      (while (< index (length (frame-list)))
+        (when fnc
+          (funcall fnc))
+        (call-interactively #'other-frame)
+        (setq index (+ index 1))))))
 
 
 (provide 'jcs-frame)
