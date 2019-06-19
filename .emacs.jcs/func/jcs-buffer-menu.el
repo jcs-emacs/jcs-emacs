ee3e8645696fe1b9933da3fb237c8e598a2ce49c
Jen-Chieh <jcs090218@gmail.com>
2019-06-19 13:47:10 +0800

    Reduce duplicate code.

--- .emacs.jcs/func/jcs-buffer-menu.el
+++ .emacs.jcs/func/jcs-buffer-menu.el
@@ -13,49 +13,48 @@
 
 ;; TOPIC: BufferMenuPlus
 ;; URL: https://www.emacswiki.org/emacs/BufferMenuPlus
-;; Sorted by (1) visit, (2) buffer, (3) size, (4) time, (5) mode, (6) file. More
+
+(defun jcs-buffer-menu-sort (type)
+  "Sort the buffer menu by TYPE.
+Sorted by (1) visit, (2) buffer, (3) size, (4) time, (5) mode, (6) file."
+  (Buffer-menu-sort type)
+  (goto-char (point-min)))
 
 ;;;###autoload
 (defun jcs-buffer-menu-sort-by-visit ()
   "Sort the Buffer Menu List by visit."
   (interactive)
-  (Buffer-menu-sort 1)
-  (goto-char (point-min)))
+  (jcs-buffer-menu-sort 1))
 
 ;;;###autoload
 (defun jcs-buffer-menu-sort-by-buffer ()
   "Sort the Buffer Menu List by buffer."
   (interactive)
-  (Buffer-menu-sort 2)
-  (goto-char (point-min)))
+  (jcs-buffer-menu-sort 2))
 
 ;;;###autoload
 (defun jcs-buffer-menu-sort-by-size ()
   "Sort the Buffer Menu List by size."
   (interactive)
-  (Buffer-menu-sort 3)
-  (goto-char (point-min)))
+  (jcs-buffer-menu-sort 3))
 
 ;;;###autoload
 (defun jcs-buffer-menu-sort-by-time ()
   "Sort the Buffer Menu List by time."
   (interactive)
-  (Buffer-menu-sort 4)
-  (goto-char (point-min)))
+  (jcs-buffer-menu-sort 4))
 
 ;;;###autoload
 (defun jcs-buffer-menu-sort-by-mode ()
   "Sort the Buffer Menu List by mode."
   (interactive)
-  (Buffer-menu-sort 5)
-  (goto-char (point-min)))
+  (jcs-buffer-menu-sort 5))
 
 ;;;###autoload
 (defun jcs-buffer-menu-sort-by-file ()
   "Sort the Buffer Menu List by file name."
   (interactive)
-  (Buffer-menu-sort 6)
-  (goto-char (point-min)))
+  (jcs-buffer-menu-sort 6))
 
 
 (provide 'jcs-buffer-menu)
