0f8c01b319195bde19a426878d9f2c2042868102
jenchieh <jcs090218@gmail.com>
2019-06-22 01:35:05 +0800

    Try dolist instead.

--- .emacs.jcs/jcs-package.el
+++ .emacs.jcs/jcs-package.el
@@ -158,15 +158,14 @@
 (defun jcs-ensure-package-installed (packages &optional without-asking)
   "Assure every package is installed, ask for installation if it’s not.
 Return a list of installed packages or nil for every skipped package."
-  (mapc (lambda (package)
-          (if (package-installed-p package)
-              nil
-            (if without-asking
-                (jcs-install-missing-package-install package)
-              (if (y-or-n-p (format "Package %s is missing. Install it? " package))
-                  (jcs-install-missing-package-install package)
-                package))))
-        packages)
+  (dolist (package packages)
+    (if (package-installed-p package)
+        nil
+      (if without-asking
+          (jcs-install-missing-package-install package)
+        (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+            (jcs-install-missing-package-install package)
+          package))))
   ;; STUDY: Not sure if you need this?
   (when (get 'jcs-install-missing-package-install 'state)
     ;; activate installed packages
