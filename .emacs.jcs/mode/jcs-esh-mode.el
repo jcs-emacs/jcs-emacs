2054102841d722a5199d7276d844fdb5364a5b82
Jen-Chieh <jcs090218@gmail.com>
2019-06-28 12:01:35 +0800

    Impls eshell.

---
+++ .emacs.jcs/mode/jcs-esh-mode.el
@@ -0,0 +1,18 @@
+;;; jcs-esh-mode.el --- Eshell mode. -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+
+(require 'esh-mode)
+
+
+(defun jcs-eshell-mode-hook ()
+  "Eshell mode hook."
+
+  (define-key eshell-mode-map (kbd "M-k") #'jcs-maybe-kill-shell)  ; Close it.
+  )
+(add-hook 'eshell-mode-hook 'jcs-eshell-mode-hook)
+
+
+(provide 'jcs-esh-mode)
+;;; jcs-esh-mode.el ends here
