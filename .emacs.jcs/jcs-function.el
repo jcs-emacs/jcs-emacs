60ec6abccdb5a2b057d699c17295c6158534905f
jenchieh <jcs090218@gmail.com>
2019-06-29 17:54:32 +0800

    Use it with lambda for now.

--- .emacs.jcs/jcs-function.el
+++ .emacs.jcs/jcs-function.el
@@ -864,7 +864,8 @@ NO-PROMPT : Don't prompt the overwrap message."
 (require 'jcs-util)
 (require 'jcs-frame)
 (require 'jcs-window)
-(jcs-with-eval-after-load-multiple '(shell eshell-mode) (require 'jcs-shell))
+(jcs-with-eval-after-load-multiple
+ '(shell eshell-mode) #'(lambda () (require 'jcs-shell)))
 (with-eval-after-load 'helm (require 'jcs-helm-func))
 
 ;; Editing
