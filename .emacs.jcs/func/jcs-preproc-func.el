d473470b69d7239cf77eabdb966b5613b432a6c5
JenChieh <jcs090218@gmail.com>
2019-07-22 18:00:44 +0800

    Complete more preprocessor hl.:

--- .emacs.jcs/func/jcs-preproc-func.el
+++ .emacs.jcs/func/jcs-preproc-func.el
@@ -13,14 +13,16 @@
     (dolist (mode preproc-highlight-modes)
       (font-lock-add-keywords
        mode
-       '(("[#%][ \t]*define[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
-         ("[#%][ \t]*ifdef[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
-         ("[#%][ \t]*ifndef[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
-         ("[#%][ \t]*elif[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
-         ("[#%][ \t]*if defined[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
-         ("[#%][ \t]*if !defined[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
-         ("[#%][ \t]*if\\([a-zA-Z0-9-.<>?,*'`@\"=_(){}:&^%$#!~ \t]+\\)" 1 'jcs-preproc-variable-name-face t)
-         ("[#%][ \t]*region\\([a-zA-Z0-9-.<>?,*'`@\"=_(){}:&^%$#!~ \t]+\\)" 1 'jcs-preproc-variable-name-face t)
+       '(("\\([#%][ \t]*define\\)" 1 'font-lock-preprocessor-face t)
+         ("[#%][ \t]*define[ \t]+\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
+         ("[#%][ \t]*ifdef[ \t]+[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
+         ("[#%][ \t]*ifndef[ \t]+[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
+         ("[#%][ \t]*elif[ \t]+[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
+         ("[#%][ \t]*elif[ \t]+defined[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
+         ("[#%][ \t]*if[ \t]+defined[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
+         ("[#%][ \t]*if[ \t]+!defined[a-z0-9_$]*[ \t]*\\([a-zA-Z0-9_$]*\\)" 1 'jcs-preproc-variable-name-face t)
+         ("[#%][ \t]*if[ \t]+\\([a-zA-Z0-9-.<>?,*'`@\"=_(){}:&^%$#!~ \t]+\\)" 1 'jcs-preproc-variable-name-face t)
+         ("[#%][ \t]*region[ \t]+\\([a-zA-Z0-9-.<>?,*'`@\"=_(){}:&^%$#!~ \t]+\\)" 1 'jcs-preproc-variable-name-face t)
          )'end))))
 
 
