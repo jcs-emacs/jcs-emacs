244bce3e14bc54d61855063b49e7e17aa1abaf4e
Jen-Chieh <jcs090218@gmail.com>
2019-08-29 11:54:41 +0800

    Fixed mode map and global map inconsistency.

--- .emacs.jcs/mode/jcs-python-mode.el
+++ .emacs.jcs/mode/jcs-python-mode.el
@@ -25,6 +25,7 @@
   ;; Normal
   (define-key python-mode-map (kbd "C-d") #'jcs-kill-whole-line)
   (define-key python-mode-map (kbd "C-c C-c") #'kill-ring-save)
+  (define-key python-mode-map (kbd "<backspace>") #'jcs-smart-backspace)
   (define-key python-mode-map [C-backspace] #'jcs-backward-delete-word)
 
   (define-key python-mode-map [M-up] #'jcs-previous-blank-line)
