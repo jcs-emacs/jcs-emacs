2ab27ed91775168e44ced080f330f326ab058686
Jen-Chieh <jcs090218@gmail.com>
2019-09-05 16:56:15 +0800

    Impls feebleline func.

---
+++ .emacs.jcs/func/jcs-feebleline-func.el
@@ -0,0 +1,32 @@
+;;; jcs-feebleline-func.el --- Feebleline function related.  -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+
+(defun jcs--feebleline--symbol-read-only ()
+  "Feebleline read-only symbol."
+  (if buffer-read-only "æ" "§"))
+
+(defun jcs--feebleline--project-name ()
+  "Feebleline project name."
+  (let ((project-root (cdr (project-current))))
+    (if (and project-root
+             (buffer-file-name))
+        (file-name-nondirectory (directory-file-name project-root))
+      "¥")))
+
+(defun jcs--feebleline--coding-system ()
+  "Feebleline coding system."
+  buffer-file-coding-system)
+
+(defun jcs--feebleline--spc/tab ()
+  "Feebleline spaces or tabs."
+  (format "%s : %s" (jcs-buffer-spaces-to-tabs) (jcs-get-tab-width-by-mode)))
+
+(defun jcs--feebleline--time ()
+  "Feebleline time."
+  (format-time-string "[%Y-%m-%d %H:%M:%S]"))
+
+
+(provide 'jcs-feebleline-func)
+;;; jcs-feebleline-func.el ends here
