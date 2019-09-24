06b38a42ada3d0553244cf58de4553e9c7463d1e
Jen-Chieh <jcs090218@gmail.com>
2019-09-24 14:26:42 +0800

    Update by using ask line endings func.

--- .emacs.jcs/func/jcs-sh-func.el
+++ .emacs.jcs/func/jcs-sh-func.el
@@ -7,14 +7,14 @@
 (defun jcs-sh-untabify-save-buffer ()
   "ShellScript save buffer function."
   (interactive)
-  (set-buffer-file-coding-system 'unix)
+  (call-interactively #'jcs-ask-line-endings-for-this-sh-script)
   (jcs-untabify-save-buffer))
 
 ;;;###autoload
 (defun jcs-sh-tabify-save-buffer ()
   "ShellScript save buffer function."
   (interactive)
-  (set-buffer-file-coding-system 'unix)
+  (call-interactively #'jcs-ask-line-endings-for-this-sh-script)
   (jcs-tabify-save-buffer))
 
 
