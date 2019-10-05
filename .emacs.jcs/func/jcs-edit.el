a34cd32b024d3fa71170906137e75347adb228e2
JenChieh <jcs090218@gmail.com>
2019-10-05 15:28:41 +0800

    Fixed use of electric with just string only.

--- .emacs.jcs/func/jcs-edit.el
+++ .emacs.jcs/func/jcs-edit.el
@@ -1496,9 +1496,13 @@ CC : Current character at position."
         (forward-char 1)
         (setq cc (jcs-get-current-char-string)))
       (setq opc (jcs-get-open-pair-char cc))
-      (backward-delete-char -1)
-      (jcs-backward-delete-open-pair-char opc)
-      (jcs-backward-delete-open-pair-char-seq cc))))
+      (if (and (jcs-is-inside-string-p)
+               (not (string= cc "\""))
+               (not (string= cc "'")))
+          (backward-delete-char -1)
+        (backward-delete-char -1)
+        (jcs-backward-delete-open-pair-char opc)
+        (jcs-backward-delete-open-pair-char-seq cc)))))
 
 ;;;###autoload
 (defun jcs-electric-backspace ()
@@ -1506,11 +1510,15 @@ CC : Current character at position."
   (interactive)
   (if (use-region-p)
       (delete-region (region-beginning) (region-end))
-    (let* ((cc (jcs-get-current-char-string))
-           (cpc (jcs-get-close-pair-char cc)))
-      (jcs-own-delete-backward-char)
-      (jcs-forward-delete-close-pair-char cpc)
-      (jcs-forward-delete-close-pair-char-seq cc))))
+    (if (and (jcs-is-inside-string-p)
+             (not (jcs-current-char-equal-p "\""))
+             (not (jcs-current-char-equal-p "'")))
+        (jcs-own-delete-backward-char)
+      (let* ((cc (jcs-get-current-char-string))
+             (cpc (jcs-get-close-pair-char cc)))
+        (jcs-own-delete-backward-char)
+        (jcs-forward-delete-close-pair-char cpc)
+        (jcs-forward-delete-close-pair-char-seq cc)))))
 
 ;;----------------------------------------------
 ;; Isearch
