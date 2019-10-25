c85a094e131f5fcf712327127de3f2a07f947e28
JenChieh <jcs090218@gmail.com>
2019-10-25 11:41:51 +0800

    Add emmet to markdown for compatibility.

--- .emacs.jcs/mode/jcs-markdown-mode.el
+++ .emacs.jcs/mode/jcs-markdown-mode.el
@@ -5,19 +5,24 @@
 
 (require 'markdown-mode)
 
+(require 'jcs-web-func)
+
 
 (defun jcs-markdown-mode-hook ()
   "Markdown mode hook."
 
   (jcs-make-electric-pair-pairs-local '((?\` . ?\`)))
 
-
   ;; Normal
   (define-key markdown-mode-map (kbd "<backspace>") #'jcs-real-backspace)
 
   (define-key markdown-mode-map (kbd "C-s") #'jcs-save-buffer)
+
+  ;; Eemmet
+  (define-key emmet-mode-keymap (kbd "C-<return>") #'jcs-emmet-expand-line)
   )
 (add-hook 'markdown-mode-hook 'jcs-markdown-mode-hook)
+(add-hook 'markdown-mode-hook 'emmet-mode)
 
 
 (provide 'jcs-markdown-mode)
