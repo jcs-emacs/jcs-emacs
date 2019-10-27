9f2aa5c4f5b4638f705c948a49894daa6dfa6685
jenchieh <jcs090218@gmail.com>
2019-10-27 14:58:31 +0800

    Cleanup code from dev module.

--- .emacs.jcs/jcs-dev.el
+++ .emacs.jcs/jcs-dev.el
@@ -6,10 +6,12 @@
 ;;----------------------------------------------------------------------------
 ;; Eval Elisp
 
-(defun jcs--eval-region--advice-after (&rest _)
-  "Advice execute after `eval-region' function."
+(defun jcs--eval-func--advice-after (&rest _)
+  "Advice execute after `eval-' related functions."
   (deactivate-mark))
-(advice-add 'eval-region :after #'jcs--eval-region--advice-after)
+(advice-add 'eval-buffer :after #'jcs--eval-func--advice-after)
+(advice-add 'eval-defun :after #'jcs--eval-func--advice-after)
+(advice-add 'eval-region :after #'jcs--eval-func--advice-after)
 
 ;;----------------------------------------------------------------------------
 ;; Build & Run
@@ -19,9 +21,7 @@
   "Open the IN-FILENAME from this project with TITLE.
 OW : Opened it in other window."
   (interactive)
-  (let ((filepath
-         (jcs-find-file-in-project-and-current-dir in-filename
-                                                   title)))
+  (let ((filepath (jcs-find-file-in-project-and-current-dir in-filename title)))
     (if ow
         (find-file-other-window filepath)
       (find-file filepath))))
@@ -30,9 +30,7 @@ OW : Opened it in other window."
 (defun jcs-compile-project-file (in-filename title)
   "Compile IN-FILENAME from the project"
   (interactive)
-  (let ((filepath
-         (jcs-find-file-in-project-and-current-dir in-filename
-                                                   title)))
+  (let ((filepath (jcs-find-file-in-project-and-current-dir in-filename title)))
     (jcs-compile filepath)))
 
 
@@ -40,29 +38,29 @@ OW : Opened it in other window."
   "Compile command rewrapper.
 IN-OP : inpuit operation script."
   (require 'f)
-  ;; NOTE: First we need to get the script directory. In order
-  ;; to change execute/workspace directory to the current target script's
-  ;; directory path.
-  (let ((script-dir (f-dirname in-op)))
-    ;; NOTE: Change the current execute/workspace directory
-    ;; to the script directory temporary. So the script will execute
-    ;; within the current directory the script is currently in.
-    ;;
-    ;; Without these lines of code, the script will execute in the
-    ;; `default-directory' variables. The `default-directory' variables
-    ;; will be the directory path where you start the Emacs. For instance,
-    ;; if you start Emacs at path `/usr/home', then the default directory
-    ;; will be at `usr/home' directory.
-    ;;
-    ;; Adding these lines of code if your scirpt is at `/usr/home/project/some-script.sh',
-    ;; Then your `default-directory' became `usr/home/project'. Hurray!
-    (let ((default-directory script-dir))
-      ;; Compile/Execute the target script.
-      (compile in-op)
-      (jcs-update-line-number-each-window)
-      (with-current-buffer "*compilation*"
-        (rename-buffer (format "*compilation*: %s" (f-filename in-op)) t))
-      (message "Executing script file: '%s'" in-op))))
+  (let* (;; NOTE: First we need to get the script directory. In order
+         ;; to change execute/workspace directory to the current target script's
+         ;; directory path.
+         (script-dir (f-dirname in-op))
+         ;; NOTE: Change the current execute/workspace directory
+         ;; to the script directory temporary. So the script will execute
+         ;; within the current directory the script is currently in.
+         ;;
+         ;; Without these lines of code, the script will execute in the
+         ;; `default-directory' variables. The `default-directory' variables
+         ;; will be the directory path where you start the Emacs. For instance,
+         ;; if you start Emacs at path `/usr/home', then the default directory
+         ;; will be at `usr/home' directory.
+         ;;
+         ;; Adding these lines of code if your scirpt is at `/usr/home/project/some-script.sh',
+         ;; Then your `default-directory' became `usr/home/project'. Hurray!
+         (default-directory script-dir))
+    ;; Compile/Execute the target script.
+    (compile in-op)
+    (jcs-update-line-number-each-window)
+    (with-current-buffer "*compilation*"
+      (rename-buffer (format "*compilation*: %s" (f-filename in-op)) t))
+    (message "Executing script file: '%s'" in-op)))
 
 
 ;;;###autoload
