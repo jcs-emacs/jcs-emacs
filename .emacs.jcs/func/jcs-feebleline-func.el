2a88acfe119b3cf87bb4d5fa97403b5e2a7974d6
Jen-Chieh <jcs090218@gmail.com>
2019-11-28 11:46:39 +0800

    Upgrade to boolean control feebleline.

--- .emacs.jcs/func/jcs-feebleline-func.el
+++ .emacs.jcs/func/jcs-feebleline-func.el
@@ -3,6 +3,10 @@
 ;;; Code:
 
 
+(require 'ffmpeg-player)
+(require 'show-eol)
+
+
 (defface jcs--feebleline-read-only--enabled
   '((t (:foreground "#FF0000")))
   "Ready only symbol face when active.."
@@ -23,25 +27,54 @@
                         jcs--feebleline-read-only--enabled
                       jcs--feebleline-read-only--disabled)))
 
+(defun jcs--feebleline--major-mode ()
+  "Feebleline major mode."
+  (if jcs-feebleline-show-major-mode
+      (format "[%s]" (jcs-current-major-mode))
+    ""))
+
 (defun jcs--feebleline--project-name ()
   "Feebleline project name."
-  (let ((project-root (cdr (project-current))))
-    (if (and project-root
-             (buffer-file-name))
-        (file-name-nondirectory (directory-file-name project-root))
-      "¥")))
+  (if jcs-feebleline-show-project-name
+      (let ((project-root (cdr (project-current))))
+        (format "{ %s }"
+                (if (and project-root (buffer-file-name))
+                    (file-name-nondirectory (directory-file-name project-root))
+                  "¥")))
+    ""))
 
-(defun jcs--feebleline--coding-system ()
-  "Feebleline coding system."
-  buffer-file-coding-system)
+(defun jcs--feebleline--buffer-name ()
+  "Feebleline buffer name."
+  (if jcs-feebleline-show-buffer-name
+      (format "%s%s" (if (feebleline-file-modified-star)
+                         (format "%s " (feebleline-file-modified-star))
+                       "")
+              (buffer-name))
+    ""))
 
-(defun jcs--feebleline--spc/tab ()
+(defun jcs--feebleline--coding-system-and-line-endings ()
+  "Feebleline coding system and line endings."
+  (if jcs-feebleline-show-coding-system-and-line-endings
+      (format "[%s : %s]" buffer-file-coding-system (show-eol-get-eol-mark-by-system))
+    ""))
+
+(defun jcs--feebleline--spc/tab-and-width ()
   "Feebleline spaces or tabs."
-  (format "%s : %s" (jcs-buffer-spaces-to-tabs) (jcs-get-tab-width-by-mode)))
+  (if jcs-feebleline-show-spc/tab-and-width
+      (format "[%s : %s]" (jcs-buffer-spaces-to-tabs) (jcs-get-tab-width-by-mode))
+    ""))
+
+(defun jcs--feebleline--line/column ()
+  "Feebleline show line numbers and column numbers."
+  (if jcs-feebleline-show-line/column
+      (format "[%s : %s]" (feebleline-line-number) (feebleline-column-number))
+    ""))
 
 (defun jcs--feebleline--time ()
   "Feebleline time."
-  (format-time-string "[%Y-%m-%d %H:%M:%S]"))
+  (if jcs-feebleline-show-time
+      (format-time-string "[%Y-%m-%d %H:%M:%S]")
+    ""))
 
 
 (provide 'jcs-feebleline-func)
