15708cddcb0123bfd58ae45aa4b309325923ea01
jenchieh <jcs090218@gmail.com>
2019-12-01 18:58:17 +0800

    Quick fixed for helm incompatbility issue.

--- .emacs.jcs/jcs-plugin.el
+++ .emacs.jcs/jcs-plugin.el
@@ -361,6 +361,10 @@
         helm-scroll-amount                    8
         helm-ff-file-name-history-use-recentf t)
 
+  (setq completion-styles (if (version<= "27.1" emacs-version)
+                              '(flex)
+                            '(helm-flex)))
+
   ;; NOTE: Make Helm window at the bottom WITHOUT using any extra package.
   ;; SOURCE: https://www.reddit.com/r/emacs/comments/345vtl/make_helm_window_at_the_bottom_without_using_any/
   (add-to-list 'display-buffer-alist
