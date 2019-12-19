4d8479b5be37e6f9fc57a19d3716f5952cf38f90
Jen-Chieh <jcs090218@gmail.com>
2019-12-19 16:26:26 +0800

    [NS] theme.

--- .emacs.jcs/jcs-theme.el
+++ .emacs.jcs/jcs-theme.el
@@ -19,6 +19,7 @@
 
 (defun jcs-powerline-set-theme-faces (ac-lst inac-lst)
   "Set `powerline' them faces with AC-LST and INAC-LST."
+  (require 'powerline)
   (progn
     (set-face-foreground 'powerline-active0 (nth 0 ac-lst))
     (set-face-background 'powerline-active0 (nth 1 ac-lst))
