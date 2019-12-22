3ccb4dabd3383a37afac2cdd4843396abc0ee541
jenchieh <jcs090218@gmail.com>
2019-12-22 22:31:56 +0800

    Add peek frame.

--- .emacs.jcs/func/jcs-frame.el
+++ .emacs.jcs/func/jcs-frame.el
@@ -30,15 +30,25 @@ FNC : Callback apply to each windows."
         (setq index (+ index 1)))
       (select-frame-set-input-focus cur-frame))))
 
-(defun jcs-make-frame-simple (name width height fnc &rest args)
+(defun jcs-max-frame-width ()
+  "Find the largest frame width."
+  (let ((fw (frame-width)))
+    (dolist (fm (frame-list))
+      (when (< fw (frame-width fm))
+        (setq fw (frame-width fm))))
+    fw))
+
+(defun jcs-make-frame-simple (name x y width height fnc &rest)
   "Make frame with a bunch of default variables set.
-You will only have to fill in NAME, WIDTH, HEIGHT, FNC and ARGS."
-  (let ((doc-frame nil) (pixel-x nil) (pixel-y nil) (cp-buf nil)
+You will only have to fill in NAME, X, Y, WIDTH, HEIGHT and FNC."
+  (let ((doc-frame nil) (pixel-x x) (pixel-y y) (cp-buf nil)
         (abs-pixel-pos (window-absolute-pixel-position)))
-    (setq pixel-x (car abs-pixel-pos))
-    (setq pixel-y (+ (cdr abs-pixel-pos) (frame-char-height)))
+    (unless pixel-x (setq pixel-x (car abs-pixel-pos)))
+    (unless pixel-y (setq pixel-y (+ (cdr abs-pixel-pos) (frame-char-height))))
+
     (setq doc-frame
-          (make-frame (list (cons 'minibuffer nil)
+          (make-frame (list (cons 'parent-frame (window-frame))
+                            (cons 'minibuffer nil)
                             (cons 'name name)
                             (cons 'width width)
                             (cons 'height height)
@@ -46,21 +56,44 @@ You will only have to fill in NAME, WIDTH, HEIGHT, FNC and ARGS."
                             (cons 'parent-frame t)
                             (cons 'fullscreen nil)
                             (cons 'no-other-frame t)
-                            (cons 'skip-taskbar t))))
+                            (cons 'skip-taskbar t)
+                            (cons 'vertical-scroll-bars nil)
+                            (cons 'horizontal-scroll-bars nil)
+                            (cons 'menu-bar-lines 0)
+                            (cons 'tool-bar-lines 0)
+                            (cons 'no-accept-focus t)
+                            (cons 'no-special-glyphs t)
+                            (cons 'no-other-frame t)
+                            (cons 'cursor-type 'hollow)
+                            ;; Do not save child-frame when use desktop.el
+                            (cons 'desktop-dont-save t))))
 
     (with-selected-frame doc-frame
       ;; Force one window only.
       (while (not (= (length (window-list)) 1)) (delete-window))
+      (when (functionp fnc) (funcall fnc)))
 
-      (apply fnc args)
-
-      ;; Set x and y position.
-      (set-frame-parameter nil 'left pixel-x)
-      (set-frame-parameter nil 'top pixel-y))
+    ;; Set x and y position.
+    (set-frame-parameter nil 'left pixel-x)
+    (set-frame-parameter nil 'top pixel-y)
 
     ;; Make frame visible again
     (make-frame-visible doc-frame)
     doc-frame))
+
+(defvar jcs-peek-frame nil
+  "Record the peek frame, only allows one at a time.")
+
+(defun jcs-create-peek-frame (name fnc)
+  "Create the peek frame with NAME and FNC."
+  (when jcs-peek-frame (delete-frame jcs-peek-frame))
+  (let ((spawn-pt (window-absolute-pixel-position (jcs-column-to-point 0))))
+    (setq jcs-peek-frame
+          (jcs-make-frame-simple
+           name
+           (- (car spawn-pt) 4) nil
+           (window-width) 15
+           fnc))))
 
 
 (provide 'jcs-frame)
