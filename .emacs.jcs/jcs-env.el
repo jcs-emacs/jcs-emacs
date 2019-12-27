d2c4a968de53b1a3984f8dd5fda677d76e7f2b75
Jen-Chieh Shen <jcs090218@gmail.com>
2019-12-27 15:47:31 +0800

    Decrease level to 20.

--- .emacs.jcs/jcs-env.el
+++ .emacs.jcs/jcs-env.el
@@ -181,7 +181,7 @@
 Hence the user can see the error/operation message.")
 
 ;;; Multiple Cursors
-(defvar jcs-mc/string-distance-level 30
+(defvar jcs-mc/string-distance-level 20
   "The standard similarity, the lower require more precision.")
 
 ;;; Previous/Next keys
