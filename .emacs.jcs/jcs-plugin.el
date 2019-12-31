9a970809f4794e8f402f50e55d0801c4890566d6
Jen-Chieh Shen <jcs090218@gmail.com>
2019-12-31 11:07:07 +0800

    Added company for lsp?

--- .emacs.jcs/jcs-plugin.el
+++ .emacs.jcs/jcs-plugin.el
@@ -202,7 +202,8 @@
   (setq diminish-buffer-list '("[*]helm" "[*]esup-" "[*]quelpa-"
                                "[*]compilation" "[*]output"
                                "[*]Async Shell Command[*]:" "[*]shell" "[*]eshell"
-                               "[*]lsp-" "[*][a-zA-Z0-9]+-ls"))
+                               "[*]lsp-" "[*][a-zA-Z0-9]+-ls"
+                               "[*]company"))
   (with-eval-after-load 'jcs-buffer-menu
     (diminish-buffer-mode 1))
   :config
