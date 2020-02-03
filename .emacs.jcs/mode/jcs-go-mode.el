12598289de0a21ea06e9ea526da556392a432315
JenChieh <jcs090218@gmail.com>
2020-02-03 15:25:50 +0800

    Use multi-line comments.

--- .emacs.jcs/mode/jcs-go-mode.el
+++ .emacs.jcs/mode/jcs-go-mode.el
@@ -9,9 +9,15 @@
 (defun jcs-go-mode-hook ()
   "Go mode hook."
 
+  (jcs-use-cc-mutliline-comment)
+
   ;; File Header
   (jcs-insert-header-if-valid '("[.]go")
-                              'jcs-insert-go-template))
+                              'jcs-insert-go-template)
+
+  ;; comment block
+  (define-key go-mode-map (kbd "RET") #'jcs-smart-context-line-break)
+  (define-key go-mode-map (kbd "*") #'jcs-c-comment-pair))
 
 (add-hook 'go-mode-hook 'jcs-go-mode-hook)
 
