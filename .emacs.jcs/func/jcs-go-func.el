118394ccb65a4fb988e0477c81123dd6b0506987
Jen-Chieh <jcs090218@gmail.com>
2020-02-10 15:16:49 +0800

    Add go func.

---
+++ .emacs.jcs/func/jcs-go-func.el
@@ -0,0 +1,27 @@
+;;; jcs-go-func.el --- Go related.  -*- lexical-binding: t -*-
+;;; Commentary: When editing the Go related file.
+;;; Code:
+
+
+;;;###autoload
+(defun jcs-go-maybe-insert-codedoc ()
+  "Insert common Go document/comment string."
+  (interactive)
+  (insert "/")
+  (let ((active-comment nil) (next-line-not-empty nil))
+    (save-excursion
+      (backward-char 1)
+      (when (jcs-current-char-equal-p "/")
+        (backward-char 1)
+        (when (jcs-is-beginning-of-line-p)
+          (setq active-comment t)))
+      (jcs-next-line)
+      (unless (jcs-current-line-empty-p) (setq next-line-not-empty t)))
+
+    (when (and active-comment next-line-not-empty)
+      (jcs-insert-comment-style-by-current-line "[{]")
+      (end-of-line))))
+
+
+(provide 'jcs-go-func)
+;;; jcs-go-func.el ends here
