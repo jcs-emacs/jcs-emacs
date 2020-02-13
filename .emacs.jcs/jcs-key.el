18bed4d58f94b08081b5a2c2774f8815845191e3
Jen-Chieh <jcs090218@gmail.com>
2020-02-13 17:51:56 +0800

    Impls neotree instead of speedbar.

--- .emacs.jcs/jcs-key.el
+++ .emacs.jcs/jcs-key.el
@@ -231,12 +231,8 @@
   (define-key esup-mode-map (kbd "C-y") #'undo-tree-redo))
 
 ;;; File Explorer
-(define-key global-map (kbd "C-M-l") #'jcs-sr-speedbar-toggle)  ; Compatible to `Visual Studio'.
-(define-key global-map (kbd "C-b") #'jcs-sr-speedbar-toggle)    ; Compatible to `VS Code'.
-(with-eval-after-load 'sr-speedbar
-  (define-key speedbar-mode-map (kbd "<backspace>") #'speedbar-up-directory)
-  (define-key speedbar-mode-map (kbd "<return>") #'jcs-speedbar-edit-line)
-  (define-key speedbar-mode-map (kbd "<f2>") #'speedbar-item-rename))
+(define-key global-map (kbd "C-M-l") #'neotree-toggle)  ; Compatible to `Visual Studio'.
+(define-key global-map (kbd "C-b") #'neotree-toggle)    ; Compatible to `VS Code'.
 
 ;;; File editing
 (define-key global-map (kbd "C-<f4>") #'kill-this-buffer)
