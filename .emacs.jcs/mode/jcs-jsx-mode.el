1b99ad10c16f1f681a047a3fb599cb09e1fb1025
jenchieh <jcs090218@gmail.com>
2020-05-19 01:06:22 +0800

    Supply better jsx.

--- .emacs.jcs/mode/jcs-jsx-mode.el
+++ .emacs.jcs/mode/jcs-jsx-mode.el
@@ -3,15 +3,29 @@
 ;;; Code:
 
 (require 'rjsx-mode)
+(require 'web-mode)
+(require 'emmet-mode)
 
 (defun jcs-jsx-mode-hook ()
   "Mode hook for JSX mode."
 
   ;; File Header
   (jcs-insert-header-if-valid '("[.]jsx$")
-                              'jcs-insert-jsx-template))
+                              'jcs-insert-jsx-template)
+
+  ;; Normal
+  (define-key rjsx-mode-map (kbd "C-v") #'jcs-web-yank)
+  (define-key rjsx-mode-map (kbd "RET") #'jcs-web-return-key)
+
+  (define-key rjsx-mode-map (kbd "{") #'jcs-web-vs-opening-curly-bracket-key)
+  (define-key rjsx-mode-map (kbd "}") #'jcs-vs-closing-curly-bracket-key)
+  (define-key rjsx-mode-map (kbd ";") #'jcs-vs-semicolon-key)
+
+  ;; Emmet
+  (define-key emmet-mode-keymap (kbd "C-<return>") #'jcs-emmet-expand-line))
 
 (add-hook 'rjsx-mode-hook 'jcs-jsx-mode-hook)
+(add-hook 'rjsx-mode-hook 'emmet-mode)
 
 (provide 'jcs-jsx-mode)
 ;;; jcs-jsx-mode.el ends here
