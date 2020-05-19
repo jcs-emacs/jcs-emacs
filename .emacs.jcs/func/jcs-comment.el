fe5fa18d2ec01a6d934b0b237590a8e2064553ea
Jen-Chieh <jcs090218@gmail.com>
2020-05-19 11:37:16 +0800

    Cleanup comment module.

--- .emacs.jcs/func/jcs-comment.el
+++ .emacs.jcs/func/jcs-comment.el
@@ -4,8 +4,7 @@
 
 (defun jcs-triple-char-comment-prefix-p (in-char)
   "Check if current line is a triple IN-CHAR style comment prefix.
-For instance, `///', `---', etc.  Those are all triple char style comment prefix.
-IN-CHAR : input triple char."
+For instance, `///', `---', etc."
   (save-excursion
     (let ((is-comment-prefix nil))
       (jcs-goto-first-char-in-line)
@@ -20,8 +19,7 @@ IN-CHAR : input triple char."
 
 (defun jcs-tripple-char-comment-prefix-at-current-point-p (in-char)
   "Check if the current point is triple IN-CHAR style comment prefix.
-For instance, `///', `---', etc.  Those are all triple char style comment prefix.
-IN-CHAR : input triple char."
+For instance, `///', `---', etc."
   (save-excursion
     (let ((is-comment-prefix-at-point nil))
       (when (jcs-current-char-equal-p in-char)
@@ -32,149 +30,120 @@ IN-CHAR : input triple char."
             (setq is-comment-prefix-at-point t))))
       is-comment-prefix-at-point)))
 
-(defun jcs-do-doc-string ()
-  "Check if should insert the doc string by checking only \
-comment character on the same line."
-  (let ((do-doc-string t))
-    (jcs-goto-first-char-in-line)
-
-    (while (not (jcs-is-end-of-line-p))
-      (forward-char 1)
-      (when (and (not (jcs-current-char-equal-p " "))
-                 (not (jcs-current-char-equal-p "\t"))
-                 (not (jcs-current-char-equal-p "*"))
-                 (not (jcs-current-char-equal-p "/")))
-        ;; return false.
-        (setq do-doc-string nil)
-        do-doc-string))
-
-    ;; return true.
-    do-doc-string))
+(defun jcs-do-doc-string-p ()
+  "Check if able to insert docstring by checking current line with only comments."
+  (save-excursion
+    (let ((do-doc-string t))
+      (jcs-goto-first-char-in-line)
+      (while (not (jcs-is-end-of-line-p))
+        (forward-char 1)
+        (unless (jcs-current-char-string-match-p "[ \t*/]")
+          ;; return false.
+          (setq do-doc-string nil)
+          do-doc-string))
+      ;; return true.
+      do-doc-string)))
 
 ;;;###autoload
 (defun jcs-smart-context-line-break ()
   "Comment block."
   (interactive)
-  (let ((last nil)
-        (point-beginning-of-line nil) (point-end-of-line nil)
-        (start-of-global-comment-doc nil))
-
-    ;; start position
-    (setq last (point))
-
-    ;; record down the beginning of the line position.
-    (beginning-of-line)
-    (setq point-beginning-of-line (point))
-
-    ;; record down the end of the line position.
-    (end-of-line)
-    (setq point-end-of-line (point))
-
-    ;; back to original position
-    (goto-char last)
-
+  (let ((start-of-global-comment-doc nil)
+        (able-insert-docstring nil))
     ;; check if inside the comment block.
-    (if (jcs-is-inside-comment-block-p)
-        (progn
-          (setq last (point))
+    (if (not (jcs-inside-comment-block-p))
+        ;; else insert new line
+        (newline-and-indent)
 
-          (setq start-of-global-comment-doc nil)
+      (setq start-of-global-comment-doc nil)
 
-          ;; check the '/*' and '*/' on the same line?
-          (if (and (search-backward "/*" point-beginning-of-line t)
-                   (search-forward "*/" point-end-of-line t)
-                   (jcs-do-doc-string))
-              (progn
-                (setq start-of-global-comment-doc t)
+      (setq able-insert-docstring
+            (and (save-excursion (search-backward "/*" (line-beginning-position) t))
+                 (save-excursion (search-forward "*/" (line-end-position) t))
+                 (jcs-do-doc-string-p)))
 
-                (goto-char last)
-
-                (insert "\n* ")
-                (indent-for-tab-command)
-
-                (insert "\n")
-                (indent-for-tab-command)
-
-                ;; back one line up
-                (jcs-previous-line)
-
-                ;; Insert comment string here..
-                (when (and (functionp 'jcs-docstring-modes-p)
-                           (jcs-docstring-modes-p))
-                  (jcs-insert-comment-string))
-
-                ;; goto the end of line
-                (end-of-line))
-            (goto-char last)
-
+      ;; check the '/*' and '*/' on the same line?
+      (if (not able-insert-docstring)
+          (progn
             (insert "\n")
-            (when (jcs-is-inside-comment-block-p)
+            (when (jcs-inside-comment-block-p)
               (insert "* "))
-
             (indent-for-tab-command))
+        (setq start-of-global-comment-doc t)
 
-          (unless start-of-global-comment-doc
-            (let ((is-global-comment-doc nil))
-              (save-excursion
-                (jcs-goto-start-of-the-comment)
-                (forward-char 1)
-                (when (jcs-current-char-equal-p "*")
-                  (setq is-global-comment-doc t)))
+        (insert "\n* ")
+        (indent-for-tab-command)
 
-              (unless is-global-comment-doc
-                (let (;; Check if the next line is the doc string
-                      ;; comment line.
-                      (is-next-line-doc-string-comment-line nil)
-                      ;; When we break a line and there are still
-                      ;; some content on the right.
-                      (line-have-content-on-right nil))
-                  (cond
-                   ((jcs-is-current-major-mode-p "csharp-mode")
-                    (save-excursion
-                      (when (not (jcs-current-line-comment-p))
-                        (setq line-have-content-on-right t)))
+        (insert "\n")
+        (indent-for-tab-command)
 
+        ;; back one line up
+        (jcs-previous-line)
+
+        ;; Insert comment string here..
+        (when (and (functionp 'jcs-docstring-modes-p) (jcs-docstring-modes-p))
+          (jcs-insert-comment-string))
+
+        ;; goto the end of line
+        (end-of-line))
+
+      (unless start-of-global-comment-doc
+        (let ((is-global-comment-doc nil))
+          (save-excursion
+            (jcs-goto-start-of-the-comment)
+            (forward-char 1)
+            (when (jcs-current-char-equal-p "*")
+              (setq is-global-comment-doc t)))
+
+          (unless is-global-comment-doc
+            (let (;; Check if the next line is the doc string
+                  ;; comment line.
+                  (is-next-line-doc-string-comment-line nil)
+                  ;; When we break a line and there are still
+                  ;; some content on the right.
+                  (line-have-content-on-right nil))
+              (cond
+               ((jcs-is-current-major-mode-p "csharp-mode")
+                (save-excursion
+                  (when (not (jcs-current-line-comment-p))
+                    (setq line-have-content-on-right t)))
+
+                (save-excursion
+                  (jcs-previous-line)
+                  (if (jcs-vs-csharp-only-vs-comment-prefix-this-line-p)
+                      (when line-have-content-on-right
+                        (setq is-next-line-doc-string-comment-line t))
+                    (when (jcs-vs-csharp-comment-prefix-p)
+                      (setq is-next-line-doc-string-comment-line t))))
+
+                ;; If we still not sure to insert docstring comment line yet.
+                ;; Then we need to do deeper check.
+                (unless is-next-line-doc-string-comment-line
+                  (let ((prev-line-vs-prefix nil) (next-line-vs-prefix nil))
                     (save-excursion
                       (jcs-previous-line)
+                      (when (jcs-vs-csharp-comment-prefix-p)
+                        (setq prev-line-vs-prefix t)))
 
-                      (if (not (jcs-vs-csharp-only-vs-comment-prefix-this-line-p))
-                          (when (jcs-vs-csharp-comment-prefix-p)
-                            (setq is-next-line-doc-string-comment-line t))
-                        (when line-have-content-on-right
-                          (setq is-next-line-doc-string-comment-line t))))
+                    ;; Only when previous have prefix.
+                    (when prev-line-vs-prefix
+                      (save-excursion
+                        (jcs-next-line)
+                        (when (jcs-vs-csharp-comment-prefix-p)
+                          (setq next-line-vs-prefix t)))
 
-                    ;; If we still not sure to insert docstring comment
-                    ;; line yet. Then we need to do deeper check.
-                    (when (not is-next-line-doc-string-comment-line)
-                      (let ((prev-line-vs-prefix nil)
-                            (next-line-vs-prefix nil))
-                        (save-excursion
-                          (jcs-previous-line)
-                          (when (jcs-vs-csharp-comment-prefix-p)
-                            (setq prev-line-vs-prefix t)))
+                      (when (and prev-line-vs-prefix next-line-vs-prefix)
+                        (setq is-next-line-doc-string-comment-line t)))))
 
-                        ;; Only when previous have prefix.
-                        (when prev-line-vs-prefix
-                          (save-excursion
-                            (jcs-next-line)
-                            (when (jcs-vs-csharp-comment-prefix-p)
-                              (setq next-line-vs-prefix t)))
-
-                          (when (and prev-line-vs-prefix
-                                     next-line-vs-prefix)
-                            (setq is-next-line-doc-string-comment-line t)))))
-
-                    ;; Is doc-string comment line. Insert
-                    ;; doc-string comment.
-                    (when is-next-line-doc-string-comment-line
-                      (insert "/// ")))
-                   ((jcs-is-current-major-mode-p "lua-mode")
-                    ;; Just insert for Lua.
-                    ;; Lua does not have issue like CSharp.
-                    (insert "-- ")))
-                  (indent-for-tab-command))))))
-      ;; else insert new line
-      (newline-and-indent))))
+                ;; Is doc-string comment line. Insert
+                ;; doc-string comment.
+                (when is-next-line-doc-string-comment-line
+                  (insert "/// ")))
+               ((jcs-is-current-major-mode-p "lua-mode")
+                ;; Just insert for Lua.
+                ;; Lua does not have issue like CSharp.
+                (insert "-- ")))
+              (indent-for-tab-command))))))))
 
 
 ;;;###autoload
@@ -182,16 +151,11 @@ comment character on the same line."
   "Auto pair c style comment block."
   (interactive)
   (let ((insert-pair nil))
-    (save-excursion
-      (ignore-errors
-        (when (jcs-current-char-equal-p "/")
-          (setq insert-pair t))))
-
+    (when (jcs-current-char-equal-p "/")
+      (setq insert-pair t))
     (insert "*")
-
     (save-excursion
-      (when (and insert-pair
-                 (jcs-is-behind-last-char-at-line-p))
+      (when (and insert-pair (jcs-is-behind-last-char-at-line-p))
         (insert "*/")))))
 
 (defun jcs-insert-comment-string ()
@@ -205,12 +169,8 @@ comment character on the same line."
     (jcs-insert-comment-style-by-current-line "[{;]")))
 
 (defun jcs-comment-or-uncomment-region (fn start end)
-  "Comment or uncomment region.
-FN : function name.
-START : start region.
-END : end region."
-  (let ((rb start)
-        (re end)
+  "Comment or uncomment region, with FN, START and END."
+  (let ((rb start) (re end)
         (rel (line-number-at-pos end)))
     (save-excursion
       (goto-char rb)
@@ -224,11 +184,10 @@ END : end region."
               (unless (jcs-is-infront-first-char-at-line-p)
                 (push (line-number-at-pos) line-reminder--change-lines))
             (push (line-number-at-pos) line-reminder--change-lines)))
-        (next-line 1)
+        (forward-line 1)
         (beginning-of-line))))
 
-  (let ((line-reminder--change-lines)
-        (line-reminder--saved-lines))
+  (let ((line-reminder--change-lines) (line-reminder--saved-lines))
     (funcall fn start end))
 
   (line-reminder--mark-buffer))
