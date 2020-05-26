f66c237769bba4fa7c67f1239e01c7f65f1e275a
Jen-Chieh <jcs090218@gmail.com>
2020-05-26 15:02:42 +0800

    Impls ask unreal c++ template.

--- .emacs.jcs/mode/jcs-c++-mode.el
+++ .emacs.jcs/mode/jcs-c++-mode.el
@@ -4,11 +4,32 @@
 
 (require 'jcs-cc-mode)
 
+(defun jcs-c++-unreal-insert-header ()
+  "Insert the Unreal C++ header depends on if is a header/source file."
+  (let ((header-ext (append jcs-c++-header-extensions jcs-c-header-extensions))
+        (source-ext (append jcs-c++-source-extensions jcs-c-source-extensions)))
+    (jcs-insert-header-if-valid header-ext 'jcs-insert-c++-unreal-header-template)
+    (jcs-insert-header-if-valid source-ext 'jcs-insert-c++-unreal-source-template)))
+
+;;;###autoload
+(defun jcs-c++-ask-source (sc)
+  "Ask the source SC for editing C++ file."
+  (interactive
+   (list (completing-read
+          "Major source for this C++ file: " '("Default"
+                                               "Unreal Scripting"))))
+  (cond ((string= sc "Default") (jcs-cc-insert-header))
+        ((string= sc "Unreal Scripting") (jcs-c++-unreal-insert-header))))
+
+;;----------------------------------------------------------------------------
+
 (defun jcs-c++-mode-hook ()
   "C++ mode handling"
 
   ;; File Header
-  (jcs-cc-insert-header)
+  (let ((ext-lst (append jcs-c-header-extensions jcs-c-source-extensions
+                         jcs-c++-header-extensions jcs-c++-source-extensions)))
+    (jcs-insert-header-if-valid ext-lst 'jcs-c++-ask-source t))
 
   ;; Normal
   (define-key c++-mode-map [f8] #'jcs-find-corresponding-file)
