b9d6da76e4ac908853c1fa47e3e323a54e25b0e1
jenchieh <jcs090218@gmail.com>
2020-06-05 16:53:53 +0800

    Update dep.

--- .emacs.d/.jcs-custom.el
+++ .emacs.d/.jcs-custom.el
@@ -3,30 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auto-save-default nil)
- '(auto-save-interval 0)
- '(auto-save-list-file-prefix nil)
- '(auto-save-timeout 0)
- '(auto-show-mode t t)
- '(delete-auto-save-files nil)
- '(delete-old-versions 'other)
- '(flymake-google-cpplint-command "C:/jcs_ide_packages/jcs_win7_packages/cpplint/cpplint.exe")
- '(httpd-port 8877)
- '(imenu-auto-rescan t)
- '(imenu-auto-rescan-maxout 500000)
- '(jdee-jdk-registry '(("1.8.0_111" . "C:/Program Files/Java/jdk1.8.0_111")))
- '(kept-new-versions 5)
- '(kept-old-versions 5)
- '(make-backup-file-name-function 'ignore)
- '(make-backup-files nil)
- '(mouse-wheel-follow-mouse nil)
- '(mouse-wheel-progressive-speed nil)
- '(mouse-wheel-scroll-amount '(15))
  '(package-selected-packages
-   '(mmm-mode yasnippet-snippets vue-mode rjsx-mode ivy counsel-projectile counsel swiper bui dashboard-ls gdscript-mode emoji-github reload-emacs multi-shell jayces-mode file-header lsp-java lsp-origami buffer-wrap manage-minor-mode-table magit masm-mode quelpa company-quickhelp-terminal company-emoji emojify ffmpeg-player flycheck command-log-mode lsp-treemacs flycheck-popup-tip flycheck-pos-tip flycheck-grammarly neotree vs-dark-theme vs-light-theme undo-tree lsp-mode company-lsp dap-mode lsp-ui manage-minor-mode tabulated-list-wrap pfuture define-it lua-mode lv smex parse-it treemacs pos-tip hydra emms dash-functional julia-mode reveal-in-folder websocket edit-indirect elisp-def request wiki-summary projectile visual-regexp kotlin-mode dockerfile-mode package-lint ess diminish-buffer markdown-toc browse-kill-ring org-bullets ht company-fuzzy flx go-mode feebleline line-reminder indicators centaur-tabs highlight-indent-guides use-ttf alt-codes preproc-font-lock rainbow-mode haxe-mode dumb-jump ssass-mode region-occurrences-highlighter yascroll esup swift-mode dart-mode basic-mode google-this right-click-context show-eol csharp-mode isearch-project async hl-todo page-break-lines goto-char-preview elixir-mode erlang multiple-cursors buffer-move dashboard glsl-mode company-quickhelp origami dash git-commit move-text cmake-font-lock restart-emacs goto-line-preview transient with-editor ini-mode htmltagwrap auto-rename-tag cobol-mode rust-mode yaml-mode markdown-mode nhexl-mode clojure-mode javadoc-lookup typescript-mode yasnippet xcscope wgrep-ag wgrep vimrc-mode sql-indent simple-httpd scss-mode s popup pkg-info json-snatcher json-reformat htmlize google-translate f epl emmet-mode diminish cmake-mode bind-key avy ace-window js2-mode company apache-mode which-key web-mode use-package tree-mode togetherly shader-mode scala-mode python-mode project-abbrev processing-mode powerline package-build organize-imports-java nasm-mode json-mode impatient-mode iedit haskell-mode gitignore-mode gitconfig-mode gitattributes-mode exec-path-from-shell com-css-sort auto-highlight-symbol ag adaptive-wrap actionscript-mode))
- '(paradox-github-token t)
- '(send-mail-function 'mailclient-send-it)
- '(version-control nil))
+   '(lsp-mode reload-emacs multi-shell jayces-mode file-header yasnippet-snippets yasnippet yascroll yaml-mode which-key wgrep-ag web-mode impatient-mode vue-mode vs-light-theme vs-dark-theme visual-regexp vimrc-mode use-ttf use-package undo-tree typescript-mode togetherly swift-mode sql-indent smex scss-mode ssass-mode show-eol shader-mode scala-mode rust-mode rjsx-mode right-click-context reveal-in-folder restart-emacs region-occurrences-highlighter rainbow-mode quelpa python-mode project-abbrev processing-mode preproc-font-lock parse-it package-lint package-build organize-imports-java org-bullets nhexl-mode neotree nasm-mode multiple-cursors move-text masm-mode markdown-toc manage-minor-mode-table manage-minor-mode magit lua-mode lsp-ui lsp-origami lsp-java line-reminder kotlin-mode json-mode js2-mode javadoc-lookup isearch-project ini-mode indicators iedit htmltagwrap hl-todo highlight-indent-guides haxe-mode haskell-mode goto-line-preview goto-char-preview google-this go-mode glsl-mode gitignore-mode gitconfig-mode gitattributes-mode gdscript-mode flycheck-pos-tip flycheck-popup-tip flycheck-grammarly flx ffmpeg-player feebleline exec-path-from-shell esup ess erlang emoji-github emmet-mode elixir-mode elisp-def dumb-jump dockerfile-mode diminish-buffer diminish define-it dashboard-ls dashboard dart-mode dap-mode csharp-mode counsel-projectile counsel company-quickhelp-terminal company-quickhelp company-lsp company-fuzzy company-emoji company command-log-mode com-css-sort cobol-mode cmake-font-lock clojure-mode centaur-tabs browse-kill-ring buffer-wrap buffer-move basic-mode auto-rename-tag auto-highlight-symbol apache-mode alt-codes ag adaptive-wrap actionscript-mode ace-window)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
