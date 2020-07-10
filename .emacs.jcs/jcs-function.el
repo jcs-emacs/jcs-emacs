52ee3755ff458dc2ab12c70a8ac89315ea07f42f
Jen-Chieh <jcs090218@gmail.com>
2020-07-10 15:01:42 +0800

    Simplify re-builder functions.

--- .emacs.jcs/jcs-function.el
+++ .emacs.jcs/jcs-function.el
@@ -494,30 +494,18 @@ LST-PR: List of pair."
   "Kill this buffer in `re-builder' mode."
   (interactive)
   (let ((is-killed nil))
-    ;; maybe kill this buffer.
     (setq is-killed (jcs-maybe-kill-this-buffer))
-    (when is-killed
-      ;; then delete this window.
-      (delete-window))))
+    (when is-killed (delete-window))))
 
 ;;;###autoload
 (defun jcs-re-builder (type)
-  "Rewrap `re-builder' function.
-TYPE : enable/disable case sensitive?"
+  "Rewrap `re-builder' function to ask search case TYPE."
   (interactive
    (list (completing-read
           "Enable case sensitive?" '("Case Sensitive"
                                      "Case Insensitive"))))
-
-  (if (string= type "Case Sensitive")
-      (setq case-fold-search nil)
-    (setq case-fold-search t))  ;; This is default.
-
-  ;; Start `RE-Builder' mode.
-  (re-builder)
-
-  ;; Set back to default.
-  (setq case-fold-search t))
+  (let ((case-fold-search (string= type "Case Insensitive")))
+    (re-builder)))
 
 ;;----------------------------------------------------------------------------
 ;; Shift Select
