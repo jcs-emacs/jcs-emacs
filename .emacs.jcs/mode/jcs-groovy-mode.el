762120197db99eceab8f2c83148bcd3fb2633b73
jenchieh <jcs090218@gmail.com>
2020-07-21 22:55:30 +0800

    Added groovy mode.

---
+++ .emacs.jcs/mode/jcs-groovy-mode.el
@@ -0,0 +1,22 @@
+;;; jcs-groovy-mode.el --- Groovy mode. -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(require 'groovy-mode)
+
+(defun jcs-groovy-mode-hook ()
+  "Hook for `groovy-mode'."
+
+  ;; File Header
+  (jcs-insert-header-if-valid '("[.]groovy"
+                                "[.]gradle")
+                              'jcs-insert-groovy-template)
+
+  ;; comment block
+  (define-key groovy-mode-map (kbd "RET") #'jcs-smart-context-line-break)
+  (define-key groovy-mode-map (kbd "*") #'jcs-c-comment-pair))
+
+(add-hook 'groovy-mode-hook 'jcs-groovy-mode-hook)
+
+(provide 'jcs-groovy-mode)
+;;; jcs-groovy-mode.el ends here
