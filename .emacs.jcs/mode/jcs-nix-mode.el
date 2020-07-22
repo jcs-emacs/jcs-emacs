682c276b0a711809fe304da78be70a13926bf147
jenchieh <jcs090218@gmail.com>
2020-07-23 03:39:41 +0800

    Added custom nix mode file.

---
+++ .emacs.jcs/mode/jcs-nix-mode.el
@@ -0,0 +1,17 @@
+;;; jcs-nix-mode.el --- Nix Expressions. -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(require 'nix-mode)
+
+(defun jcs-nix-mode-hook ()
+  "Hook for `nix-mode'."
+
+  ;; File Header
+  (jcs-insert-header-if-valid '("[.]nix")
+                              'jcs-insert-nix-template))
+
+(add-hook 'nix-mode-hook 'jcs-nix-mode-hook)
+
+(provide 'jcs-nix-mode)
+;;; jcs-nix-mode.el ends here
