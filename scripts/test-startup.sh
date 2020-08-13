d6e1d4df45de17d0884707cd4903f1ca77c53321
Jen-Chieh <jcs090218@gmail.com>
2020-08-13 11:54:07 +0800

    Rename startup.

---
+++ scripts/test-startup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+echo "Attempting startup..."
+
+mv -f ./build.el ~/build.el
+mv -f ./.emacs ~/.emacs
+mv -f ./.emacs.d ~/.emacs.d
+mv -f ./.emacs.jcs ~/.emacs.jcs
+
+${EMACS:=emacs} -nw --batch \
+                --eval '(let ((debug-on-error (>=  emacs-major-version 26))
+                              (url-show-status nil)
+                              (user-emacs-directory default-directory)
+                              (user-init-file (expand-file-name "~/build.el"))
+                              (load-path (delq default-directory load-path)))
+                           (load-file user-init-file)
+                           (run-hooks (quote after-init-hook))
+                           (run-hooks (quote emacs-startup-hook)))'
+
+echo "Startup successful"
