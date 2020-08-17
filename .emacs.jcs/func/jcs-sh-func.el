2d69a02116a143a84ec51706e62fbd53266e2295
jenchieh <jcs090218@gmail.com>
2020-08-17 15:58:48 +0800

    Chaned ask line endings location.

--- .emacs.jcs/func/jcs-sh-func.el
+++ .emacs.jcs/func/jcs-sh-func.el
@@ -3,6 +3,26 @@
 ;;; Code:
 
 ;;;###autoload
+(defun jcs-ask-line-endings-for-this-sh-script (type)
+  "Ask the saved line endings TYPE for this shell script."
+  (require 'show-eol)
+  (interactive
+   (list
+    (completing-read
+     (format "Line Endings Type for file `%s`: " (jcs-buffer-name-or-buffer-file-name))
+     (let ((read-lst '("Windows (dos)" "macOS (mac)" "Linux (unix)")))
+       (push (format "=> system: (%s)" (jcs-get-current-sysem)) read-lst)
+       (push (format "=> file: (%s)" (show-eol--get-current-system)) read-lst)
+       read-lst))))
+  (let ((sys-type nil))
+    (cond ((string= type "Windows (dos)") (setq sys-type 'dos))
+          ((string= type "macOS (mac)") (setq sys-type 'mac))
+          ((string= type "Linux (unix)") (setq sys-type 'unix))
+          ((string-match-p "file" type) (setq sys-type (show-eol--get-current-system)))
+          ((string-match-p "system" type) (setq sys-type (jcs-get-current-sysem))))
+    (set-buffer-file-coding-system sys-type)))
+
+;;;###autoload
 (defun jcs-sh-untabify-save-buffer ()
   "ShellScript save buffer function."
   (interactive)
--- .emacs.jcs/func/jcs-util.el
+++ .emacs.jcs/func/jcs-util.el
@@ -1632,26 +1632,6 @@ IN-VAL : input value to set to IN-VAR."
         (jcs-is-linux 'unix)
         (t nil)))
 
-;;;###autoload
-(defun jcs-ask-line-endings-for-this-sh-script (type)
-  "Ask the saved line endings TYPE for this shell script."
-  (require 'show-eol)
-  (interactive
-   (list
-    (completing-read
-     "Line Endings Type: "
-     (let ((read-lst '("Windows (dos)" "macOS (mac)" "Linux (unix)")))
-       (push (format "=> system: (%s)" (jcs-get-current-sysem)) read-lst)
-       (push (format "=> file: (%s)" (show-eol--get-current-system)) read-lst)
-       read-lst))))
-  (let ((sys-type nil))
-    (cond ((string= type "Windows (dos)") (setq sys-type 'dos))
-          ((string= type "macOS (mac)") (setq sys-type 'mac))
-          ((string= type "Linux (unix)") (setq sys-type 'unix))
-          ((string-match-p "file" type) (setq sys-type (show-eol--get-current-system)))
-          ((string-match-p "system" type) (setq sys-type (jcs-get-current-sysem))))
-    (set-buffer-file-coding-system sys-type)))
-
 ;;----------------------------------------------------------------------------
 ;; Parentheses
 
