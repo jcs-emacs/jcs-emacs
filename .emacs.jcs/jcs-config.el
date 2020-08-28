9e665e5907103c260c6c00796cf42288c3010b1d
jenchieh <jcs090218@gmail.com>
2020-08-28 15:45:57 +0800

    Add customize config.

---
+++ .emacs.jcs/jcs-config.el
@@ -0,0 +1,8 @@
+;;; jcs-config.el --- Your own configuration.  -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;; Put your configuration code here ...
+
+(provide 'jcs-config)
+;;; jcs-config.el ends here
