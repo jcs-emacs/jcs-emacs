77f44e55d09335e59e88b1d5d8ab61dc393cca68
jenchieh <jcs090218@gmail.com>
2020-08-29 10:53:30 +0800

    Update scr.

--- scripts/test-startup.bat
+++ scripts/test-startup.bat
@@ -18,14 +18,15 @@ echo
 emacs --version
 echo
 
-emacs -nw --batch \
-      --eval '(let ((debug-on-error (>=  emacs-major-version 26))
-                    (url-show-status nil)
-                    (user-emacs-directory default-directory)
-                    (user-init-file (expand-file-name "~/build.el"))
-                    (load-path (delq default-directory load-path)))
-                      (load-file user-init-file)
-                      (run-hooks (quote after-init-hook))
-                      (run-hooks (quote emacs-startup-hook)))'
+emacs -nw
+
+emacs -nw --batch --eval '(let ((debug-on-error (>=  emacs-major-version 26))
+                                (url-show-status nil)
+                                (user-emacs-directory default-directory)
+                                (user-init-file (expand-file-name "~/build.el"))
+                                (load-path (delq default-directory load-path)))
+                                  (load-file user-init-file)
+                                  (run-hooks (quote after-init-hook))
+                                  (run-hooks (quote emacs-startup-hook)))'
 
 echo "Startup successful"
