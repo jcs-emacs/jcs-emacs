9ce2f2d4e85eb44b480bf63e69cc8131f0749ff8
jenchieh <jcs090218@gmail.com>
2020-08-29 16:17:12 +0800

    Update startup script.

--- scripts/test-startup.bat
+++ scripts/test-startup.bat
@@ -1,5 +1,7 @@
 @echo off
 
+SETLOCAL EnableDelayedExpansion
+
 echo "Moving core files..."
 
 set CONFIG_PATH=%UserProfile%\AppData\Roaming
@@ -14,13 +16,14 @@ robocopy /e "./.emacs.jcs" "%CONFIG_PATH%/.emacs.jcs"
 
 echo "Attempting startup..."
 
-emacs -nw --batch --eval '(let ((debug-on-error (>=  emacs-major-version 26))
-                                (url-show-status nil)
-                                (user-emacs-directory default-directory)
-                                (user-init-file (expand-file-name "~/build.el"))
-                                (load-path (delq default-directory load-path)))
-                                  (load-file user-init-file)
-                                  (run-hooks (quote after-init-hook))
-                                  (run-hooks (quote emacs-startup-hook)))'
+emacs -nw --batch --eval ^
+"(let ((debug-on-error (>=  emacs-major-version 26)) !^
+       (url-show-status nil) !^
+       (user-emacs-directory default-directory) !^
+       (user-init-file (expand-file-name \"~/build.el\")) !^
+       (load-path (delq default-directory load-path))) !^
+         (load-file user-init-file) !^
+         (run-hooks (quote after-init-hook)) !^
+         (run-hooks (quote emacs-startup-hook)))"
 
 echo "Startup successful"
