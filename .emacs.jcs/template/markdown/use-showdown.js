f1747ea546faee5057cd825cf1d2eb36b90eec34
jenchieh <jcs090218@gmail.com>
2020-08-30 16:28:43 +0800

    Add markdown parsing.

---
+++ .emacs.jcs/template/markdown/use-showdown.js
@@ -0,0 +1,18 @@
+/**
+ * $File: index.js $
+ * $Date: 2020-08-30 16:02:11 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2020 by Shen, Jen-Chieh $
+ */
+
+"use strict";
+
+var converter = new showdown.Converter();
+
+var markdownBody = document.getElementById('markdown-body');
+var text = markdownBody.innerHTML;
+var newHTML      = converter.makeHtml(text);
+
+markdownBody.innerHTML = newHTML;
