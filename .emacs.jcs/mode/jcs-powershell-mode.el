d02b02245e0e1d2c3c6172db69607f49e92920ba
JenChieh <jcs090218@gmail.com>
2020-09-08 21:27:17 +0800

    Added powershell mode.

---
+++ .emacs.jcs/mode/jcs-powershell-mode.el
@@ -0,0 +1,20 @@
+;;; jcs-powershell-mode.el --- R programming language mode. -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(require 'powershell)
+
+(defun jcs-powershell-mode-hook ()
+  "PowerShell mode hook."
+
+  ;; Treat underscore as word.
+  (modify-syntax-entry ?_ "w")
+
+  ;; File Header
+  (jcs-insert-header-if-valid '("[.]ps1")
+                              'jcs-insert-powershell-template))
+
+(add-hook 'powershell-mode-hook 'jcs-powershell-mode-hook)
+
+(provide 'jcs-powershell-mode)
+;;; jcs-powershell-mode.el ends here
