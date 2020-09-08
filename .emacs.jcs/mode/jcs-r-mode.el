1196965e62b4e1c2ce6ee0de7688c6e64bd70023
JenChieh <jcs090218@gmail.com>
2020-09-08 21:13:59 +0800

    Add r mode.

---
+++ .emacs.jcs/mode/jcs-r-mode.el
@@ -0,0 +1,20 @@
+;;; jcs-r-mode.el --- R programming language mode. -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(require 'ess-r-mode)
+
+(defun jcs-r-mode-hook ()
+  "R mode hook."
+
+  ;; Treat underscore as word.
+  (modify-syntax-entry ?_ "w")
+
+  ;; File Header
+  (jcs-insert-header-if-valid '("[.]r")
+                              'jcs-insert-r-template))
+
+(add-hook 'ess-r-mode-hook 'jcs-r-mode-hook)
+
+(provide 'jcs-r-mode)
+;;; jcs-r-mode.el ends here
