b909f79ea438299c87b8a76eeb05b23a4fa67a60
JenChieh <jcs090218@gmail.com>
2020-09-23 02:51:20 +0800

    Fixed not ivy, is echo area.

--- .emacs.jcs/func/jcs-minibuf.el
+++ .emacs.jcs/func/jcs-minibuf.el
@@ -2,6 +2,26 @@
 ;;; Commentary:
 ;;; Code:
 
+;;; Echo Area
+
+(defconst jcs-echo-area-buffer-name " *Minibuf-0*"
+  "Name of the minibuffer echo area buffer.")
+
+(defun jcs-echo-area--init ()
+  "Initialize echo area."
+  (with-current-buffer jcs-echo-area-buffer-name
+    (add-hook 'window-size-change-functions #'jcs-minibuf--window-size-change nil t)))
+
+;;; Minibuffer
+
+(defconst jcs-minibuf-buffer-name " *Minibuf-1*"
+  "Name of the minibuffer buffer.")
+
+(defun jcs-minibuf--init ()
+  "Initialize minibuffer."
+  (with-current-buffer jcs-minibuf-buffer-name
+    (add-hook 'window-size-change-functions #'jcs-minibuf--window-size-change nil t)))
+
 (defun jcs-minibuf--setup-hook ()
   "Hook when minibuffer setup."
   ;; NOTE: Avoid GCs while using `ivy'/`counsel'/`swiper' and `helm', etc.
@@ -9,6 +29,9 @@
     (jcs-gc-cons-threshold-speed-up t))
 
   (jcs-dark-blue-mode-line)
+
+  (jcs-echo-area--init)
+  (jcs-minibuf--init)
 
   (setq jcs-minibuf--setup-for-ivy-p t)
 
@@ -37,33 +60,28 @@
   (unless win (setq win (selected-window)))
   (eq win (minibuffer-window)))
 
+;;; Window
+
+(defun jcs-minibuf--window-size-change (&rest _)
+  "Hook for echo area when window size changed."
+  (jcs-minibuf--window-setup))
+
+(defun jcs-minibuf--window-setup ()
+  "Resize window for minibuffer and echo area."
+  (jcs-walk-through-all-windows-once
+   (lambda ()
+     (save-excursion
+       (let* ((cur-ln (line-number-at-pos (point) t))
+              (last-display-ln (jcs-last-visible-line-in-window))
+              (first-display-ln (jcs-first-visible-line-in-window))
+              (max-ln (line-number-at-pos (point-max) t))
+              (visible-win-height (- max-ln first-display-ln)))
+         (when (and (<= last-display-ln cur-ln)
+                    (<= (window-body-height) visible-win-height))
+           (message "--->> does!! %s %s" (current-buffer) jcs-minibuf--last-window-height)
+           (jcs-recenter-top-bottom 'bottom)))))))
+
 ;;; Ivy
-
-(defvar jcs-minibuf--setup-for-ivy-p nil
-  "Flag to check if execute setup for `ivy-mode'.")
-
-(defun jcs-ivy--exhibit ()
-  "Advice execute after `ivy--exhibit' function."
-  (when jcs-minibuf--setup-for-ivy-p
-    (setq jcs-minibuf--setup-for-ivy-p nil)
-    (jcs-minibuf--ivy-setup)))
-
-(advice-add 'ivy--exhibit :after #'jcs-ivy--exhibit)
-
-(defun jcs-minibuf--ivy-setup ()
-  "Setup hook for Ivy in minibuffer."
-  (when ivy-mode
-    (jcs-walk-through-all-windows-once
-     (lambda ()
-       (save-excursion
-         (let* ((cur-ln (line-number-at-pos (point) t))
-                (last-display-ln (jcs-last-visible-line-in-window))
-                (first-display-ln (jcs-first-visible-line-in-window))
-                (max-ln (line-number-at-pos (point-max) t))
-                (visible-win-height (- max-ln first-display-ln)))
-           (when (and (<= last-display-ln cur-ln)
-                      (<= (window-body-height) visible-win-height))
-             (jcs-recenter-top-bottom 'bottom))))))))
 
 (defun jcs-minibuf--ivy-post-command ()
   "Post command for Ivy in minibuffer."
