38f333b2263c857f628a02b2d2183ba74029c6e2
Jen-Chieh <jcs090218@gmail.com>
2020-11-19 14:16:37 +0800

    Add jenkinsfile mode config.

---
+++ .emacs.jcs/mode/jcs-jenkinsfile-mode.el
@@ -0,0 +1,15 @@
+;;; jcs-jenkinsfile-mode.el --- Jenkinsfile mode. -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(require 'jenkinsfile-mode)
+
+(defun jcs-jenkinsfile-mode-hook ()
+  "Jenkinsfile mode hook."
+  (jcs-insert-header-if-valid '("Jenkinsfile")
+                              'jcs-insert-jenkinsfile-template))
+
+(add-hook 'jenkinsfile-mode-hook 'jcs-jenkinsfile-mode-hook)
+
+(provide 'jcs-jenkinsfile-mode)
+;;; jcs-jenkinsfile-mode.el ends here
