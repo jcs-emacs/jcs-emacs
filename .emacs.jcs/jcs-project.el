ef7c4b271a3327bc0b1bb51c12786cf886a6c4dd
Jen-Chieh <jcs090218@gmail.com>
2020-12-11 15:11:04 +0800

    Add project module.

---
+++ .emacs.jcs/jcs-project.el
@@ -0,0 +1,21 @@
+;;; jcs-project.el --- Project related  -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(defun jcs-project-remember (&optional dir)
+  "Remeber the project from DIR.
+
+If optional argument DIR is nil, use variable `default-directory' instead."
+  (unless dir (setq dir default-directory))
+  (ignore-errors (project-remember-project (project--find-in-directory dir))))
+
+(defun jcs-project-list-clean ()
+  "Clean up the project list if the project no longer exists."
+  (project--ensure-read-project-list)
+  (let (pr-lst)
+    (dolist (pr project--list)
+      (when (jcs-file-directory-exists-p (nth 0 pr)) (push pr pr-lst)))
+    (setq project--list (reverse pr-lst))))
+
+(provide 'jcs-project)
+;;; jcs-project.el ends here
