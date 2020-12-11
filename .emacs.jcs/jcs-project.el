0bf5d71cdc99aa6dee175ead358c6a1f2a1c3157
JenChieh <jcs090218@gmail.com>
2020-12-11 18:16:09 +0800

    Add project remove.

--- .emacs.jcs/jcs-project.el
+++ .emacs.jcs/jcs-project.el
@@ -17,7 +17,17 @@ If optional argument DIR is nil, use variable `default-directory' instead."
   (let (pr-lst)
     (dolist (pr project--list)
       (when (jcs-file-directory-exists-p (nth 0 pr)) (push pr pr-lst)))
-    (setq project--list (reverse pr-lst))))
+    ;; Check if we need to update cache.
+    (unless (= (length project--list) (length pr-lst))
+      (setq project--list (reverse pr-lst))
+      (project--write-project-list))))
+
+(defun jcs-project-remove (dir)
+  "Remove project by project DIR."
+  (let (pr-lst)
+    (dolist (pr project--list) (unless (string= dir (nth 0 pr)) (push pr pr-lst)))
+    (setq project--list (reverse pr-lst))
+    (project--write-project-list)))
 
 (provide 'jcs-project)
 ;;; jcs-project.el ends here
