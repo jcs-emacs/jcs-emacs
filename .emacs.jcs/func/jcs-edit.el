65b38689e1731d1f8d6769cf7327d6cd16d4d86d
JenChieh <jcs090218@gmail.com>
2020-12-12 00:51:01 +0800

    Impls safe revert ask.

--- .emacs.jcs/func/jcs-edit.el
+++ .emacs.jcs/func/jcs-edit.el
@@ -634,11 +634,12 @@ REGEXP : reqular expression use to align."
   "Revert buffer without confirmation."
   (interactive)
   (require 'flycheck)
-  ;; Record all the enabled mode that you want to
-  ;; remain enabled after revert the file.
+  ;; Record all the enabled mode that you want to remain enabled after
+  ;; revert the file.
   (let ((was-flycheck flycheck-mode) (was-readonly buffer-read-only))
     ;; Revert it!
     (ignore-errors (revert-buffer :ignore-auto :noconfirm :preserve-modes))
+    (when (featurep 'line-reminder) (line-reminder-clear-reminder-lines-sign))
     ;; Revert all the enabled mode.
     (if was-flycheck (flycheck-mode 1) (flycheck-mode -1))
     (if was-readonly (read-only-mode 1) (read-only-mode -1))))
@@ -673,17 +674,59 @@ Argument TYPE can either be the following value.
         (if (file-readable-p filename)
             (setq do-revert-p t)
           (let (kill-buffer-query-functions) (kill-buffer buf))))
-      (when (and (buffer-name buf) (jcs-revert-buffer-p buf type))
+      (when (and (buffer-name buf) (or (jcs-revert-buffer-p buf type) do-revert-p))
         (with-current-buffer buf (jcs-revert-buffer-no-confirm))))))
 
 ;;;###autoload
-(defun jcs-revert-all-file-buffers ()
+(defun jcs-revert-all-buffers ()
   "Refresh all open file buffers without confirmation."
   (interactive)
   (jcs-window-record-once)
-  (jcs-revert-all-virtual-buffers jcs-revert-default-buffers)
-  (jcs-revert-all-valid-buffers t)
-  (jcs-window-restore-once))
+  (save-window-excursion (jcs-revert-all-virtual-buffers jcs-revert-default-buffers))
+  (jcs-window-restore-once)
+  (save-window-excursion (jcs-revert-all-valid-buffers nil)))
+
+(defun jcs-ask-revert-all (bufs &optional index)
+  "Ask to revert all buffers decided by ANSWER.
+
+This is called when only buffer changes externally and there are modification
+still in this editor.
+
+Optional argument INDEX is used to loop through BUFS."
+  (require 's)
+  (unless index (setq index 0))
+  (let* ((buf (nth index bufs)) path prompt answer)
+    (when buf
+      (setq path (buffer-file-name buf)
+            prompt (concat
+                    path "\n
+The file has unsaved changes inside this editor and has been changed externally.
+Do you want to reload it and lose the changes made in this source editor?")
+            answer (completing-read prompt '("Yes" "Yes to All" "No" "No to All"))
+            index (1+ index))
+      (cond ((string= answer "Yes")
+             (with-current-buffer buf (jcs-revert-buffer-no-confirm))
+             (jcs-ask-revert-all bufs index))
+            ((string= answer "Yes to All") (jcs-revert-all-valid-buffers t))
+            ((string= answer "No") (jcs-ask-revert-all bufs index))
+            ;; Does nothing, exit.
+            ((string= answer "No to All"))))))
+
+(defun jcs-un-save-modified-buffers ()
+  "Return non-nil if there is un-save modified buffer."
+  (let ((buf-lst (jcs-valid-buffer-list)) un-save-buf-lst)
+    (dolist (buf buf-lst)
+      (when (buffer-modified-p buf) (push buf un-save-buf-lst)))
+    (reverse un-save-buf-lst)))
+
+(defun jcs-safe-revert-all-buffers ()
+  "Revert buffers in the safe way."
+  (let ((un-save-buf-lst (jcs-un-save-modified-buffers)))
+    (when un-save-buf-lst (jcs-ask-revert-all un-save-buf-lst))))
+
+;;
+;; (@* "Windows" )
+;;
 
 ;;;###autoload
 (defun jcs-other-window-next (&optional cnt not-all-frames)
