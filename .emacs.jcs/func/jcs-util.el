f6e2c77a29f3ce01b53755418ca4c9997a19a493
JenChieh <jcs090218@gmail.com>
2020-12-20 11:48:12 +0800

    Update doc string.

--- .emacs.jcs/func/jcs-util.el
+++ .emacs.jcs/func/jcs-util.el
@@ -69,11 +69,11 @@ time is displayed."
   (get-buffer buf-name))
 
 (defun jcs-virtual-buffer-p (&optional buffer)
-  "Check if the BUFFER a virtual buffer."
+  "Return non-nil if buffer doesn't exists on disk."
   (not (jcs-valid-buffer-p buffer)))
 
 (defun jcs-valid-buffer-p (&optional buffer)
-  "Check if the BUFFER a valid buffer."
+  "Return non-nil if buffer does exists on disk."
   (buffer-file-name buffer))
 
 (defun jcs-virtual-buffer-list ()
