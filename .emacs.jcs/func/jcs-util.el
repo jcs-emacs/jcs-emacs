ac964f5c0b441c1df9c87a7e2f7ca8cf54700c8f
JenChieh <jcs090218@gmail.com>
2021-02-03 22:25:18 +0800

    Fixed util get start comment symbol

--- .emacs.jcs/func/jcs-util.el
+++ .emacs.jcs/func/jcs-util.el
@@ -1136,7 +1136,7 @@ CMDS should be a list of commands."
         (jcs-goto-start-comment)
         (progn  ; Make sure to go outside of symbol
           (re-search-backward "[ \t\r\n]" nil t)
-          (forward-char 1))
+          (unless (jcs-is-beginning-of-buffer-p) (forward-char 1)))
         (setq start-pt (point))
         (re-search-forward comment-start-skip (1+ (line-end-position)) t)
         (if (= start-pt (point)) nil
