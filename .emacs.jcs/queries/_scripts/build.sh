40d9e022d32a199db15f4e09739ed8987f7ed468
JenChieh <jcs090218@gmail.com>
2021-02-13 03:34:56 +0800

    Add sh version to build query files

---
+++ .emacs.jcs/queries/_scripts/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd ..
+temp="./_temp/"
+
+target=("c-sharp" "css" "javascript")
+
+for out in "${target[@]}"
+do
+    git clone https://github.com/tree-sitter/tree-sitter-${out} ${temp}
+    cp -R "${temp}queries/" "./${out}"
+    rm -rf ${temp}
+done
