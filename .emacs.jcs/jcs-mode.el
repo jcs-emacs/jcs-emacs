ffd43c6b2f4d200dea063866317a98e0c0d177e0
JenChieh <jcs090218@gmail.com>
2021-02-23 23:50:41 +0800

    Add load mode alist for applescript-mode

--- .emacs.jcs/jcs-mode.el
+++ .emacs.jcs/jcs-mode.el
@@ -484,7 +484,10 @@ See `add-to-list' function description for argument APPEND."
 
 ;;; A
 (progn
-  (jcs-add-auto-mode-alist '("\\.as'?\\'" . actionscript-mode)))
+  (jcs-add-auto-mode-alist '("\\.as'?\\'" . actionscript-mode))
+  (jcs-add-auto-mode-alist '("\\.applescript'?\\'" . applescript-mode))
+  (jcs-add-auto-mode-alist '("\\.scpt'?\\'" . applescript-mode))
+  (jcs-add-auto-mode-alist '("\\.scptd'?\\'" . applescript-mode)))
 
 ;;; B
 (progn
