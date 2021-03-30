2079b43825df3f5d07cf305489212db6acd9ad8a
JenChieh <jcs090218@gmail.com>
2021-03-30 18:03:42 +0800

    Customize company theme

--- .emacs.jcs/func/jcs-company.el
+++ .emacs.jcs/func/jcs-company.el
@@ -2,27 +2,86 @@
 ;;; Commentary:
 ;;; Code:
 
+(require 'cl-lib)
+
 ;;
 ;; (@* "Faces" )
 ;;
 
-(custom-set-faces
- ;; --- Preview
- '(company-preview ((t (:foreground "dark gray" :underline t))))
- '(company-preview-common ((t (:inherit company-preview))))
- ;; --- Base Selection
- '(company-tooltip ((t (:background "light gray" :foreground "black"))))
- '(company-tooltip-selection ((t (:background "steel blue" :foreground "white"))))
- ;; --- Keyword Selection
- '(company-tooltip-common
-   ((((type x)) (:inherit company-tooltip :weight bold))
-    (t (:background "light gray" :foreground "#C00000"))))
- '(company-tooltip-common-selection
-   ((((type x)) (:inherit company-tooltip-selection :weight bold))
-    (t (:background "steel blue" :foreground "#C00000"))))
- ;; --- Scroll Bar
- '(company-scrollbar-fg ((t (:background "black"))))
- '(company-scrollbar-bg ((t (:background "dark gray")))))
+(defcustom jcs-company-theme (if (display-graphic-p) 'vs 'auto-complete)
+  "Company theem style."
+  :type '(choice (const :tag "Auto-Complete" auto-complete)
+                 (const :tag "Visual Studio IDE" vs))
+  :group 'jcs)
+
+(cl-defun jcs-company-set-face
+    (&key annotation tooltip scrollbar quickhelp
+          tooltip-selection tooltip-common tooltip-common-selection)
+  "Set face for `company-mode'."
+  (custom-set-faces
+   ;; --- Annotation
+   `(company-tooltip-annotation ((t (:foreground ,(car annotation)))))
+   `(company-fuzzy-annotation-face ((t (:foreground ,(cdr annotation)))))
+   ;; --- Preview
+   `(company-preview ((t (:foreground "dark gray" :underline t))))
+   `(company-preview-common ((t (:inherit company-preview))))
+   ;; --- Base Selection
+   `(company-tooltip ((t (:background ,(car tooltip) :foreground ,(cdr tooltip)))))
+   `(company-tooltip-selection ((t (:background ,(car tooltip-selection) :foreground ,(cdr tooltip-selection)))))
+   ;; --- Keyword Selection
+   `(company-tooltip-common
+     ((((type x)) (:inherit company-tooltip :weight bold))
+      (t (:background ,(car tooltip-common) :foreground ,(cdr tooltip-common)))))
+   `(company-tooltip-common-selection
+     ((((type x)) (:inherit company-tooltip-selection :weight bold))
+      (t (:background ,(car tooltip-common-selection) :foreground ,(cdr tooltip-common-selection)))))
+   ;; --- Scroll Bar
+   `(company-scrollbar-bg ((t (:background ,(car scrollbar)))))
+   `(company-scrollbar-fg ((t (:background ,(cdr scrollbar)))))
+   ;; --- Tooltip
+   `(popup-tip-face ((t (:background ,(car quickhelp) :foreground ,(cdr quickhelp))))))
+  (setq company-quickhelp-color-background (car quickhelp)
+        company-quickhelp-color-foreground (cdr quickhelp)))
+
+(defun jcs-company-auto-complete-theme ()
+  "Auto-Complete theme for `company-mode'."
+  (jcs-company-set-face
+   :annotation '("red4" . "red4")
+   :tooltip '("light gray" . "black")
+   :tooltip-selection '("steel blue" . "white")
+   :tooltip-common '("light gray" . "#C00000")
+   :tooltip-common-selection '("steel blue" . "#C00000")
+   :scrollbar '("dark gray" . "black")
+   :quickhelp '("#FFF08A" . "black")))
+
+(defun jcs-company-vs-theme ()
+  "Auto-Complete theme for `company-mode'."
+  (if (jcs-is-light-theme-p)
+      (jcs-company-set-face
+       :annotation '("#41474D" . "#5E85AB")
+       :tooltip '("#F5F5F5" . "black")
+       :tooltip-selection '("#D6EBFF" . "black")
+       :tooltip-common '("#F5F5F5" . "#0066BF")
+       :tooltip-common-selection '("#D6EBFF" . "#0066BF")
+       :scrollbar '("#F5F5F5" . "#C2C3C9")
+       :quickhelp '("#F6F6F6" . "#1E1E1E"))
+    (jcs-company-set-face
+     :annotation '("#96A2AA" . "#7BABCA")
+     :tooltip '("#252526" . "#BEBEBF")
+     :tooltip-selection '("#062F4A" . "#BEBEBF")
+     :tooltip-common '("#252526" . "#0096FA")
+     :tooltip-common-selection '("#062F4A" . "#0096FA")
+     :scrollbar '("#3E3E42" . "#686868")
+     :quickhelp '("#424245" . "#F1F1F1"))))
+
+(defun jcs-company-default-theme ()
+  "Default theme for `company-mode'."
+  (cl-case jcs-company-theme
+    (auto-complete (jcs-company-auto-complete-theme))
+    (vs (jcs-company-vs-theme))
+    (t (user-error "Unknown `company-mode` theme type: %s" jcs-company-theme))))
+
+(jcs-company-default-theme)
 
 ;;
 ;; (@* "Util" )
