5ddd73928614fee02954b9270d6d23c1141b9559
JenChieh <jcs090218@gmail.com>
2021-07-11 01:08:13 +0800

    Revert the special command for eldoc

--- .emacs.jcs/jcs-function.el
+++ .emacs.jcs/jcs-function.el
@@ -384,6 +384,8 @@ If optional argument FORCE is non-nil, force refresh it."
         '(jcs-eldoc-message-now
           self-insert-command
           mouse-set-point
+          jcs-real-space jcs-smart-space
+          jcs-real-backspace jcs-smart-backspace
           previous-line next-line
           jcs-previous-line jcs-next-line
           jcs-smart-indent-up jcs-smart-indent-down
