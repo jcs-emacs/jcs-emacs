43579c68bda8ef891a75e61773139c8c4ec656cc
JenChieh <jcs090218@gmail.com>
2021-09-13 14:32:11 +0800

    Clean up lang

--- .emacs.jcs/jcs-env.el
+++ .emacs.jcs/jcs-env.el
@@ -208,17 +208,17 @@
 
 ;;; Language Environment
 (prefer-coding-system 'utf-8)
-(defconst jcs-language-environment "UTF-8" "Default language environment.")
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(set-file-name-coding-system 'utf-8)
-(set-clipboard-coding-system 'utf-8)
-(set-w32-system-coding-system 'utf-8)
-(set-buffer-file-coding-system 'utf-8)
-
 (setq locale-coding-system 'utf-8)
+
+(set-language-environment 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-clipboard-coding-system 'utf-8)
+(set-file-name-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(set-w32-system-coding-system 'utf-8)
+(modify-coding-system-alist 'process "*" 'utf-8)
 
 ;; Resolve performance issue moving around Unicode Text.
 (setq inhibit-compacting-font-caches t)
