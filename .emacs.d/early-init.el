a49de0860833d432279269f890617a2b030d9e43
Jen-Chieh <jcs090218@gmail.com>
2021-09-15 14:42:04 +0800

    Improve startup time

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -5,10 +5,20 @@
 ;; Defer garbage collection further back in the startup process
 (setq gc-cons-threshold most-positive-fixnum)
 
+;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+;; to skip the mtime checks on every *.elc file.
+(setq load-prefer-newer noninteractive)
+
+;; Inhibit resizing frame
+(setq frame-inhibit-implied-resize t)
+
 ;; Faster to disable these here (before they've been initialized)
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
+(when (featurep 'ns)
+  (push '(ns-transparent-titlebar . t) default-frame-alist))
 
 (provide 'early-init)
 ;;; early-init.el ends here
