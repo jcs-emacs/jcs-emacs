f4dcd6d083216eb01fe5c2204002349dd8b95d5b
JenChieh <jcs090218@gmail.com>
2021-09-16 17:22:00 +0800

    Move

--- scripts/copy_config.bat
+++ scripts/copy_config.bat
@@ -6,10 +6,8 @@ echo "Copying core files..."
 
 set CONFIG_PATH=%UserProfile%\AppData\Roaming\
 
-echo %CONFIG_PATH%
-
-xcopy  "./build.el" "%CONFIG_PATH%" /K /D /H /Y
-xcopy  "./.emacs" "%CONFIG_PATH%" /K /D /H /Y
+move "./build.el" "%CONFIG_PATH%"
+move "./.emacs" "%CONFIG_PATH%"
 
 mkdir "%CONFIG_PATH%/.emacs.d"
 mkdir "%CONFIG_PATH%/.emacs.jcs"
@@ -17,7 +15,6 @@ robocopy /e "./.emacs.d" "%CONFIG_PATH%/.emacs.d"
 robocopy /e "./.emacs.jcs" "%CONFIG_PATH%/.emacs.jcs"
 
 echo %CONFIG_PATH%
-
 dir %CONFIG_PATH%
 
 echo "Done copying configuration files"
