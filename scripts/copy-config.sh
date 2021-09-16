f01be5ff65fb128dc98e97d16b062973147e0fe7
Jen-Chieh <jcs090218@gmail.com>
2021-09-16 15:11:31 +0800

    Rmeove recursive falg

--- scripts/copy-config.sh
+++ scripts/copy-config.sh
@@ -2,9 +2,9 @@
 
 echo "Copying core files..."
 
-cp -f ./build.el ~/build.el
-cp -f ./.emacs ~/.emacs
-cp -f ./.emacs.d ~/.emacs.d
-cp -f ./.emacs.jcs ~/.emacs.jcs
+cp ./build.el ~/build.el
+cp ./.emacs ~/.emacs
+cp ./.emacs.d ~/.emacs.d
+cp ./.emacs.jcs ~/.emacs.jcs
 
 echo "Done copying configuration files"
