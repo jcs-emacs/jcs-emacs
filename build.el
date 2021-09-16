30b523e0cd0287b104da95ea81f25a9fe48a2193
JenChieh <jcs090218@gmail.com>
2021-09-16 21:09:14 +0800

    Try build

--- build.el
+++ build.el
@@ -6,7 +6,25 @@
   "Define for build testing.")
 
 ;; Start regular Emacs file.
-(load-file (expand-file-name "~/.emacs"))
+;;(load-file (expand-file-name "~/.emacs"))
+
+(require 'package)
+
+(let* ((package-archives '(("melpa" . "https://melpa.org/packages/")
+                           ("gnu" . "https://elpa.gnu.org/packages/")))
+       package-enable-at-startup package-check-signature
+       (pkgs '(dash f lv ht spinner markdown-mode deferred el-mock)))
+  (package-initialize)
+
+  (mapc (lambda (pkg)
+          (unless (package-installed-p pkg)
+            (package-refresh-contents) (package-install pkg)))
+        pkgs)
+
+  (add-hook 'kill-emacs-hook
+            `(lambda ()
+               (unless (boundp 'emacs-lsp-ci)
+                 (delete-directory ,user-emacs-directory t)))))
 
 ;; Local Variables:
 ;; coding: utf-8
