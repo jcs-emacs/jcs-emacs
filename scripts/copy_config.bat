2f15181862d9aa1d6b8a32d2fe1020a9afbb41e4
JenChieh <jcs090218@gmail.com>
2021-09-16 17:11:38 +0800

    Ues ph

--- scripts/copy_config.bat
+++ scripts/copy_config.bat
@@ -8,8 +8,8 @@ set CONFIG_PATH=%UserProfile%\AppData\Roaming\
 
 echo %CONFIG_PATH%
 
-xcopy /s "./build.el" "%CONFIG_PATH%"
-xcopy /s "./.emacs" "%CONFIG_PATH%"
+Copy-Item "./build.el" -Destination "%CONFIG_PATH%"
+Copy-Item "./.emacs" -Destination "%CONFIG_PATH%"
 
 mkdir "%CONFIG_PATH%/.emacs.d"
 mkdir "%CONFIG_PATH%/.emacs.jcs"
