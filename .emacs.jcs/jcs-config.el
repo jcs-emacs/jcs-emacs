b2dddbd6218765749dcb8801a0f39c0765aa2158
Jen-Chieh <jcs090218@gmail.com>
2021-12-07 16:04:54 +0800

    Add user settings

--- .emacs.jcs/jcs-config.el
+++ .emacs.jcs/jcs-config.el
@@ -4,5 +4,8 @@
 
 ;; Put your configuration code here ...
 
+(setq user-full-name "Jen-Chieh Shen"
+      user-mail-address "jcs090218@gmail.com")
+
 (provide 'jcs-config)
 ;;; jcs-config.el ends here
