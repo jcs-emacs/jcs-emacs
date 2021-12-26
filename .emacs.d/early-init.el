3a7ae23f4163e19422e7b8d102e4c8eae8cb16b7
Jen-Chieh Shen <jcs090218@gmail.com>
2021-12-26 18:42:49 +0800

    Update dep

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -11,11 +11,14 @@
 (setq load-prefer-newer noninteractive)
 
 ;; Faster to disable these here (before they've been initialized)
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
-(when (featurep 'ns)
-  (push '(ns-transparent-titlebar . t) default-frame-alist))
+(setq default-frame-alist
+      (append
+       '((menu-bar-lines . 0)
+         (tool-bar-lines . 0)
+         (vertical-scroll-bars))
+       (when (featurep 'ns)
+         '((ns-transparent-titlebar . t)))
+       default-frame-alist))
 
 ;;
 ;;; Bootstrap
