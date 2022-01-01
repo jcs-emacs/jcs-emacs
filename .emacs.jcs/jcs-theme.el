59857b5b33de227c00f75e09559d051699443e9f
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-01 18:57:11 +0800

    Add theme current utility function

--- .emacs.jcs/jcs-theme.el
+++ .emacs.jcs/jcs-theme.el
@@ -6,49 +6,22 @@
 ;; (@* "Core" )
 ;;
 
-(defun jcs-theme-call (fnc)
-  "Execute FNC with default arguments."
-  (funcall fnc (jcs-light-theme-p)))
-
-(defun jcs-theme-reapply ()
-  "Reset certain plugins base on the theme."
-  (jcs-reset-common-faces-by-theme)
-  (jcs-reload-active-mode)
-  (jcs-re-enable-mode-if-was-enabled 'highlight-indent-guides-mode))
-
-(defun jcs--set-theme (cc hlc rc frc)
-  "Config the theme to all frames.
-
-Arguments stand for these explanation,
-
-  * CC - cursor color.
-  * HLC - highlight line color.
-  * RC - region color.
-  * FRC - fringe color."
-  (set-cursor-color cc)
-  (with-eval-after-load 'hl-line (set-face-background 'hl-line hlc))
-  (set-face-background 'region rc)
-  (set-face-background 'fringe frc)
-  (jcs-theme-reapply))
-
-(add-hook 'jcs-after-load-theme-hook
-          (lambda (light-p)
-            (if light-p
-                (jcs--set-theme "#909090" "#E6E6E6" "#99C9EF" "#E6E7E8")
-              (jcs--set-theme "#909090" "#2E2E2E" "#264F78" "#333333"))))
+(defun jcs-theme-current ()
+  "Return current theme name."
+  (or (nth 0 custom-enabled-themes) jcs-theme-default))
 
 (defun jcs-light-theme-p ()
   "Return non-nil if current theme is light theme."
   (ignore-errors (jcs-light-color-p (face-background 'default))))
 
-(defun jcs-dark-theme-p ()
-  "Return non-nil if current theme is dark theme."
-  (not (jcs-light-theme-p)))
+(defun jcs-theme-call (fnc)
+  "Execute FNC with default arguments."
+  (funcall fnc (jcs-theme-current)))
 
-(defun jcs-refresh-theme ()
+(defun jcs-theme-refresh ()
   "Refresh theme."
   (interactive)
-  (load-theme (or (nth 0 custom-enabled-themes) jcs-theme-default) t))
+  (load-theme (jcs-theme-current) t))
 
 (defun jcs-setup-default-theme ()
   "Set default theme color."
@@ -65,9 +38,8 @@ Arguments stand for these explanation,
 (defun jcs--enable-theme--advice-after (&rest _)
   "Advice execute after `load-theme' function."
   (jcs-set-font-size jcs-default-font-size)
-  (let ((light-p (jcs-light-theme-p)))
-    (jcs-walk-frames
-     (lambda () (run-hook-with-args 'jcs-after-load-theme-hook light-p)))))
+  (jcs-walk-frames
+   (lambda () (run-hook-with-args 'jcs-after-load-theme-hook (jcs-theme-current)))))
 (advice-add 'enable-theme :after #'jcs--enable-theme--advice-after)
 
 (provide 'jcs-theme)
