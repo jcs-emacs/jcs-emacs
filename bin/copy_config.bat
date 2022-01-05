c9d340c6ffed34aa46c56950986a4ab957216898
Jen-Chieh <jcs090218@gmail.com>
2022-01-05 16:03:47 +0800

    Move build

--- bin/copy_config.bat
+++ bin/copy_config.bat
@@ -8,11 +8,7 @@ set CONFIG_PATH=%UserProfile%
 
 echo %CONFIG_PATH%
 
-move "./build.el" "%CONFIG_PATH%"
-move "./.emacs" "%CONFIG_PATH%"
-
 mkdir "%CONFIG_PATH%/.emacs.d"
-mkdir "%CONFIG_PATH%/.emacs.jcs"
 robocopy /e "./.emacs.d" "%CONFIG_PATH%/.emacs.d"
 robocopy /e "./.emacs.jcs" "%CONFIG_PATH%/.emacs.jcs"
 
--- bin/copy_config.sh
+++ bin/copy_config.sh
@@ -2,9 +2,6 @@
 
 echo "Copying core files..."
 
-mv -f ./build.el ~/build.el
-mv -f ./.emacs ~/.emacs
 mv -f ./.emacs.d ~/.emacs.d
-mv -f ./.emacs.jcs ~/.emacs.jcs
 
 echo "Done copying configuration files"
