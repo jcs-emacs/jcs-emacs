4fed6ab4c206f6a75c10fa71d7b5b90695b3c192
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-05 20:00:55 +0800

    Fix symbol

--- bin/test.el
+++ bin/test.el
@@ -20,8 +20,8 @@
       (user-init-file (locate-user-emacs-file "init.el")))
   (load early-init-file)
   (load user-init-file)  ; Start regular Emacs file
-  (run-hooks after-init-hook)
-  (run-hooks emacs-startup-hook))
+  (run-hooks 'after-init-hook)
+  (run-hooks 'emacs-startup-hook))
 
 (jcs-emacs-version)
 
