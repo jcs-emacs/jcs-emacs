8b4453a635fe39bde9cf434f84640e2d7629f707
Jen-Chieh <jcs090218@gmail.com>
2022-01-05 16:04:03 +0800

    Update custom file

--- early-init.el
+++ early-init.el
@@ -29,7 +29,7 @@
 
 ;;
 ;;; Set Custom file
-(setq-default custom-file (expand-file-name ".jcs-custom.el" user-emacs-directory))
+(setq-default custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file) (load custom-file))
 
 (provide 'early-init)
