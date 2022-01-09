ea1c0a969d56934011c3e9cb35a2d534a71be709
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-09 16:40:35 +0800

    Move frame to just window

--- lisp/func/jcs-window.el
+++ lisp/func/jcs-window.el
@@ -3,6 +3,36 @@
 ;;; Code:
 
 ;;
+;; (@* "Frame" )
+;;
+
+(defun jcs-frame-util-p (&optional frame)
+  "Return non-nil if FRAME is an utility frame."
+  (unless frame (setq frame (selected-frame)))
+  (frame-parent frame))
+
+(defun jcs-make-frame ()
+  "Select new frame after make frame."
+  (interactive)
+  (let ((new-frame (call-interactively #'make-frame)))
+    (select-frame-set-input-focus new-frame)
+    (jcs-theme-refresh)
+    (split-window-horizontally)))
+
+(defun jcs-walk-frames (fun &optional minibuf)
+  "Like `walk-windows', but only for frames.
+
+See function `walk-windows' description for arguments FUN and MINIBUF."
+  (let (last-frame cur-frame)
+    (walk-windows
+     (lambda (win)
+       (setq cur-frame (window-frame win))
+       (unless (equal last-frame cur-frame)
+         (setq last-frame cur-frame)
+         (with-selected-frame cur-frame (funcall fun))))
+     minibuf t)))
+
+;;
 ;; (@* "Navigation" )
 ;;
 
