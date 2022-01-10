43128e5b7db2a0eb3c9689ea2bdf885a2e1fb7d1
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-11 01:26:27 +0800

    Add speed

---
+++ bin/test-speed.el
@@ -0,0 +1,13 @@
+;;; test-speed.el --- Test the configuration  -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(load "./test-startup.el")
+
+(message "[INFO] %s" dashboard-init-info)
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
+;;; test-speed.el ends here
