ef98f7c34431ba9d5990338708714cd0237dee1a
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-15 00:42:52 +0800

    Set default modeline

--- lisp/func/jcs-mode-line.el
+++ lisp/func/jcs-mode-line.el
@@ -2,6 +2,20 @@
 ;;; Commentary:
 ;;; Code:
 
+(setq-default
+ mode-line-format
+ '((:eval
+    (jcs-mode-line-render
+     (quote ("%e "
+             mode-line-front-space
+             mode-line-buffer-identification
+             (:eval (moody-tab (format-mode-line mode-line-modes)))
+             (:eval (jcs-vc-project))
+             (:eval (jcs-vc-info))))
+     (quote ((:eval (moody-tab " %l : %c " 0 'up))
+             " %p "
+             mode-line-end-spaces))))))
+
 ;;
 ;; (@* "Util" )
 ;;
@@ -18,35 +32,12 @@
   (set-face-foreground 'vertical-border color)
   (set-face-foreground 'window-divider color))
 
-(defun jcs-powerline-set-theme-faces (ac-lst inac-lst)
-  "Set `powerline' them faces with AC-LST and INAC-LST."
-  (when (featurep 'powerline)
-    ;; Active
-    (set-face-foreground 'powerline-active0 (nth 0 ac-lst))
-    (set-face-background 'powerline-active0 (nth 1 ac-lst))
-    (set-face-foreground 'powerline-active1 (nth 2 ac-lst))
-    (set-face-background 'powerline-active1 (nth 3 ac-lst))
-    (set-face-foreground 'powerline-active2 (nth 4 ac-lst))
-    (set-face-background 'powerline-active2 (nth 5 ac-lst))
-
-    ;; Inactive
-    (set-face-foreground 'powerline-inactive0 (nth 0 inac-lst))
-    (set-face-background 'powerline-inactive0 (nth 1 inac-lst))
-    (set-face-foreground 'powerline-inactive1 (nth 2 inac-lst))
-    (set-face-background 'powerline-inactive1 (nth 3 inac-lst))
-    (set-face-foreground 'powerline-inactive2 (nth 4 inac-lst))
-    (set-face-background 'powerline-inactive2 (nth 5 inac-lst))
-
-    (powerline-reset)))
-
-(defun jcs--set-mode-line-theme (ml-ac-lst ml-inac-lst bc power-ac-lst power-inac-lst)
+(defun jcs--set-mode-line-theme (ml-ac-lst ml-inac-lst bc)
   "Set the mode line theme.
 ML-AC-LST : mode-line active list.  ML-INAC-LST : mode-line inactive list.
-BC : border color.
-POWER-AC-LST : powerline active list.  POWER-INAC-LST : powerline inactive list."
+BC : border color."
   (jcs--set-mode-line-color ml-ac-lst ml-inac-lst)
-  (jcs--set-border-color bc)
-  (jcs-powerline-set-theme-faces power-ac-lst power-inac-lst))
+  (jcs--set-border-color bc))
 
 ;;
 ;; (@* "Themes" )
@@ -57,78 +48,54 @@ POWER-AC-LST : powerline active list.  POWER-INAC-LST : powerline inactive list.
   (interactive)
   (if (jcs-light-theme-p)
       (jcs--set-mode-line-theme
-       '("#1C1C1C" "#BFBFBF") '("#CCCCCC" "#4D4D4D") "#161616"
-       '("#1C1C1C" "#EBEBEB" "#000000" "#B8B8B8" "#1C1C1C" "#C7C7C7")
-       '("#1C1C1C" "#DEDEDE" "#000000" "#B8B8B8" "#1C1C1C" "#C7C7C7"))
+       '("#1C1C1C" "#BFBFBF") '("#CCCCCC" "#4D4D4D") "#161616")
     (jcs--set-mode-line-theme
-     '("#1C1C1C" "#BFBFBF") '("#CCCCCC" "#4D4D4D") "#D2D2D2"
-     '("#1C1C1C" "#CCCCCC" "#CCCCCC" "#1C1C1C" "#CCCCCC" "#333333")
-     '("#CCCCCC" "#4D4D4D" "#CCCCCC" "#1C1C1C" "#CCCCCC" "#333333"))))
+     '("#D2D2D2" "#4D4D4D") '("#CCCCCC" "#333333") "#D2D2D2")))
 
 (defun jcs-dark-green-mode-line ()
   "Dark green mode line."
   (interactive)
   (if (jcs-light-theme-p)
       (jcs--set-mode-line-theme
-       '("#CCCCCC" "#467E7D") '("#CCCCCC" "#2B4D4D") "#7ED5D5"
-       '("#1C1C1C" "#7ED5D5" "#1C1C1C" "#5B928F" "#1C1C1C" "#68AFAC")
-       '("#1C1C1C" "#75C0C0" "#1C1C1C" "#5B928F" "#1C1C1C" "#68AFAC"))
+       '("#CCCCCC" "#467E7D") '("#CCCCCC" "#2B4D4D") "#7ED5D5")
     (jcs--set-mode-line-theme
-     '("#CCCCCC" "#467E7D") '("#CCCCCC" "#2B4D4D") "#467E7D"
-     '("#1C1C1C" "#529191" "#CCCCCC" "#1C2E2D" "#CCCCCC" "#294645")
-     '("#CCCCCC" "#2B4D4D" "#CCCCCC" "#1C2E2D" "#CCCCCC" "#294645"))))
+     '("#CCCCCC" "#467E7D") '("#CCCCCC" "#2B4D4D") "#467E7D")))
 
 (defun jcs-dark-blue-mode-line ()
   "Dark blue mode line."
   (interactive)
   (if (jcs-light-theme-p)
       (jcs--set-mode-line-theme
-       '("#CCCCCC" "#246AAF") '("#CCCCCC" "#0E2944") "#2E84D9"
-       '("#EDEDED" "#2E84D9" "#EDEDED" "#225F9A" "#EDEDED" "#2D7AC4")
-       '("#EDEDED" "#2C7AC6" "#EDEDED" "#225F9A" "#EDEDED" "#2D7AC4"))
+       '("#CCCCCC" "#246AAF") '("#CCCCCC" "#0E2944") "#2E84D9")
     (jcs--set-mode-line-theme
-     '("#CCCCCC" "#246AAF") '("#CCCCCC" "#0E2944") "#246AAF"
-     '("#1C1C1C" "#246AAF" "#CCCCCC" "#091A2B" "#CCCCCC" "#0E2944")
-     '("#CCCCCC" "#14375B" "#CCCCCC" "#091A2B" "#CCCCCC" "#0E2944"))))
+     '("#CCCCCC" "#246AAF") '("#CCCCCC" "#0E2944") "#246AAF")))
 
 (defun jcs-dark-orange-mode-line ()
   "Dark orange mode line."
   (interactive)
   (if (jcs-light-theme-p)
       (jcs--set-mode-line-theme
-       '("#1C1C1C" "#FF6C32") '("#CCCCCC" "#682B12") "#FF6C32"
-       '("#1C1C1C" "#FF6C32" "#CCCCCC" "#682B12" "#CCCCCC" "#9A431F")
-       '("#CCCCCC" "#9A431F" "#CCCCCC" "#682B12" "#CCCCCC" "#883919"))
+       '("#1C1C1C" "#FF6C32") '("#CCCCCC" "#682B12") "#FF6C32")
     (jcs--set-mode-line-theme
-     '("#1C1C1C" "#FF6C32") '("#CCCCCC" "#682B12") "#FF6C32"
-     '("#1C1C1C" "#FF6C32" "#CCCCCC" "#682B12" "#CCCCCC" "#9A431F")
-     '("#CCCCCC" "#9A431F" "#CCCCCC" "#682B12" "#CCCCCC" "#883919"))))
+     '("#1C1C1C" "#FF6C32") '("#CCCCCC" "#682B12") "#FF6C32")))
 
 (defun jcs-red-mode-line ()
   "Red mode line."
   (interactive)
   (if (jcs-light-theme-p)
       (jcs--set-mode-line-theme
-       '("#CCCCCC" "#FF0000") '("#CCCCCC" "#6A0101") "#FF0000"
-       '("#1C1C1C" "#FF0000" "#CCCCCC" "#6A0101" "#CCCCCC" "#920101")
-       '("#CCCCCC" "#920101" "#CCCCCC" "#6A0101" "#CCCCCC" "#970000"))
+       '("#CCCCCC" "#FF0000") '("#CCCCCC" "#6A0101") "#FF0000")
     (jcs--set-mode-line-theme
-     '("#CCCCCC" "#FF0000") '("#CCCCCC" "#6A0101") "#FF0000"
-     '("#1C1C1C" "#FF0000" "#CCCCCC" "#6A0101" "#CCCCCC" "#920101")
-     '("#CCCCCC" "#920101" "#CCCCCC" "#6A0101" "#CCCCCC" "#970000"))))
+     '("#CCCCCC" "#FF0000") '("#CCCCCC" "#6A0101") "#FF0000")))
 
 (defun jcs-purple-mode-line ()
   "Purple mode line."
   (interactive)
   (if (jcs-light-theme-p)
       (jcs--set-mode-line-theme
-       '("#CCCCCC" "#B100EB") '("#CCCCCC" "#650286") "#B100EB"
-       '("#1C1C1C" "#B100EB" "#CCCCCC" "#4B0263" "#CCCCCC" "#69018B")
-       '("#CCCCCC" "#69018B" "#CCCCCC" "#4B0263" "#CCCCCC" "#670188"))
+       '("#CCCCCC" "#B100EB") '("#CCCCCC" "#650286") "#B100EB")
     (jcs--set-mode-line-theme
-     '("#CCCCCC" "#B100EB") '("#CCCCCC" "#650286") "#B100EB"
-     '("#1C1C1C" "#B100EB" "#CCCCCC" "#4B0263" "#CCCCCC" "#69018B")
-     '("#CCCCCC" "#69018B" "#CCCCCC" "#4B0263" "#CCCCCC" "#670188"))))
+     '("#CCCCCC" "#B100EB") '("#CCCCCC" "#650286") "#B100EB")))
 
 (provide 'jcs-mode-line)
 ;;; jcs-mode-line.el ends here
