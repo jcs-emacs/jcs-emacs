7721a39b194627099b1ea50d8824189de5132ede
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-23 02:09:27 +0800

    Clean sort

--- lisp/func/jcs-util.el
+++ lisp/func/jcs-util.el
@@ -285,20 +285,20 @@ See function `jcs-string-compare-p' for argument TYPE."
   "Sort CANDIDATES with PREFIX and FNC.
 
 If optional argument FLIP is non-nil, reverse query and pattern order."
-  (let ((scoring-table (ht-create)) scoring-keys scoring score)
+  (let ((scoring-table (ht-create)) scoring-keys)
     (dolist (cand candidates)
-      (setq scoring (ignore-errors
-                      (if flip (funcall fnc prefix cand)
-                        (funcall fnc cand prefix)))
-            score (cond ((listp scoring) (nth 0 scoring))
-                        ((vectorp scoring) (aref scoring 0))
-                        ((numberp scoring) scoring)
-                        (t 0)))
-      (when score
-        ;; XXX ht causes unknown error on start, use regular hash table functions
-        ;; for now
-        (unless (gethash score scoring-table) (setf (gethash score scoring-table) nil))
-        (push cand (gethash score scoring-table))))
+      (let* ((scoring (ignore-errors
+                        (if flip (funcall fnc prefix cand)
+                          (funcall fnc cand prefix))))
+             (score (cond ((listp scoring) (nth 0 scoring))
+                          ((vectorp scoring) (aref scoring 0))
+                          ((numberp scoring) scoring)
+                          (t 0))))
+        (when score
+          ;; XXX ht causes unknown error on start, use regular hash table functions
+          ;; for now
+          (unless (gethash score scoring-table) (setf (gethash score scoring-table) nil))
+          (push cand (gethash score scoring-table)))))
     ;; Get all keys, and turn into a list.
     (ht-map (lambda (score-key _) (push score-key scoring-keys)) scoring-table)
     (setq scoring-keys (sort scoring-keys #'>)  ; Sort keys in order
@@ -1059,6 +1059,10 @@ If optional argument REVERSE is non-nil, LIST item and ELT argument."
     (save-selected-window
       (select-window (active-minibuffer-window))
       (apply fnc args))))
+
+(defun jcs-minibuf--compare-p (name type)
+  "Compare buffer-string with NAME and TYPE."
+  (jcs-minibuffer-do-stuff (lambda () (jcs-string-compare-p name (buffer-string) type))))
 
 ;;
 ;; (@* "Mode" )
