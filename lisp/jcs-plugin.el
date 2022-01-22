8f664d503a14b3c5c5d7a0124765e93a65a3eb4f
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-23 02:10:31 +0800

    Use private function

--- lisp/jcs-plugin.el
+++ lisp/jcs-plugin.el
@@ -844,7 +844,7 @@
   (setq vertico-cycle t
         vertico-resize t
         vertico-scroll-margin 0
-        vertico-sort-function #'jcs-vertico-sort)
+        vertico-sort-function #'jcs-vertico--sort-function)
   :defer-config
   (require 'jcs-vertico)
   (defconst jcs-vertico-height-ratio 0.3
