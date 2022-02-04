230f922d7d386eabfcea70f42951340bf15971a7
Jen-Chieh Shen <jcs090218@gmail.com>
2022-02-04 17:26:29 +0800

    Fix ask sh eol logic

--- lisp/mode/jcs-sh-mode.el
+++ lisp/mode/jcs-sh-mode.el
@@ -4,7 +4,7 @@
 
 (require 'sh-script)
 
-(defvar-local jcs-sh-buffer-eol nil
+(defvar-local jcs-sh--buffer-eol nil
   "Record of buffer's line endings type.")
 
 (defun jcs-ask-line-endings-for-this-sh-script (type)
@@ -13,38 +13,24 @@
    (list
     (completing-read
      (format "Line Endings Type for file `%s`: " (jcs-buffer-name-or-buffer-file-name))
-     (let ((read-lst '("Windows (dos)" "macOS (mac)" "Linux (unix)")))
+     (progn
        (require 'show-eol)
-       (push (format "=> system: (%s)" jcs-system-type) read-lst)
-       (push (format "=> file: (%s)" (show-eol--get-current-system)) read-lst)
-       read-lst))))
-  (let (sys-type)
-    (cond ((string-match-p "file" type)
-           (setq sys-type (show-eol--get-current-system)))
-          ((string-match-p "system" type)))
-    (pcase type
-      ("Windows (dos)" (setq sys-type 'dos))
-      ("macOS (mac)" (setq sys-type 'mac))
-      ("Linux (unix)" (setq sys-type 'unix)))
-    (setq jcs-sh-buffer-eol sys-type)
-    (set-buffer-file-coding-system sys-type)))
+       (list (format "=> file: (%s)" (show-eol--get-current-system))
+             (format "=> system: (%s)" jcs-system-type)
+             "Windows (dos)" "macOS (mac)" "Linux (unix)")))))
+  (setq jcs-sh--buffer-eol
+        (cond ((string-match-p "file:" type) (show-eol--get-current-system))
+              ((string-match-p "system:" type) jcs-system-type)
+              (t (pcase type
+                   ("Windows (dos)" 'dos)
+                   ("macOS (mac)" 'mac)
+                   ("Linux (unix)" 'unix)))))
+  (set-buffer-file-coding-system jcs-sh--buffer-eol))
 
-(defun jcs-sh-before-save ()
+(defun jcs-sh--before-save ()
   "Run execution before saving."
-  (if jcs-sh-buffer-eol (set-buffer-file-coding-system jcs-sh-buffer-eol)
+  (if jcs-sh--buffer-eol (set-buffer-file-coding-system jcs-sh--buffer-eol)
     (call-interactively #'jcs-ask-line-endings-for-this-sh-script)))
-
-(defun jcs-sh-untabify-save-buffer ()
-  "ShellScript save buffer function."
-  (interactive)
-  (jcs-sh-before-save)
-  (jcs-untabify-save-buffer))
-
-(defun jcs-sh-tabify-save-buffer ()
-  "ShellScript save buffer function."
-  (interactive)
-  (jcs-sh-before-save)
-  (jcs-tabify-save-buffer))
 
 ;;
 ;; (@* "Templates" )
@@ -60,6 +46,8 @@
 
 (jcs-add-hook 'sh-mode-hook
   (modify-syntax-entry ?_ "w")
+
+  (add-hook 'before-save-hook #'jcs-sh--before-save nil t)
 
   ;; File Header
   (jcs-insert-header-if-valid '("[.]sh"
