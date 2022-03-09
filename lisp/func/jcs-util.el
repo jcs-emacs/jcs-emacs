565709e4480768343c87bf1883ba47da605c29c0
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-09 22:00:06 +0800

    Fix eager macro expand error

--- lisp/func/jcs-util.el
+++ lisp/func/jcs-util.el
@@ -87,10 +87,11 @@
   "Temporary replace all switch file functions with other window during BODY
 execution."
   (declare (indent 0) (debug t))
-  (require 'noflet)
-  `(noflet ((switch-to-buffer (&rest args) (apply #'jcs-switch-to-buffer-other-window args))
-            (find-file (&rest args) (apply #'find-file-other-window args)))
-     ,@body))
+  `(progn
+     (require 'noflet)
+     (noflet ((switch-to-buffer (&rest args) (apply #'jcs-switch-to-buffer-other-window args))
+              (find-file (&rest args) (apply #'find-file-other-window args)))
+             ,@body)))
 
 ;;
 ;; (@* "Module" )
