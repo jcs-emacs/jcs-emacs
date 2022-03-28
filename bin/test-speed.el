bdc11171de0fbef1a36cb98417d808fc125727ec
Jen-Chieh Shen <jcs090218@gmail.com>
2022-03-29 03:19:57 +0800

    ci: Add generate svg speed badges

--- bin/test-speed.el
+++ bin/test-speed.el
@@ -7,7 +7,29 @@
 
 (load (concat user-emacs-directory "bin/test-startup.el"))
 
-(message "[INFO] Start within %s" (format "%0.5f" (string-to-number (emacs-init-time))))
+(defvar startup-time (format "%0.5f" (string-to-number (emacs-init-time)))
+  "Total startup time.")
+
+(message "[INFO] Start within %s" startup-time)
+
+;;
+;;; Generate speed badge
+
+(defconst svg-url-format
+  "https://img.shields.io/badge/%s-%s-green.svg"
+  "Format to generate badges.")
+
+(defconst output-dir "./docs/badges/speed/"
+  "Where the badges store.")
+
+(ignore-errors (delete-directory output-dir t))  ; clean up first
+(make-directory output-dir t)                    ; recursive
+
+(let ((os (format "%s" jcs-system-type))
+      (file (concat output-dir os ".svg"))
+      (url (format svg-url-format os startup-time)))
+  (message "Downloading SVG from `%s` to `%s`... done!" url file)
+  (url-copy-file url file))
 
 ;; Local Variables:
 ;; coding: utf-8
