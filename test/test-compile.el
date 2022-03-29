18c573bf805445c00000e7c2db8867614aff1e8d
Jen-Chieh <jcs090218@gmail.com>
2022-03-29 13:17:17 +0800

    docstring: Add commentary

--- test/test-compile.el
+++ test/test-compile.el
@@ -1,21 +1,29 @@
 ;;; test-compile.el --- Byte compile concatenate file  -*- lexical-binding: t -*-
+
 ;;; Commentary:
+;;
+;; Test the concatened file built from `eask concat`.
+;;
+;; We are only going to check for rule `redefine', and shall be sufficient for
+;; regular Emacs configuration.
+;;
+
 ;;; Code:
 
 (setq byte-compile-error-on-warn t)
 
 (let* ((concated-file "./dist/jcs-emacs.built.el")
        (concated-file (expand-file-name concated-file))
-       (test-conditions "
+       (conditions "
 ;; Local Variables:
 ;; coding: utf-8
 ;; byte-compile-warnings: (redefine)
 ;; End:
 "))
   (with-current-buffer (find-file concated-file)
-    (unless (string-suffix-p test-conditions (buffer-string))
+    (unless (string-suffix-p conditions (buffer-string))
       (goto-char (point-max))
-      (insert test-conditions))
+      (insert conditions))
     (byte-compile-file buffer-file-name)))
 
 ;; Local Variables:
