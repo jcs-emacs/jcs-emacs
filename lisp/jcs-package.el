67e50c90ae02107a5273f9211b24332d215c7dbb
Jen-Chieh Shen <jcs090218@gmail.com>
2022-04-07 04:21:12 +0800

    ci: test 2

--- lisp/jcs-package.el
+++ lisp/jcs-package.el
@@ -225,7 +225,33 @@
     toggle-quotes
     toggle-window
     transpose-frame
-)
+    transwin
+    tree-sitter-indent
+    tree-sitter-langs
+    ts
+    ts-fold
+    turbo-log
+    typescript-mode
+    undercover
+    undo-tree
+    unfill
+    use-ttf
+    vertico
+    vimrc-mode
+    visual-regexp
+    vs-dark-theme
+    vs-edit-mode
+    vs-light-theme
+    vs-revbuf
+    vue-mode
+    web-mode
+    which-key
+    whitespace-cleanup-mode
+    whole-line-or-region
+    winum
+    yarn-mode
+    yasnippet-snippets
+    zoom-window)
   "List of packages this config needs.")
 
 (setq package-pinned-packages
