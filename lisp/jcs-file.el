e9b7c045a2cbd2dfdfc4f519e24a973a0807410b
Jen-Chieh Shen <jcs090218@gmail.com>
2022-10-08 12:54:55 +0800

    refactor(ffpc): Extract files project module to external package, ffpc

--- lisp/jcs-file.el
+++ lisp/jcs-file.el
@@ -12,65 +12,6 @@
   (jcs-with-other-window (project-find-file)))
 
 ;;
-;; (@* "Project" )
-;;
-
-(defun jcs-select-find-file-current-dir (filename title)
-  "Find FILENAME in current directory.
-
-Argument FILENAME accept regular expression string.
-
-Argument TITLE is a string used when there are more than one matches."
-  (require 'f)
-  (let* ((target-files
-          (jcs-f-files-ignored-dir default-directory
-                                   (lambda (file)
-                                     (string-match-p filename (f-filename file)))))
-         (target-files-len (length target-files)))
-    (when (zerop target-files-len)
-      (user-error "[ERROR] No file '%s' found in the current directory" filename))
-    (if (= target-files-len 1)
-        (nth 0 target-files)  ; If only one file found, just get that file.
-      (completing-read title target-files))))  ; Get the selected file.
-
-(defun jcs-select-find-file-in-project (filename title)
-  "Find FILENAME in current project.
-
-Argument FILENAME accept regular expression string.
-
-Argument TITLE is a string used when there are more than one matches."
-  (require 'f)
-  (let ((project-dir (jcs-project-root)) target-files target-files-len)
-    ;; Do the find file only when the project directory exists.
-    (when project-dir
-      (setq target-files
-            (jcs-f-files-ignored-dir project-dir
-                                     (lambda (file)
-                                       (string-match-p filename (f-filename file)))
-                                     t)))
-    (when target-files (setq target-files-len (length target-files)))
-    (unless target-files-len
-      (user-error "[ERROR] No file '%s' found in project, make sure the project root exists" filename))
-    (if (= target-files-len 1)
-        (nth 0 target-files)  ; If only one file found, just get that file.
-      (completing-read title target-files))))  ; Get the selected file.
-
-(defun jcs-find-file-in-project-and-current-dir (filename title)
-  "Find the file from project root, if not found find it in current directory.
-Return full path if found, else error prompt.  FILENAME to search in project
-or current directory.  TITLE search uses regexp, meaning it could found
-multiple files at a time.  We need a title to present which file to select."
-  (if-let ((filepath
-            (or (ignore-errors (jcs-select-find-file-current-dir filename title))
-                (ignore-errors (jcs-select-find-file-in-project filename title)))))
-      filepath
-    (user-error
-     (concat "[ERROR] Can't find file '%s' in the project or current directory "
-             ", make sure the project root exists or the '%s' file exists in the "
-             "current directory")
-     filename filename)))
-
-;;
 ;; (@* "Path" )
 ;;
 
@@ -133,30 +74,6 @@ If optional argument WITH-EXT is non-nil; return path with extension."
           (unless with-ext (setq file (file-name-sans-extension file)))
           (push file types))))
     (sort types #'string-lessp)))
-
-(defun jcs-f-directories-ignored-dir (path &optional rec)
-  "Find all directories in PATH by ignored common directories with FN and REC."
-  (require 'dash)
-  (let ((dirs (f-directories path)) valid-dirs final-dirs)
-    (dolist (dir dirs)
-      (unless (member (f-filename (f-slash dir)) project-vc-ignores)
-        (push dir valid-dirs)))
-    (when rec
-      (dolist (dir valid-dirs)
-        (push (jcs-f-directories-ignored-dir dir rec) final-dirs)))
-    (setq valid-dirs (reverse valid-dirs)
-          final-dirs (reverse final-dirs))
-    (-flatten (append valid-dirs final-dirs))))
-
-(defun jcs-f-files-ignored-dir (path &optional fn rec)
-  "Find all files in PATH by ignored common directories with FN and REC."
-  (require 'dash)
-  (let ((dirs (append (list path) (jcs-f-directories-ignored-dir path rec)))
-        files)
-    (dolist (dir dirs)
-      (when-let ((fs (ignore-errors (f-files dir fn))))
-        (push fs  files)))
-    (-flatten (reverse files))))
 
 ;;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 ;; IMPORTANT: Keep core function at the top of this file.
