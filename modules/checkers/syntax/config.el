7a502624aa7ac242ece27c47a12972e0a85f9382
Jen-Chieh Shen <jcs090218@gmail.com>
2022-12-31 22:42:56 +0800

    chore(checkers): Update flycheck config

--- modules/checkers/syntax/config.el
+++ modules/checkers/syntax/config.el
@@ -7,10 +7,15 @@
   ;; And don't recheck on idle as often
   (setq flycheck-idle-change-delay 1.0)
 
+  (setq flycheck-indication-mode (if elenv-graphic-p 'right-fringe 'right-margin))
+
   ;; For the above functionality, check syntax in a buffer that you switched to
   ;; only briefly. This allows "refreshing" the syntax check state for several
   ;; buffers quickly after e.g. changing a config file.
   (setq flycheck-buffer-switch-check-intermediate-buffers t)
 
   ;; Display errors a little quicker (default is 0.9s))
-  (setq flycheck-display-errors-delay 0.25))
+  (setq flycheck-display-errors-delay 0.25)
+  :config
+  ;; Rerunning checks on every newline is a mote excessive.
+  (delq 'new-line flycheck-check-syntax-automatically))
