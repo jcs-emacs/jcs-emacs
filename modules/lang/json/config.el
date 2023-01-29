ea0072356c3cd4aa5edfb59c07f308ca5c4e94a1
Jen-Chieh <jcs090218@gmail.com>
2023-01-30 03:01:14 +0800

    fix: Remove already removed js2-mode config

--- modules/lang/json/config.el
+++ modules/lang/json/config.el
@@ -14,11 +14,6 @@
 ;;
 
 (jcs-add-hook 'json-mode-hook
-  (setq js2-bounce-indent-p t)
-  (remove-hook 'after-change-functions 'js2-minor-mode-edit t)
-
-  (js2-minor-mode -1)
-
   ;; File Header
   (jcs-insert-header-if-valid '("[.]json")
                               'jcs-insert-json-template))
