73e95d84b5885d258b7fe7c6fa1b3b230d0a3cb2
JenChieh <jcs090218@gmail.com>
2023-04-10 03:49:15 -0700

    chore(cc): Configure style for c/c++

--- modules/lang/cc/config.el
+++ modules/lang/cc/config.el
@@ -74,6 +74,51 @@
       (setq restore-point (point)))))
 
 ;;
+;; (@* "Style" )
+;;
+
+(defun jcs-cc-lineup-arglist-close (langlem)
+  "Line up the closing brace in an arglist with the opening brace IF cursor is
+preceded by the opening brace or a comma (disregarding whitespace in between)."
+  (when (save-excursion
+          (save-match-data
+            (skip-chars-backward " \t\n" (c-langelem-pos langelem))
+            (memq (char-before) (list ?, ?\( ?\;))))
+    (c-lineup-arglist langlem)))
+
+(c-add-style
+ "jcs" '((c-comment-only-line-offset . 0)
+         (c-hanging-braces-alist (brace-list-open)
+                                 (brace-entry-open)
+                                 (substatement-open after)
+                                 (block-close . c-snug-do-while)
+                                 (arglist-cont-nonempty))
+         (c-cleanup-list brace-else-brace)
+         (c-offsets-alist
+          (knr-argdecl-intro . 0)
+          (substatement-open . 0)
+          (substatement-label . 0)
+          (statement-cont . +)
+          (case-label . +)
+          ;; align args with open brace OR don't indent at all (if open
+          ;; brace is at eolp and close brace is after arg with no trailing
+          ;; comma)
+          (brace-list-intro . 0)
+          (brace-list-close . -)
+          (arglist-intro . +)
+          (arglist-close jcs-cc-lineup-arglist-close 0)
+          ;; don't over-indent lambda blocks
+          (inline-open . 0)
+          (inlambda . 0)
+          ;; indent access keywords +1 level, and properties beneath them
+          ;; another level
+          (access-label . -)
+          (label . 0))))
+
+(when (listp c-default-style)
+  (setf (alist-get 'other c-default-style) "jcs"))
+
+;;
 ;; (@* "Header" )
 ;;
 
