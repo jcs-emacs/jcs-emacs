9d9df273cac7986bc9e797302218e1cbfa41041a
JenChieh <jcs090218@gmail.com>
2023-04-17 19:07:03 -0700

    Add more shader template options

--- modules/lang/shader/config.el
+++ modules/lang/shader/config.el
@@ -10,14 +10,29 @@
 ;; (@* "Templates" )
 ;;
 
-(file-header-defins jcs-insert-shader-template "shader" "default.txt"
-  "Header for Shader header file.")
+(file-header-defins jcs-insert-shader-unlit-template "shader" "Unlit.txt"
+  "Header for Shader Unlit file.")
+
+(file-header-defins jcs-insert-shader-image-effect-template "shader" "ImageEffect.txt"
+  "Header for Shader ImageEffect file.")
+
+(file-header-defins jcs-insert-shader-surface-template "shader" "Surface.txt"
+  "Header for Shader Surface file.")
+
+(file-header-defsrc jcs-ask-shader-template "Select Shader template: "
+  '(("Unlit"       . "Effects or unique objects in your visuals that don’t need lighting")
+    ("ImageEffect" . "Are a way of post-processing rendered image")
+    ("Surface"     . "Shaders that interact with lighting is complex"))
+  (pcase index
+    (0 (jcs-insert-shader-unlit-template))
+    (1 (jcs-insert-shader-image-effect-template))
+    (2 (jcs-insert-shader-surface-template))))
 
 (file-header-defins jcs-insert-glsl-template "glsl" "default.txt"
-  "Header for GLSL header file.")
+  "Header for GLSL file.")
 
 (file-header-defins jcs-insert-hlsl-template "hlsl" "default.txt"
-  "Header for HLSL header file.")
+  "Header for HLSL file.")
 
 ;;
 ;; (@* "Hook" )
@@ -31,7 +46,8 @@
 
   ;; File Header
   (jcs-insert-header-if-valid '("[.]shader")
-                              'jcs-insert-shader-template))
+                              'jcs-ask-shader-template
+                              :interactive t))
 
 (jcs-add-hook 'glsl-mode-hook
   (modify-syntax-entry ?_ "w")
