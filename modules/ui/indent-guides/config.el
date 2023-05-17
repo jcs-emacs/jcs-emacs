8456bd051b096af816cd9c0840b6980c21d94c5e
JenChieh <jcs090218@gmail.com>
2023-05-17 04:21:40 -0700

    fix(indent-guides): Indent guides not obvious in 29.x

--- modules/ui/indent-guides/config.el
+++ modules/ui/indent-guides/config.el
@@ -5,4 +5,7 @@
   (setq highlight-indent-guides-method 'character
         highlight-indent-guides-character ?\|
         highlight-indent-guides-responsive 'top
-        highlight-indent-guides-suppress-auto-error t))
+        highlight-indent-guides-suppress-auto-error t)
+  (setq highlight-indent-guides-auto-character-face-perc 150
+        highlight-indent-guides-auto-top-character-face-perc 250
+        highlight-indent-guides-auto-stack-character-face-perc 200))
