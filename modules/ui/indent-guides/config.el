da04d070da125050e0ae728ceb3a8042d8e93b21
JenChieh <jcs090218@gmail.com>
2023-09-03 23:35:44 -0700

    fix: indent guides for light theme

--- modules/ui/indent-guides/config.el
+++ modules/ui/indent-guides/config.el
@@ -5,7 +5,23 @@
   (setq highlight-indent-guides-method 'character
         highlight-indent-guides-character ?\|
         highlight-indent-guides-responsive 'top
-        highlight-indent-guides-suppress-auto-error t)
-  (setq highlight-indent-guides-auto-character-face-perc 150
-        highlight-indent-guides-auto-top-character-face-perc 250
-        highlight-indent-guides-auto-stack-character-face-perc 200))
+        highlight-indent-guides-suppress-auto-error t))
+
+;;
+;; (@* "Faces" )
+;;
+
+(defun jcs-highlight-indent-guides--theme (theme)
+  "Update theme for `highlight-indent-guides'."
+  (pcase theme
+    (`vs-dark
+     (setq highlight-indent-guides-auto-character-face-perc 150
+           highlight-indent-guides-auto-top-character-face-perc 250
+           highlight-indent-guides-auto-stack-character-face-perc 200))
+    (`vs-light
+     (setq highlight-indent-guides-auto-character-face-perc 10
+           highlight-indent-guides-auto-top-character-face-perc 30
+           highlight-indent-guides-auto-stack-character-face-perc 20))))
+
+(jcs-theme-call #'jcs-highlight-indent-guides--theme)
+(add-hook 'jcs-after-load-theme-hook #'jcs-highlight-indent-guides--theme)
