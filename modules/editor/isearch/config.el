9b480f75f897c9a2c09ae353aae47bab0e486a9d
JenChieh <jcs090218@gmail.com>
2023-11-04 02:01:50 -0700

    fix: Use built-in isearch forward thing at point

--- modules/editor/isearch/config.el
+++ modules/editor/isearch/config.el
@@ -23,17 +23,4 @@
   (setq isearch-project-ignore-paths '("bin/"
                                        "build/"
                                        "build.min/"
-                                       "res/"))
-  :config
-  (jcs-add-hook 'isearch-mode-hook
-    ;; Paste the current symbol when `isearch' enabled.
-    (cond ((use-region-p)
-           (progn
-             (deactivate-mark)
-             (ignore-errors
-               (isearch-yank-string (buffer-substring-no-properties (region-beginning) (region-end))))))
-          ((memq this-command '(jcs-isearch-project-backward-symbol-at-point))
-           (when (char-or-string-p isearch-project--thing-at-point)
-             (backward-word 1)
-             (isearch-project--isearch-yank-string isearch-project--thing-at-point)
-             (isearch-repeat-backward))))))
+                                       "res/")))
