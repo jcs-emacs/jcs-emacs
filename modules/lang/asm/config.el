a5b24a3eb5b4454c2a89b20d219639354a5f762b
JenChieh <jcs090218@gmail.com>
2023-12-17 21:11:05 -0800

    chore: Apply comment skip

--- modules/lang/asm/config.el
+++ modules/lang/asm/config.el
@@ -8,60 +8,11 @@
 ;; (@* "Keys" )
 ;;
 
-(defun nasm-indent-line ()
-  "Indent current line as NASM assembly code."
-  (interactive)
-  (let ((orig (- (point-max) (point))))
-    (back-to-indentation)
-    (if (or (looking-at (nasm--opt nasm-directives))
-            (looking-at (nasm--opt nasm-pp-directives))
-            (looking-at "\\[")
-            (looking-at ";;+")
-            ;;--------------------------------------------------
-            ;; ATTENTION: Self copy this function from source code. Add these
-            ;; rules for my own use.
-            (jcs-is-nasm-indent-p)
-            ;;--------------------------------------------------
-            (looking-at nasm-label-regexp))
-        (indent-line-to 0)
-      (indent-line-to nasm-basic-offset))
-    (when (> (- (point-max) orig) (point))
-      (goto-char (- (point-max) orig)))))
-
-(defun jcs-is-nasm-indent-p ()
-  "JayCeS's own indent nasm rules.
-@return boolean : true - do indent, false - vice versa."
-  (let (do-indent)
-    (save-excursion
-      (back-to-indentation)
-      (forward-char 1)
-      ;; Check rule here..
-      (when (jcs-current-char-equal-p ".")
-        (setq do-indent t)))
-    do-indent))
-
-(defun jcs-asm-return ()
-  "Return key for `nasm-mode'."
-  (interactive)
-  (let (continue-comment)
-    (save-excursion
-      (ignore-errors
-        (back-to-indentation)
-        (forward-char 1)
-        (when (jcs-current-char-equal-p ";")
-          (forward-char 1)
-          (when (jcs-current-char-equal-p ";")
-            (setq continue-comment t)))))
-    (newline-and-indent)
-    (when continue-comment
-      (insert ";; ")
-      (save-excursion (indent-line-to 0)))))
-
 (defun jcs-asm-comment ()
   "Comment key for `nasm-mode'."
   (interactive)
   ;; Call normal nasm comment function before do our own nasm comment.
-  (call-interactively 'nasm-comment)
+  (call-interactively #'nasm-comment)
 
   (let (should-indent)
     (save-excursion
@@ -144,11 +95,11 @@
 ;;
 
 (jcs-add-hook '(fasm-mode-hook masm-mode-hook nasm-mode-hook)
+  (jcs-use-lisp-comment)
   (modify-syntax-entry ?_ "w")
   (jcs-asm-mode--init)
   (jcs-key-local
-    `(((kbd "RET")    . jcs-asm-return)
-      ((kbd ";")      . jcs-asm-comment))))
+    `(((kbd ";")      . jcs-asm-comment))))
 
 ;;
 ;; (@* "Extensions" )
