31deaa9a9850ae904b9325af2b39dc5a57d0d82b
JenChieh <jcs090218@gmail.com>
2024-01-12 03:13:56 -0800

    feat: Configure Tex completion packages

--- modules/lang/latex/config.el
+++ modules/lang/latex/config.el
@@ -1,5 +1,27 @@
 ;;; lang/latex/config.el  -*- lexical-binding: t; -*-
 
+(require 'tex)
+
+;;
+;; (@* "Settings" )
+;;
+
+(setq TeX-parse-self t ; parse on load
+      TeX-auto-save t  ; parse on save
+      ;; Use hidden directories for AUCTeX files.
+      TeX-auto-local ".auctex-auto"
+      TeX-style-local ".auctex-style"
+      TeX-source-correlate-mode t
+      TeX-source-correlate-method 'synctex
+      ;; Don't start the Emacs server when correlating sources.
+      TeX-source-correlate-start-server nil
+      ;; Automatically insert braces after sub/superscript in `LaTeX-math-mode'.
+      TeX-electric-sub-and-superscript t
+      ;; Just save, don't ask before each compilation.
+      TeX-save-query nil)
+
+(message-clean-mode-add-echo-commands '(TeX-auto-list-information))
+
 ;;
 ;; (@* "Templates" )
 ;;
@@ -11,8 +33,22 @@
 ;; (@* "Hook" )
 ;;
 
-(jcs-add-hook 'latex-mode-hook
-  (company-fuzzy-backend-add-before 'company-bibtex 'company-dabbrev)
+(jcs-add-hook 'LaTeX-mode-hook
+  (progn  ; BibTeX
+    (company-fuzzy-backend-add-before 'company-bibtex 'company-dabbrev))
+  (progn  ; AUCTeX
+    (company-fuzzy-backend-add-before 'company-auctex-labels 'company-dabbrev)
+    (company-fuzzy-backend-add-before 'company-auctex-bibs 'company-dabbrev)
+    (company-fuzzy-backend-add-before 'company-auctex-macros 'company-dabbrev)
+    (company-fuzzy-backend-add-before 'company-auctex-symbols 'company-dabbrev)
+    (company-fuzzy-backend-add-before 'company-auctex-environments 'company-dabbrev))
+  (progn  ; RefTeX
+    (company-fuzzy-backend-add-before 'company-reftex-labels 'company-dabbrev)
+    (company-fuzzy-backend-add-before 'company-reftex-citations 'company-dabbrev))
+  (progn  ; LaTex
+    (company-fuzzy-backend-add-before 'company-latex-commands 'company-dabbrev)
+    (company-fuzzy-backend-add-before 'company-math-symbols-latex 'company-dabbrev)
+    (company-fuzzy-backend-add-before 'company-math-symbols-unicode 'company-dabbrev))
 
   ;; File Header
   (jcs-insert-header-if-valid '("[.]tex")
