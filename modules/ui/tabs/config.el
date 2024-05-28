308cdca352dbfee524d96777d42fb262c8e6b104
JenChieh <jcs090218@gmail.com>
2024-05-28 00:51:45 -0700

    chore: Configure centaur-tabs

--- modules/ui/tabs/config.el
+++ modules/ui/tabs/config.el
@@ -2,11 +2,11 @@
 
 (use-package centaur-tabs
   :init
-  (setq centaur-tabs-cycle-scope 'tabs
-        centaur-tabs-style "wave"
+  (setq centaur-tabs-style "wave"
         centaur-tabs-set-bar 'under
         centaur-tabs-set-icons t
         centaur-tabs-icon-type 'nerd-icons
+        centaur-tabs-gray-out-icons nil
         centaur-tabs-set-modified-marker t
         centaur-tabs-hide-tab-function (lambda (x &rest _)
                                          (or (centaur-tabs-hide-tab x)
@@ -16,6 +16,10 @@
         centaur-tabs-show-navigation-buttons t
         centaur-tabs-down-tab-text " ▾ "
         centaur-tabs-backward-tab-text "⏴"
-        centaur-tabs-forward-tab-text "⏵")
+        centaur-tabs-forward-tab-text "⏵"
+        ;; Scrolling (with the mouse wheel) past the end of the tab list
+        ;; replaces the tab list with that of another Doom workspace. This
+        ;; prevents that.
+        centaur-tabs-cycle-scope 'tabs)
   :config
   (advice-add 'centaur-tabs-buffer-track-killed :override #'ignore))
