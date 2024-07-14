04020d7596f88ede7b4b9817b4abd7171f76f2c9
JenChieh <jcs090218@gmail.com>
2024-07-14 01:06:56 -0700

    feat: Clean diminished buffers

--- modules/emacs/clean-buffers/config.el
+++ modules/emacs/clean-buffers/config.el
@@ -3,12 +3,14 @@
 (use-package clean-buffers
   :init
   (setq clean-buffers-kill-active-buffer t
-        clean-buffers-useless-buffer-names
-        '("[*]CPU-Profiler-Report "
-          "[*]Memory-Profiler-Report "
-          "[*]esup"
-          "[*]Process List")
-        clean-buffers-useful-buffer-names nil)
+        clean-buffers-useless-buffer-names diminish-buffer-list
+        clean-buffers-useful-buffer-names `("[*]Echo Area" "[*]Minibuf-"
+                                            ,buffer-menu-filter-name
+                                            "[*]company")
+        clean-buffers-judge-useless-buffer-functions
+        '( clean-buffers-judge-useless-buffer-by-time
+           clean-buffers-judge-useless-buffer-by-name
+           diminish-buffer--filter))
   :config
   (jcs-advice-add 'clean-buffers-kill-useless-buffers :after
     (jcs-buffer-menu-refresh-buffer)))
