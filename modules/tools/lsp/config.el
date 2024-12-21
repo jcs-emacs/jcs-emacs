775a054aaab127d1cfa0f2d2972a6a72bf688640
JenChieh <jcs090218@gmail.com>
2024-12-20 16:17:42 -0800

    feat: Disable LSP Copilot

--- modules/tools/lsp/config.el
+++ modules/tools/lsp/config.el
@@ -10,7 +10,8 @@
         lsp-enable-symbol-highlighting nil
         ;; Reduce unexpected modifications to code
         lsp-enable-on-type-formatting nil
-        lsp-disabled-clients '( alive-lsp)
+        lsp-disabled-clients '( alive-lsp
+                                copilot-ls)
         ;; Make breadcrumbs opt-in; they're redundant with the modeline and imenu
         lsp-headerline-breadcrumb-enable nil
         ;; Inlay Hints
