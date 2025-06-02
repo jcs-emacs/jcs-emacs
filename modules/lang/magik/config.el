5ab68e5a9deb1671a55c36ca81ea866217f2cfd2
JenChieh <jcs090218@gmail.com>
2025-06-02 03:02:31 -0700

    Add magik module

---
+++ modules/lang/magik/config.el
@@ -0,0 +1,19 @@
+;;; lang/magik/config.el  -*- lexical-binding: t; -*-
+
+;;
+;; (@* "Templates" )
+;;
+
+(file-header-defins jcs-insert-magik-template "magik" "default.txt"
+  "Header for Magik header file.")
+
+;;
+;; (@* "Hook" )
+;;
+
+(jcs-add-hook 'magik-mode-hook
+  (company-fuzzy-backend-add-before 'company-magik 'company-dabbrev)
+
+  ;; File Header
+  (jcs-insert-header-if-valid '("[.]magik")
+                              'jcs-insert-markdown-template))
