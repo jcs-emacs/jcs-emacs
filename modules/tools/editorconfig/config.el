9d48e3b622a5b71531f56038e20d88b242103bcd
Jen-Chieh Shen <jcs090218@gmail.com>
2025-07-26 23:34:10 +0800

    feat(tools): Configure editorconfig custom majormode

--- modules/tools/editorconfig/config.el
+++ modules/tools/editorconfig/config.el
@@ -3,3 +3,7 @@
 (use-package editorconfig
   :init
   (setq editorconfig-trim-whitespaces-mode 'whitespace-cleanup-mode))
+
+(use-package editorconfig-custom-majormode
+  :init
+  (add-hook 'editorconfig-after-apply-functions #'editorconfig-custom-majormode))
