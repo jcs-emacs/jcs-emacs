409f9b6461e05f3befcfc84397c648dc0b9997ff
Jen-Chieh Shen <jcs090218@gmail.com>
2025-11-11 10:48:45 +0800

    configur foldvis

--- modules/editor/fold/config.el
+++ modules/editor/fold/config.el
@@ -2,19 +2,19 @@
 
 (use-package ts-fold
   :hook (tree-sitter-after-on . ts-fold-mode)
-  :hook (tree-sitter-after-on . ts-fold-line-comment-mode)
-  :hook (tree-sitter-after-on . ts-fold-indicators-mode)
+  :hook (tree-sitter-after-on . ts-fold-line-comment-mode))
+
+(use-package foldvis
   :init
-  (setq ts-fold-indicators-fringe 'left-fringe
-        ts-fold-indicators-face-function
-        (lambda (pos &rest _)
-          ;; Return the face of it's function.
-          (line-reminder--get-face (line-number-at-pos pos t))))
+  (setq foldvis-fringe 'left-fringe
+        foldvis-face-function (lambda (pos &rest _)
+                                ;; Return the face of it's function.
+                                (line-reminder--get-face (line-number-at-pos pos t))))
   :config
   (require 'line-reminder)
   (setq line-reminder-add-line-function
         (lambda (&rest _)
-          (null (ts-fold--overlays-in 'ts-fold-indicators-window (selected-window)
+          (null (foldvis--overlays-in 'foldvis-window (selected-window)
                                       (line-beginning-position) (line-end-position))))))
 
 (use-package savefold
