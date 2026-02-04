b996f6fb55726cfcbe1d713c92d65cf0f5d15d06
JenChieh <jcs090218@gmail.com>
2026-02-04 23:56:49 +0800

    chore: configure message auto save dir

--- modules/email/message/config.el
+++ modules/email/message/config.el
@@ -2,16 +2,18 @@
 
 (require 'message)
 
+;; Auto set to default.
+(setq message-auto-save-directory
+      (file-name-as-directory (expand-file-name "drafts" message-directory)))
+
+(msg-clean-add-echo-commands '( mail-send))
+
 ;;
 ;; (@* "Hook" )
 ;;
-
-(setq message-auto-save-directory nil)
 
 (jcs-add-hook 'message-mode-hook
   (jcs-key-local
     `(((kbd "<up>")   . vs-edit-previous-line)
       ((kbd "<down>") . vs-edit-next-line)
       ((kbd "C-a")    . mark-whole-buffer))))
-
-(msg-clean-add-echo-commands '( mail-send))
