a4b7961aab11ff8885233a568db2e65f47950f30
JenChieh <jcs090218@gmail.com>
2026-02-04 23:17:38 +0800

    feat: Don't auto save message email

--- modules/email/message/config.el
+++ modules/email/message/config.el
@@ -6,9 +6,12 @@
 ;; (@* "Hook" )
 ;;
 
+(setq message-auto-save-directory nil)
+
 (jcs-add-hook 'message-mode-hook
   (jcs-key-local
     `(((kbd "<up>")   . vs-edit-previous-line)
-      ((kbd "<down>") . vs-edit-next-line))))
+      ((kbd "<down>") . vs-edit-next-line)
+      ((kbd "C-a")    . mark-whole-buffer))))
 
 (msg-clean-add-echo-commands '( mail-send))
