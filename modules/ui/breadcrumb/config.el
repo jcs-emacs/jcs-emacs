9c926a30f5e780d69d14d3be01c066936329d4ca
JenChieh <jcs090218@gmail.com>
2026-05-06 19:09:33 +0800

    fix: breadcrumb opininated mode, disable it

---
+++ modules/ui/breadcrumb/config.el
@@ -0,0 +1,6 @@
+;;; ui/breadcrumb/config.el  -*- lexical-binding: t; -*-
+
+(use-package breadcrumb
+  :config
+  ;; Influenced by https://github.com/joaotavora/breadcrumb/commit/04c50e32e8f32afd68242e4dc28da02e8a45e237
+  (advice-remove 'mode--line-format-right-align 'bc-opinionated-mlf))
